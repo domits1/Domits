01332d3f758cc65d9fcceb6f3db4ef0a44c2f949
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2023-11-15 09:25:15 +0100

    initial commit. experimenting with react

---
+++ src/login.js
@@ -0,0 +1,17 @@
+// import { Amplify } from 'aws-amplify';
+//
+// import { withAuthenticator } from '@aws-amplify/ui-react';
+// import '@aws-amplify/ui-react/styles.css';
+//
+// import awsExports from './aws-exports';
+// Amplify.configure(awsExports);
+
+function Login() {
+    return (
+        <>
+            <h1>this works fine </h1>
+        </>
+    );
+}
+
+export default Login
