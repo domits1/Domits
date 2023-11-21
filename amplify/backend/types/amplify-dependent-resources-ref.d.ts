38e34e098bd8629bc8de701b30f57b3b57088af4
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2023-11-21 16:19:09 +0100

    changed backend environment

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "domits420cd8cf": {
+    "domits59290817": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
       "IdentityPoolId": "string",
@@ -8,6 +8,21 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger7b5ed19c": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "s39c22bbfa": {
+      "BucketName": "string",
+      "Region": "string"
     }
   }
 }
