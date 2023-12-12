32d54ae73583681c44085cd5f7489393723acad9
Mona Lisa <mona@Lisa.com>
2023-12-12 16:43:09 +0100

    overview pagina moet alleen nog filter

--- src/App.js
+++ src/App.js
@@ -56,6 +56,7 @@ function App() {
                     <Route path="/assortment" element={<Assortment />} />
                     {/*host dashboard*/}
                     <Route path="/hostdashboard" element={<HostDashboard />} />
+                    
                     <Route path="/hostdashboard/create" element={<CreateAccommodation />} />
                     <Route path="/hostdashboard/Read" element={<ReadAccommodation />} />
                     <Route path="/hostdashboard/Update" element={<UpdateAccommodation />} />
---
+++ src/components/guestdashboard/GuestDashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from './Navbar';
+
+const GuestDashboard = () => {
+    return ( 
+        <div className="guestdashboard">
+            <div className="header">
+                <Navbar />
+            </div>
+        </div>
+     );
+}
+ 
+export default GuestDashboard;
