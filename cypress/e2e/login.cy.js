ba379622704882591373e2ee0ef907605a377a7c
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2024-01-10 17:14:59 +0100

    tried to implement a base url for the testing env so that github can do that aswell

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,7 +3,9 @@
 describe('Authentication spec', () => {
 
   it('Login', () => {
-    cy.visit('http://localhost:3000/')
+    const baseUrl = Cypress.env('BASE_URL') || 'http://localhost:3000/';
+    cy.visit(baseUrl);
+
     // click login button
     cy.get('#root > div:nth-child(1) > div.App > header > nav > div.login-container > button').click();
     cy.get('#amplify-id-\\:ra\\:').type('quintenschaap12@gmail.com');
