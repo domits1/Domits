41805b2ff6a3f0aed02c415a1cf46e32c24f4118
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2024-01-17 13:59:06 +0100

    Fixed navigation for Assortment page

--- src/components/Assortment.js
+++ src/components/Assortment.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import Accommodations from "./Accommodations";
 import Header from "./Header";
+import Modal from "react-modal";
+import Login from "./Login";
 
 const Assortment = () => {
     const [searchQuery, setSearchQuery] = useState("");
@@ -14,9 +16,23 @@ const Assortment = () => {
         setFilterQuery(query);
     };
 
+    const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+    const openLoginModal = () => setIsLoginModalOpen(true);
+    const closeLoginModal = () => setIsLoginModalOpen(false);
+
     return (
         <div>
-            <Header/>
+            <Header openLoginModal={openLoginModal} />
+            {/* Login Modal */}
+            <Modal
+                isOpen={isLoginModalOpen}
+                onRequestClose={closeLoginModal}
+                contentLabel="Login Modal"
+            >
+                {/* Render the Login component inside the modal */}
+                <Login />
+                <button className="close-button" onClick={closeLoginModal} >Close Modal</button>
+            </Modal>
             <div className="assortment">
                 <div id="search-container">
                     <input
