65d943460b7129c94b46c954d577e1b2d58e40cd
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2024-02-06 13:52:50 +0100

    reconfigured the test script: auth

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+  // Specify where your Cypress tests are located
+  integrationFolder: 'cypress/e2e',
 
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
-    },
+  // Specify the base URL of your application
+  baseUrl: 'http://localhost:3000',
+
+  // Configure Cypress to work with webpack dev server for React
+  devServer: {
+    command: 'npm run start', // Command to start your React development server
+    port: 3000, // Port where your application runs
+    waitForServerMs: 5000, // Time to wait for the server to start before failing tests
+    // Other options if needed
   },
 });
---
+++ cypress/e2e/Auth.js
@@ -0,0 +1,14 @@
+describe('Authentication spec', () => {
+  it('Login', () => {
+    // Visit the login page
+    cy.visit('https://develop.domits.com/');
+
+    // Fill in the login form and submit
+    cy.get('input[name="email"]').type('quintenschaap12@gmail.com');
+    cy.get('input[name="password"]').type('123!Pizza');
+    cy.get('button[type="submit"]').click();
+
+    // Assert the user is redirected to the admin dashboard after successful login
+    cy.url().should('include', '/admin');
+  });
+});
