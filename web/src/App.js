7bc90c90f5a4ec945134d782616b145ddca07346
Timmhart <tim.hart@hva.nl>
2024-02-08 16:49:19 +0100

    Double quote removal

--- web/src/App.js
+++ web/src/App.js
@@ -31,7 +31,6 @@ import Error from "./components/errorpage/errorpage";
 import Stripe from 'stripe';
 export const stripe = new Stripe(process.env.STRIPE_TEST_KEY);
 
-// HERE IS THE CHANGE TIM (CAN BE REMOVED AFTERWARDS
 
 // Set the app element for react-modal
 Modal.setAppElement('#root'); // Assuming your root element has the id 'root'
