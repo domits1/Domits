7a1e8f0f63af72b7a32985f1c8c092abe8c9f747
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2024-02-27 13:21:25 +0100

    converted files to js. still need to make most of it work

---
+++ web/src/components/chat/JoinChat.js
@@ -0,0 +1,56 @@
+import React, { useState, ReactElement } from "react";
+import { useChatBackend } from "./ChatBackend";
+import { useUser } from "../../context/UserContext";
+
+const JoinChat: React.FC = (signOut): ReactElement => {
+    const { user } = useUser();
+    const {
+        recipientEmail,
+        setShowJoinButton,
+        showAlert,
+        handleStartNewChat,
+        handleAlertInputChange,
+        handleAlertConfirm,
+        handleAlertCancel,
+    } = useChatBackend(user, signOut); // Timo: sends the user from ../../context/UserContext into useChatBackend users
+    return (
+        <>
+            <div>
+                <div className="button_containerc">
+                    <button
+                        type="button"
+                        className="buttonc"
+                        onClick={handleStartNewChat}
+                    >
+                        Start New Chat
+                    </button>
+                </div>
+
+                {
+                    // @ts-ignore
+                    showAlert && (
+                        <div className="alert">
+                            <input
+                                type="text"
+                                placeholder="Enter recipient's email"
+                                value={recipientEmail}
+                                onChange={handleAlertInputChange}
+                            />
+                            <button
+                                onClick={() => {
+                                    handleAlertConfirm();
+                                    setShowJoinButton(true);
+                                }}
+                            >
+                                Confirm
+                            </button>
+                            <button onClick={handleAlertCancel}>Cancel</button>
+                        </div>
+                    )
+                }
+            </div>
+        </>
+    );
+};
+
+export default JoinChat;
