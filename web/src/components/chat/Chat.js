3a47cf13c49dae2cd2df5dc541fffe4006b917fc
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2024-02-27 09:42:49 +0100

    reworked some of the code. chatmain is now also chat

--- web/src/components/chat/Chat.js
+++ web/src/components/chat/Chat.js
@@ -1,16 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ChatMain from "../../components/chat/ChatMain";
+import "./chatbox.css";
 
 const Chat = () => {
     return (
         <div className="chat">
-            return <ChatMain user={undefined} signOut={undefined} />;
+            <div>
+                <div className="button_containerc">
+                    <button type="button" className="buttonc">
+                        Start New Chat
+                    </button>
+                </div>
+
+                <div className="">
+                    <div className="chat_box">
+                        <div className="flex-auto rounded-md p-3 ring-1 ring-inset ring-gray-200 w-3/4 my-2 self-end bg-gray-200">
+                            <div>
+                                <div className="">
+                                    <div className="username">
+                                        <span className="username-name">Username</span>{" "}
+                                    </div>
+                                    <time dateTime="2023-01-23T15:56" className="time">
+                                        2 minutes ago
+                                    </time>
+                                </div>
+                                <p className="text">
+                                    This is a message text. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="input_form">
+                            <input
+                                type="text"
+                                name="search"
+                                id="search"
+                                className="inputchat"
+                            />
+                            <div className="chat-enter">
+                                <kbd className="">Enter</kbd>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="button_containerc">
+                    <button type="button" className="buttonc">
+                        Sign Out
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
 
 export default Chat;
-
-
-
