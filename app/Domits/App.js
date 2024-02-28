aea4ccfd3224c4ba3fe3ebda0add703505dd3d97
Hamzatjo <Hamzabitc@live.nl>
2024-02-28 17:37:16 +0100

    android app development

---
+++ app/Domits/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Hello, Domits! </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+});
+
+export default App;
