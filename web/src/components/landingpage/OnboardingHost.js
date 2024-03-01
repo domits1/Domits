df85e6aeb2b2c7aa040ecc30b08d2706e5324d44
ChhD <90622630+ChahidLaaouar@users.noreply.github.com>
2024-03-01 13:58:04 +0100

    Tweede (en derde) page front-end

--- web/src/components/landingpage/OnboardingHost.js
+++ web/src/components/landingpage/OnboardingHost.js
@@ -6,15 +6,18 @@ import './landing.css';
 function OnboardingHost() {
     const [page, setPage] = useState(1); // Track the current page
     const [formData, setFormData] = useState({
-        roomType: "",
-        guests: 0,
-        bedrooms: 0,
-        bathrooms: 0,
-        beds: 0,
-        country: "",
-        postalCode: "",
-        street: "",
-        neighbourhood: ""
+        RoomType: "",
+        Guests: 0,
+        Bedrooms: 0,
+        Bathrooms: 0,
+        Beds: 0,
+        Country: "",
+        PostalCode: "",
+        Street: "",
+        Neighbourhood: "",
+        Features: {
+
+        }
     });
 
     const pageUpdater = (pageNumber) => {
@@ -35,38 +38,36 @@ function OnboardingHost() {
                 return (
                     <div class="formContainer">
                         <div class="form-section">
-                            <form>
-                                <div className="formRow">
-                                    <div class="room-type">
-                                        <label><input className="radioInput" type="radio" name="roomType" onChange={handleInputChange} value="entireHouse"></input> Entire house</label>
-                                        <label><input className="radioInput" type="radio" name="roomType" onChange={handleInputChange} value="room"></input> Room</label>
-                                        <label><input className="radioInput" type="radio" name="roomType" onChange={handleInputChange} value="sharedRoom"></input> Shared room</label>
-                                    </div>
-
-                                    <div class="quantity">
-                                        <label>How many guests? <input className="textInput" type="number" name="guests" onChange={handleInputChange} value={formData.guests} min={0}></input></label>
-                                        <label>How many bedrooms? <input className="textInput" type="number" name="bedrooms" onChange={handleInputChange} value={formData.bedrooms} min={0}></input></label>
-                                        <label>How many bathrooms? <input className="textInput" type="number" name="bathrooms" onChange={handleInputChange} value={formData.bathrooms} min={0}></input></label>
-                                        <label>How many fixed beds? <input className="textInput" type="number" name="beds" onChange={handleInputChange} value={formData.beds} min={0}></input></label>
-                                    </div>
+                            <div className="formRow">
+                                <div class="room-type">
+                                    <label><input className="radioInput" type="radio" name="RoomType" onChange={handleInputChange} value="entireHouse"></input> Entire house</label>
+                                    <label><input className="radioInput" type="radio" name="RoomType" onChange={handleInputChange} value="room"></input> Room</label>
+                                    <label><input className="radioInput" type="radio" name="RoomType" onChange={handleInputChange} value="sharedRoom"></input> Shared room</label>
                                 </div>
 
-                                <div class="locationInput">
-                                    <h2>Fill in Location</h2>
-                                    <label>Country<input className="textInput locationText" name="country" onChange={handleInputChange} value={formData.country}></input></label>
-                                    <label>Postal Code <input className="textInput locationText" name="postalCode" onChange={handleInputChange} value={formData.postalCode}></input></label>
-                                    <label>Street + house nr.<input className="textInput locationText" name="street" onChange={handleInputChange} value={formData.street}></input></label>
-                                    <label>Neighbourhood<input className="textInput locationText" name="neighbourhood" onChange={handleInputChange} value={formData.neighbourhood}></input></label>
+                                <div class="quantity">
+                                    <label>How many guests? <input className="textInput" type="number" name="Guests" onChange={handleInputChange} value={formData.guests} min={0}></input></label>
+                                    <label>How many bedrooms? <input className="textInput" type="number" name="Bedrooms" onChange={handleInputChange} value={formData.bedrooms} min={0}></input></label>
+                                    <label>How many bathrooms? <input className="textInput" type="number" name="Bathrooms" onChange={handleInputChange} value={formData.bathrooms} min={0}></input></label>
+                                    <label>How many fixed beds? <input className="textInput" type="number" name="Beds" onChange={handleInputChange} value={formData.beds} min={0}></input></label>
                                 </div>
-                                <div className='buttonHolder'>
-                                    <button type="button" className='nextButtons'>Go back to change</button>
-                                    <button className='nextButtons' onClick={() => pageUpdater(page + 1)}>Confirm and proceed</button>
-                                </div>
-                            </form>
+                            </div>
+
+                            <div class="locationInput">
+                                <h2>Fill in Location</h2>
+                                <label>Country<input className="textInput locationText" name="Country" onChange={handleInputChange} value={formData.country}></input></label>
+                                <label>Postal Code <input className="textInput locationText" name="PostalCode" onChange={handleInputChange} value={formData.postalCode}></input></label>
+                                <label>Street + house nr.<input className="textInput locationText" name="Street" onChange={handleInputChange} value={formData.street}></input></label>
+                                <label>Neighbourhood<input className="textInput locationText" name="Neighbourhood" onChange={handleInputChange} value={formData.neighbourhood}></input></label>
+                            </div>
+                            <div className='buttonHolder'>
+                                <button type="button" className='nextButtons'>Go back to change</button>
+                                <button className='nextButtons' onClick={() => pageUpdater(page + 1)}>Confirm and proceed</button>
+                            </div>
                         </div>
                         <div class="map-section">
                             <label>What we show on Domits.com</label>
-                            <div id="map-placeholder">Map will go here</div>
+                            <div id="map-placeholder">Map is still being worked on</div>
                         </div>
                     </div>
                 );
@@ -74,21 +75,44 @@ function OnboardingHost() {
                 return (
                     <div class="formContainer">
                         <div class="form-section">
-                            <form>
-
-                                <div className='buttonHolder'>
-                                    <button className='nextButtons' onClick={() => pageUpdater(page - 1)}>Go back to change</button>
-                                    <button className='nextButtons' onClick={() => pageUpdater(page + 1)}>Confirm and proceed</button>
+                            <label>Add accomodation features</label>
+                            <h2>Fill in ammendities</h2>
+                            <div class="room-features formRow">
+                                <div>
+                                    <label><input type="checkbox" className="radioInput" name="Wifi"></input>Wifi</label>
+                                    <label><input type="checkbox" className="radioInput" name="Television"></input>Television</label>
+                                    <label><input type="checkbox" className="radioInput" name="Kitchen"></input>Kitchen</label>
+                                    <label><input type="checkbox" className="radioInput" name="Washing machine"></input>Washing machine</label>
                                 </div>
-                            </form>
+                                <div>
+                                    <label><input type="checkbox" className="radioInput" name="Airconditioning"></input>Airconditioning</label>
+                                    <label><input type="checkbox" className="radioInput" name="Onsite parking"></input>Onsite parking</label>
+                                    <label><input type="checkbox" className="radioInput" name="Home office"></input>Home office</label>
+                                </div>
+                            </div>
+                            <h2>Fill in safety measures</h2>
+                            <div class="room-features formRow">
+                            <div>
+                                    <label><input type="checkbox" className="radioInput" name="Smoke detector"></input>Smoke detector</label>
+                                    <label><input type="checkbox" className="radioInput" name="First Aid kit"></input>First Aid kit</label>
+                                </div>
+                                <div>
+                                    <label><input type="checkbox" className="radioInput" name="Fire extinguisher"></input>Fire extinguisher</label>
+                                </div>
+                            </div>
+                            <div className='buttonHolder'>
+                                <button className='nextButtons' onClick={() => pageUpdater(page - 1)}>Go back to change</button>
+                                <button className='nextButtons' onClick={() => pageUpdater(page + 1)}>Confirm and proceed</button>
+                            </div>
+
                         </div>
                         <div class="front-section">
                             <div className="room-info">
                                 <label>Add accomodation information</label>
                                 <label>Title<input className="textInput locationText" name="title"></input></label>
-                                <label style={{alignItems: 'start'}}>Description<textarea className="textInput locationText" name="description" rows="10" cols="30"></textarea></label>
-                                
-                                <div id="map-placeholder">Map will go here</div>
+                                <label style={{ alignItems: 'start' }}>Description<textarea className="textInput locationText" name="description" rows="10" cols="30"></textarea></label>
+
+                                <div id="map-placeholder">Images are still being worked on</div>
                             </div>
                         </div>
                     </div>
