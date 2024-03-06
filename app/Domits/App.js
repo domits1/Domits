c97e1fe5fc0f4c78ae74189ee7f721ce7c2fbaef
Hamzatjo <Hamzabitc@live.nl>
2024-03-06 12:27:16 +0100

    Footer, start on guestDashboard.js

--- app/Domits/App.js
+++ app/Domits/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigation from './src/navigation/appNavigation.js';
-import BottomTabNavigator from './src/components/BottomTabNavigator'
 
 function App() {
     return (
         <NavigationContainer>
-            <BottomTabNavigator />
+            <AppNavigation />
         </NavigationContainer>
     );
 }
--- app/Domits/src/navigation/appNavigation.js
+++ app/Domits/src/navigation/appNavigation.js
@@ -1,23 +1,35 @@
+// appNavigation.js
+
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/homeScreen';
 import HostDashboard from '../screens/hostdashboard/hostDashboard';
 import GuestDashboard from '../screens/guestdashboard/guestDashboard';
 import BottomTabNavigator from '../components/BottomTabNavigator';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function DashboardNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Messages" component={Messages} />
+      <Tab.Screen name="Account" component={AccountPage} />
+    </Tab.Navigator>
+  );
+}
 
 function AppNavigation() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false, // Hide the header for all screens
+      }}>
+      <Stack.Screen name="Dashboard" component={BottomTabNavigator} />
       <Stack.Screen name="HostDashboard" component={HostDashboard} />
       <Stack.Screen name="GuestDashboard" component={GuestDashboard} />
-      <Stack.Screen
-        name="Main"
-        component={BottomTabNavigator}
-        options={{headerShown: false}}
-      />
     </Stack.Navigator>
   );
 }
