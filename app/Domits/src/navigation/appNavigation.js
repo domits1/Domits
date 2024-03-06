f7579fa0801a9de6cc39572662a061c33ccdc6fe
Timmhart <tim.hart@hva.nl>
2024-03-06 17:00:05 +0100

    removed 1 double line of code (createNativeStackNavigator)

--- app/Domits/src/navigation/appNavigation.js
+++ app/Domits/src/navigation/appNavigation.js
@@ -1,7 +1,6 @@
 // appNavigation.js
 
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/homeScreen';
