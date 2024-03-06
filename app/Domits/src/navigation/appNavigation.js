984b42016b7deb3c017bc703f26de8d6230be7b1
kacperfl <kacperfl29@gmail.com>
2024-03-06 09:38:41 +0100

    small changes

--- app/Domits/src/navigation/appNavigation.js
+++ app/Domits/src/navigation/appNavigation.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/homeScreen';
 import HostDashboard from '../screens/hostdashboard/hostDashboard';
 import GuestDashboard from '../screens/guestdashboard/guestDashboard';
- 
 
 const Stack = createNativeStackNavigator();
 
 function AppNavigation() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false, // Hiermee wordt de volledige header verborgen
+      }}>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="HostDashboard" component={HostDashboard} />
       <Stack.Screen name="GuestDashboard" component={GuestDashboard} />
