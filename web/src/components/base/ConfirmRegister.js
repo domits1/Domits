70b6192f66ff6afd3176dba1b84c961f169ed542
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2024-03-07 16:55:45 +0100

    patched merge by using full confirm register from paymentsfix

--- web/src/components/base/ConfirmRegister.js
+++ web/src/components/base/ConfirmRegister.js
@@ -3,21 +3,18 @@ import { Auth } from 'aws-amplify';
 import { useLocation, useNavigate } from 'react-router-dom';
 import DigitInputs from '../ui/DigitsInputs/DigitsInputs';
 import './ConfirmRegister.css';
-import { useAuth } from './AuthContext'; // Import the AuthContext hook
 import FlowContext from '../../FlowContext';
 
 function ConfirmEmail() {
     const [isConfirmed, setIsConfirmed] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
-    const { credentials } = useAuth(); // Access credentials from AuthContext
+    const inputRef = useRef([]);
     const location = useLocation();
     const navigate = useNavigate();
-    const inputRef = useRef([]);
     const { flowState } = useContext(FlowContext);
     const { isHost } = flowState;
 
     const userEmail = location.state === null ? "" : location.state.email;
-    const userPassword = credentials.password; // Access password from AuthContext
 
     async function createStripeAccount() {
         const options = {
@@ -43,31 +40,24 @@ function ConfirmEmail() {
         }
     }
 
-
     function onSubmit(e) {
         e.preventDefault();
         let code = "";
         inputRef.current.forEach((input) => { code += input.value });
 
-        try {
-            const result = await Auth.confirmSignUp(userEmail, code);
-            if (result === 'SUCCESS') {
-                setIsConfirmed(true);
-                // Manually sign in the user after confirming their sign-up
-                await Auth.signIn(userEmail, userPassword); // Sign in with email and password
-
-                if (isHost) {
-                    createStripeAccount();
+        Auth.confirmSignUp(userEmail, code)
+            .then(result => {
+                if (result === 'SUCCESS') {
+                    setIsConfirmed(true);
+                    if (isHost) {
+                        createStripeAccount();
+                    }
+                    setTimeout(() => navigate('/'), 3000);
                 }
-                // Redirect to home page ("/") after 3 seconds
-                setTimeout(() => {
-                    navigate('/');
-                }, 3000);
-            }
-        } catch (error) {
-            setErrorMessage('Invalid verification code, please check your email!');
-            console.error('Error confirming sign up:', error);
-        }
+            })
+            .catch(error => {
+                setErrorMessage('Invalid verification code, please check your email!');
+            });
     }
 
     const handleResendCode = () => {
@@ -82,9 +72,13 @@ function ConfirmEmail() {
         <div className="confirmEmailContainer">
             <div className="confirmEmailTitle">Verify your registration</div>
             <form className="confirmEmailForm" onSubmit={onSubmit}>
-                <div className="enter6DigitText">Enter 6 digit code sent to your email</div>
-                <DigitInputs amount={6} inputRef={inputRef} className="confirmEmailDigitsInput" />
-                {errorMessage && <div className="errorText">{errorMessage}</div>}
+                <div className="enter6DigitText">
+                    Enter 6 digit code sent to your email
+                </div>
+                <DigitInputs amount={6} inputRef={inputRef} />
+                {errorMessage && (
+                    <div className="errorText">{errorMessage}</div>
+                )}
                 <button className="verifyRegisterButton" type="submit">Verify registration</button>
                 <div className="notReceivedCodeText">
                     Not received a code? Check your spam folder or let us resend a code.
