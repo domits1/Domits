d1ddd312e4a1246726dd8966a60993dc57da1448
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2024-03-07 12:03:32 +0100

    login unit tests works not as expected

--- web/src/components/__tests__/Login.test.js
+++ web/src/components/__tests__/Login.test.js
@@ -8,29 +8,32 @@ import { Auth } from 'aws-amplify';
 jest.mock('aws-amplify', () => ({
     Auth: {
         signIn: jest.fn(),
+        currentAuthenticatedUser: jest.fn(),
+        signOut: jest.fn()
     },
 }));
 
 describe('Login Component', () => {
-    it('renders without crashing', () => {
-        render(
+    it('displays error message on invalid login attempt', async () => {
+        Auth.signIn.mockRejectedValueOnce(new Error('Invalid username or password'));
+
+        const { getByLabelText, getByText } = render(
             <Router>
                 <Login />
             </Router>
         );
-    });
 
-    // it('displays error message on invalid login attempt', async () => {
-    //     Auth.signIn.mockRejectedValueOnce(new Error('Invalid username or password'));
-    //
-    //     const { getByLabelText, getByText } = render(<Login />);
-    //     fireEvent.change(getByLabelText('Username:'), { target: { value: 'quintenschaap12@gmail.com' } });
-    //     fireEvent.change(getByLabelText('Password:'), { target: { value: 'pizza!123' } });
-    //     fireEvent.click(getByText('Login'));
+        // Simulate user input
+        // fireEvent.change(getByLabelText('Username:'), { target: { name: 'email', value: '123@123' } });
+        // fireEvent.change(getByLabelText('Password:'), { target: { name: 'password', value: 'pizza!123' } });
+        //
+        // // Click on the login button
+        fireEvent.click(getByText('Login'));
 
         // Wait for error message to appear
         // await waitFor(() => {
         //     expect(getByText('Invalid username or password. Please try again.')).toBeInTheDocument();
         // });
-    // });
+    });
 });
+
