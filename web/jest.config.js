565653ff9abb96cf12b9a7a71dd84433e8176b57
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2024-03-14 10:29:44 +0100

    added babel configuration for the pipeline

--- web/jest.config.js
+++ web/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     testEnvironment: "jsdom",
-    testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"]
+    testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
+    transform: {
+        "^.+\\.[t|j]sx?$": "babel-jest",
+    },
 };
