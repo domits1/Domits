82344a52144394a696e3bf2a8b51086d9886d4cf
ChhD <90622630+ChahidLaaouar@users.noreply.github.com>
2024-03-18 17:29:36 +0100

    Aws lambda functie begin gemaakt.

---
+++ api_chhd/checkout.js
@@ -0,0 +1,2 @@
+import { ListTablesCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { UpdateCommand, PutCommand, DynamoDBDocumentClient, ScanCommand, DeleteCommand } from "@aws-sdk/lib-dynamodb";
