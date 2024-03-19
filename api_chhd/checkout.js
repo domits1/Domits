5a294a86febe23ac32c437855b9ba1123d8cab2c
ChhD <chhdlaaouar@gmail.com>
2024-03-19 21:47:08 +0100

    Kleine aanpassingen en test checkout

--- api_chhd/checkout.js
+++
@@ -1,2 +0,0 @@
-import { ListTablesCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { UpdateCommand, PutCommand, DynamoDBDocumentClient, ScanCommand, DeleteCommand } from "@aws-sdk/lib-dynamodb";
