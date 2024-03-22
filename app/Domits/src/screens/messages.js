3dc05e93a91d389f4ffb31977f5b7ebe7a8872eb
Timmhart <tim.hart@hva.nl>
2024-03-22 16:46:54 +0100

    basic chat example

--- app/Domits/src/screens/messages.js
+++ app/Domits/src/screens/messages.js
@@ -1,13 +1,48 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import {SafeAreaView} from "react-native-safe-area-context";
+import React, { useState, useCallback, useEffect } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
 
-const Messages = () => {
+export function Messages() {
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'This is just an example for the Domits app messages tab!',
+        createdAt: new Date(),
+        quickReplies: {
+          type: 'checkbox',
+          keepIt: true,
+          values: [
+            {
+              title: '😋 Yes',
+              value: 'yes',
+            },
+          ],
+        },
+        user: {
+          _id: 2,
+          name: 'Domits',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    )
+  }, [])
+
   return (
-      <SafeAreaView style={{ flex: 1 }}>
-      <Text>Messsages</Text>
-      </SafeAreaView>
-  );
-};
+    <GiftedChat
+      messages={messages} 
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  )
+}
 
-export default Messages;
+export default Messages
