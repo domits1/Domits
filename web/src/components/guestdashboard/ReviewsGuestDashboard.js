de25fbdb70596962b49ef9ff0424712fc7916340
Jiaming <chaosgamingnl33@gmail.com>
2024-03-29 16:54:57 +0100

    API call

--- web/src/components/guestdashboard/ReviewsGuestDashboard.js
+++ web/src/components/guestdashboard/ReviewsGuestDashboard.js
@@ -14,8 +14,38 @@ function GuestReviews() {
         }
     };
 
+    async function retrieveReviews() {
+        await setUserId();
+        if(userId === null) {
+            console.log("No user id")
+            return;
+        }
+
+        const options = {
+            userIdFrom: userId
+        }
+
+        try {
+            const response = await fetch('https://gwl35zqai9.execute-api.eu-north-1.amazonaws.com/default/params', {
+            method: 'POST',
+            body: JSON.stringify(options),
+            headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            }
+           });
+           if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+           }
+
+           const data = await response.json();
+           console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     useEffect(() => {
-        setUserId();
+        retrieveReviews();
     });
     return (
         <div className="container">
