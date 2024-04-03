579ed86d94a52f126ff7c4f7c07de9d9eac269b7
Timmhart <tim.hart@hva.nl>
2024-04-03 15:13:30 +0200

    Added bath & bedrooms to accommodation on homepage

--- web/src/components/hostdashboard/Accommodations.js
+++ web/src/components/hostdashboard/Accommodations.js
@@ -26,6 +26,9 @@ const Accommodations = ({ searchQuery }) => {
                 size: `${item.Size}m²`,
                 price: `€${item.Price}/night`,
                 id: item['#PK'],
+                bathrooms: `${item.Bathrooms} Bathrooms`,
+                bedrooms: `${item.Bedrooms} Bedrooms`,
+                persons: `${item.Persons} Persons`,
             }));
             setAccolist(formattedData);
             // console.log("Image URLs:", formattedData.map(item => item.image));
@@ -51,7 +54,11 @@ const Accommodations = ({ searchQuery }) => {
                             <div className="accocard-title">{accommodation.title}</div>
                             <div className="accocard-price">{accommodation.price}</div>
                             <div className="accocard-detail">{accommodation.details}</div>
-                            <div className="accocard-size">{accommodation.size}</div>
+                            <div className="accocard-specs">
+                                <div className="accocard-size">{accommodation.size}</div>
+                                <div className="accocard-size">{accommodation.bathrooms}</div>
+                                <div className="accocard-size">{accommodation.bedrooms}</div>
+                            </div>
                         </div>
                     </Link>
                 </div>
