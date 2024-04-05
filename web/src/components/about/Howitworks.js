47e2cc1f2999fb282ce116ff6b1554f15bb4d600
ChhD <90622630+ChahidLaaouar@users.noreply.github.com>
2024-04-05 18:35:09 +0200

    Staticpages aangemaakt

---
+++ web/src/components/about/Howitworks.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './howitworks.css'
+
+function Howitworks() {
+
+
+
+    return (
+        <div>
+            <p>Howitworks</p>
+        </div>
+    )
+}
+
+export default Howitworks;
