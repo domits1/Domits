eb1cfa9429d2b7ca335153a6497921e769f46ca2
Jiaming <chaosgamingnl33@gmail.com>
2024-04-11 14:02:03 +0200

    Accommodation overview + styling

--- web/src/components/hostdashboard/HostListings.js
+++ web/src/components/hostdashboard/HostListings.js
@@ -4,11 +4,25 @@ import './HostHomepage.css'
 import add from "../../images/icons/host-add.png";
 import { useNavigate } from 'react-router-dom';
 import { Auth } from "aws-amplify";
+import house from "../../images/house1.jpeg";
 
 function HostListings() {
     const [accommodations, setAccommodations] = useState([]);
     const [userId, setUserId] = useState(null);
     const navigate = useNavigate();
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const day = date.getDate().toString().padStart(2, '0'); // Ensures the day is two digits
+        const month = (date.getMonth() + 1).toString().padStart(2, '0'); // getMonth() is zero-based; +1 to make it 1-based
+        const year = date.getFullYear();
+
+        return `${day}/${month}/${year}`;
+    }
+    const getFeatures = (features) => {
+        return Object.entries(features)
+            .filter(([key, value]) => value === true)
+            .map(([key, value]) => key);
+    }
     useEffect(() => {
         const setUserIdAsync = async () => {
             try {
@@ -77,25 +91,38 @@ function HostListings() {
                             <p>Add new accommodation</p>
                         </div>
                         <div className="box fullBox">
-                            <p className="">Current listings</p>
+                            <p className="header">Current listings</p>
                             {accommodations.length > 0 ? (
                                 accommodations.map((accommodation) => (
-                                    <div key={accommodation.ID} className="review-tab">
-                                        <p>Accommodation: {accommodation.Title}</p>
-                                        <p>Location: {accommodation.Country}, {accommodation.City}, {accommodation.Street}</p>
-                                        <p>Type: {accommodation.Type}</p>
-                                        <p>Room Type: {accommodation.Roomtype}</p>
-                                        <p>Guests Allowed: {accommodation.Guests}</p>
+                                    <div key={accommodation.ID} className="accommodation-tab">
+                                        <div className="accommodation-left">
+                                            <p className="accommodation-title">{accommodation.Title}</p>
+                                            <p>{accommodation.Country},
+                                                {accommodation.City},
+                                                {accommodation.Street},
+                                                {accommodation.PostalCode}
+                                            </p>
+                                            <img src={house} alt="icon" className="accommodation-img"/>
+                                        </div>
+                                        <div className="accommodation-right">
+                                            <p>Description: {accommodation.Description}</p>
+                                            <p>Listed on: {formatDate(accommodation.createdAt)}</p>
+                                            <p>Measurements: {accommodation.Measurements}m²</p>
+                                            <p>Features: {accommodation.Features.length > 0 ? (
+                                                getFeatures(accommodation.Features)) : ('none')}
+                                            </p>
+                                            <p>Rent: ${accommodation.Rent}</p>
+                                        </div>
                                     </div>
                                 ))
                             ) : (
-                                <div className="reviewBox">
-                                    <p className="review-alert">It appears that you have not listed any accommodations yet...</p>
+                                <div className="accommodation-box">
+                                    <p className="accommodation-alert">It appears that you have not listed any accommodations yet...</p>
                                 </div>
                             )}
                         </div>
                         <div className="box">
-                            <p className="">Pending</p>
+                            <p className="header">Pending</p>
                         </div>
                     </div>
                 </div>
