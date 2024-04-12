b17ab0e931bac0ff62de580907ebe8bf6f1d2fa8
Timmhart <tim.hart@hva.nl>
2024-04-12 15:20:39 +0200

    Listing details page front end fix.

--- web/src/components/listingdetails/ListingDetails.js
+++ web/src/components/listingdetails/ListingDetails.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React from 'react';
 import "./listingdetails.css";
+import huubHomer from '../../images/huubHomerSmall.png';
 import detailbigimg from '../../images/accobigimg.png';
 import detailimg1 from '../../images/accoimg1.png';
-import detailimg2 from '../../images/accoimg2.png';
+// import detailimg2 from '../../images/accoimg2.png';
 import accomap from '../../images/accomap.png';
 import detailimg3 from '../../images/accoimg3.png';
 import detailimg4 from '../../images/accoimg4.png';
 import adultroom from '../../images/adultacco.png';
 import teenroom from '../../images/teenacco.jpg';
 import kidroom from '../../images/kidacco.jpg';
-import back from '../../images/arrowleft.png';
+// import back from '../../images/arrowleft.png';
 import { Link } from "react-router-dom";
 import telescope from "../../images/telescope.png";
 import smarttv from "../../images/tv.png";
@@ -18,13 +19,35 @@ import vault from "../../images/vault.png";
 import superfastwifi from "../../images/wifi.png";
 import welcomegift from "../../images/gift.png";
 import dimmablelight from "../../images/light.png";
-import goated from "../../images/goated.jpg"
+// import goated from "../../images/goated.jpg"
 import bookarrow from "../../images/whitearrow.png"
 import backarrow from "../../images/arrowleft.png";
 import pluscircle from "../../images/plus-circle.svg";
 import pluscircleblack from "../../images/plus-circle-black.svg";
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
 import star from "../../images/Star.svg";
 import arrow from "../../images/arrow.svg"
+
+const images = [
+    {
+        original: detailbigimg,
+        thumbnail: detailbigimg,
+    },
+    {
+        original: detailimg1,
+        thumbnail: detailimg1,
+    },
+    {
+        original: detailimg3,
+        thumbnail: detailimg3,
+    },
+    {
+        original: detailimg4,
+        thumbnail: detailimg4,
+    },
+];
+
 
 const ListingDetails = () => {
     return (
@@ -37,12 +60,16 @@ const ListingDetails = () => {
                             <div className="listing-details-back-arrow">Back</div>
                         </a>
                     </div>
-                    <div className="listing-details-title">
-                        Minimalistic and cozy place in Haarlem
-                    </div>
+                    <h1 className="listing-details-title">
+                        Large white villa in Athens
+                    </h1>
                 </div>
                 <div className="listing-details-image-window">
-                    <div className="listing-details-big-img">
+                <ImageGallery 
+                    items={images} 
+                    thumbnailPosition="right" 
+                    />
+                    {/* <div className="listing-details-big-img">
                         <img src={detailbigimg} alt="detailbigimg" />
                     </div>
                     <div className="listing-details-side-img">
@@ -50,13 +77,24 @@ const ListingDetails = () => {
                         <img src={accomap} alt="detailimg2" id="top-right-img" />
                         <img src={detailimg3} alt="detailimg3" />
                         <img src={detailimg4} alt="detailimg4" id="bottom-right-img" />
-                    </div>
+                    </div> */}
                 </div>
                 <div className="price-and-rooms-row">
-                    <div className="price-per-night-text">$1400 night</div>
-                    <div className="guest-and-rooms-text">8 guests - 8 beds - 4 bedrooms - 4 bathrooms</div>
+                    <p className="price-per-night-text">$1400 per night</p>
+                    <p className="guest-and-rooms-text">8 guests</p>
+                    <p className="guest-and-rooms-text">8 beds</p>
+                    <p className="guest-and-rooms-text">4 bedrooms</p>
+                    <p className="guest-and-rooms-text">4 bathrooms</p>
+                    <div className="listing-details-book-button">
+                        <Link to={`/bookingoverview`}>
+                            <button>Book* <img src={bookarrow} alt="Book Arrow" /></button>
+                        </Link>
+                    </div>
                 </div>
-                <div className="accommodation-detais-text">Accommodation details</div>
+
+                {/* Ik weet niet of we dit ooit nodig gaan hebben  */}
+
+                {/* <div className="accommodation-detais-text">Accommodation details</div>
                 <div className="rooms-text">Rooms</div>
                 <div className="listing-details-rooms-section">
                     <div className="listing-details-room">
@@ -88,37 +126,52 @@ const ListingDetails = () => {
                     <button className="listing-details-show-more">
                         <img src={pluscircle} alt="Plust circle" />Show more
                     </button>
+                </div> */}
+                <div className="listing-details-services-container">
+                    <div className="listing-details-hostDetails">
+                        <img src={huubHomer} alt="Profile picture of host" className="detailsHostImg" />
+                        <div className="hostDetailsColumn">
+                            <h3 className="hostDetailsHeading">Hosted by Huub</h3>
+                            <span>Verified host</span>
+                        </div>
+                    </div>
+                    <div className="listing-description">
+                        <p>A sleek, white minimalist home stands under a clear blue sky, accented by a solitary tree.</p>
+                    </div>
+                    <h3 className="listing-details-services-header">This place offers the following:</h3>
+                    <ul className="listing-details-place-offers">
+                        <li className="listing-details-offer-item">
+                            <img src={smarttv} alt="Smart TV" /> <span>Smart TV</span>
+                        </li>
+                        <li className="listing-details-offer-item">
+                            <img src={sauna} alt="Sauna" /> <span>Sauna</span>
+                        </li>
+                        <li className="listing-details-offer-item">
+                            <img src={vault} alt="Vault" /> <span>Vault</span>
+                        </li>
+                        <li className="listing-details-offer-item">
+                            <img src={welcomegift} alt="Welcome Gift" /> <span>Welcome Gift</span>
+                        </li>
+                        <li className="listing-details-offer-item">
+                            <img src={dimmablelight} alt="Dimmable lights" /> <span>Dimmable lights</span>
+                        </li>
+                        <li className="listing-details-offer-item">
+                            <img src={telescope} alt="Telescope" /> <span>Telescope</span>
+                        </li>
+                        <li className="listing-details-offer-item">
+                            <img src={superfastwifi} alt="Super fast WiFi" /> <span>Super fast WiFi</span>
+                        </li>
+                    </ul>
+                    <div className="show-more-button">
+                        <button className="listing-details-show-more black-show-more">
+                            <img src={pluscircleblack} alt="Plust circle" />Show more
+                        </button>
+                    </div>
                 </div>
-                <div className="listing-details-place-offers-text">This place offers the following:</div>
-                <div className="listing-details-place-offers">
-                    <div className="listing-details-offer-item">
-                        <img src={smarttv} alt="Smart TV" /> Smart TV
-                    </div>
-                    <div className="listing-details-offer-item">
-                        <img src={sauna} alt="Sauna" /> Sauna
-                    </div>
-                    <div className="listing-details-offer-item">
-                        <img src={vault} alt="Vault" /> Vault
-                    </div>
-                    <div className="listing-details-offer-item">
-                        <img src={welcomegift} alt="Welcome Gift" /> Welcome Gift
-                    </div>
-                    <div className="listing-details-offer-item">
-                        <img src={dimmablelight} alt="Dimmable lights" /> Dimmable lights
-                    </div>
-                    <div className="listing-details-offer-item">
-                        <img src={telescope} alt="Telescope" /> Telescope
-                    </div>
-                    <div className="listing-details-offer-item">
-                        <img src={superfastwifi} alt="Super fast WiFi" /> Super fast WiFi
-                    </div>
-                </div>
-                <div className="show-more-button">
-                    <button className="listing-details-show-more black-show-more">
-                        <img src={pluscircleblack} alt="Plust circle" />Show more
-                    </button>
-                </div>
-                <div className="listing-details-review-card-title">Reviews</div>
+
+                {/* Het gedeelte reviews heb ik uitgecomment. Dit moet later toegevoegd worden als het werkt */}
+
+                {/* <div className="listing-details-review-card-title">Reviews</div>
                 <div className="listing-details-verified-guests-text">Verified Domits guests have said this:</div>
                 <div className="listing-details-review-card">
                     <div className="listing-details-review-title">Clint Eastwood</div>
@@ -157,7 +210,11 @@ const ListingDetails = () => {
                     <button className="listing-details-show-more black-show-more listing-details-extra-margin-top">
                         <img src={pluscircleblack} alt="Plust circle" />Show more
                     </button>
-                </div>
+                </div> */}
+
+
+                {/* Dit gedeelte is voor things nearby. Misschien is dit nodig in de future voor nu niet */}
+
                 {/* <div className="listing-details-country-place-text">The Netherlands, Haarlem</div>
                 <div className="listing-details-things-closeby-text">Things to do closeby:</div>
                 <div className="listing-details-things-to-do-card">
@@ -186,8 +243,11 @@ const ListingDetails = () => {
             </div>
             {/* End of the booking-information-section*/}
 
+
+                {/* De bookingscard heb ik voor nu verwijderd.  */}
+
             {/* Start of the booking-details-section*/}
-            <div className="booking-details-section">
+            {/* <div className="booking-details-section">
                 <div className="booking-details-text">Booking details</div>
                 <div className="booking-details-card">
                     <div className="listing-details-check-container">
@@ -217,9 +277,9 @@ const ListingDetails = () => {
                             <button>Book* <img src={bookarrow} alt="Book Arrow" /></button>
                         </Link>
                     </div>
-                    <div className="listing-details-book-button-star-text">*You wont be charged yet</div>
+                    <div className="listing-details-book-button-star-text">*You wont be charged yet</div> */}
                     {/*  Horizontal line  */}
-                    <hr />
+                    {/* <hr />
                     <div className="listing-details-booking-information-section">
                         <div className="listing-details-booking-information-row">
                             <div className="listing-details-booking-information-text">7 nights x $1400 a night</div>
@@ -243,7 +303,7 @@ const ListingDetails = () => {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 }
