e450b9aa65031099cadbabd8ced3d379350ae3d9
SHvanderWilt <sebastiaanvanderwilt@gmail.com>
2024-04-18 16:43:44 +0200

    Settings now shows the username of the currnt user istead of "Lotte's Settings".

--- web/src/components/guestdashboard/SettingsGuestDashboard.js
+++ web/src/components/guestdashboard/SettingsGuestDashboard.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './settingsguestdashboard.css';
 import './guestdashboard.css';
 import Pages from "./Pages.js";
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import faceHappyIcon from "../../images/icons/face-happy.png";
 import settingsIcon from "../../images/icons/settings-04.png";
 import { changeEmail, confirmEmailChange } from './emailSettings.js';
+import { Auth } from 'aws-amplify';
 
 const SettingsTab = () => {
     const navigate = useNavigate();
@@ -14,20 +15,31 @@ const SettingsTab = () => {
     const [confirmEmail, setConfirmEmail] = useState('');
     const [verificationCode, setVerificationCode] = useState('');
     const [showVerificationCodeInput, setShowVerificationCodeInput] = useState(false);
+    const [username, setUsername] = useState('');
+
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                const user = await Auth.currentAuthenticatedUser();
+                setUsername(user.attributes['custom:username']); 
+            } catch (error) {
+                console.error('Error fetching user info:', error);
+            }
+        };
+        
+        fetchUserInfo();
+    }, []);
 
     const handleChangeEmail = async () => {
         if (newEmail !== confirmEmail) {
-            // Handle email mismatch
             console.log("Emails do not match");
             return;
         }
 
         const { success, error } = await changeEmail(newEmail);
         if (success) {
-            // Show input for verification code
             setShowVerificationCodeInput(true);
         } else {
-            // Failed to change email, handle error
             console.error('Failed to change email:', error);
         }
     };
@@ -35,14 +47,11 @@ const SettingsTab = () => {
     const handleConfirmEmailChange = async () => {
         const { success, error } = await confirmEmailChange(verificationCode);
         if (success) {
-            // Email change confirmed, update UI accordingly
             console.log('Email change confirmed');
-            // Optionally, you can reset the input fields here
             setNewEmail('');
             setConfirmEmail('');
             setShowVerificationCodeInput(false);
         } else {
-            // Failed to confirm email change, handle error
             console.error('Failed to confirm email change:', error);
         }
     };
@@ -53,74 +62,74 @@ const SettingsTab = () => {
                 <Pages />
                 <div className="content">
                     <div className="settingsContent">
-                        <h1>Lotte’s Settings</h1>
+                        <h1>{username}'s Settings</h1>
                         <div className="settingsOptions">
-                            {/* Dropdown for mail settings */}
                             <div className="settingsOption" onClick={() => setShowMailSettings(!showMailSettings)}>
                                 <img src={settingsIcon} alt="Settings Icon" className="icon" />
                                 Change mail settings and frequency
                             </div>
                             {showMailSettings && (
                                 <div className="settingsOption">
-                                    <input 
-                                        type="text" 
-                                        placeholder="Enter new email address" 
-                                        value={newEmail} 
-                                        onChange={(e) => setNewEmail(e.target.value)} 
+                                    <input
+                                        type="text"
+                                        placeholder="Enter new email address"
+                                        value={newEmail}
+                                        onChange={(e) => setNewEmail(e.target.value)}
                                     />
-                                    <input 
-                                        type="text" 
-                                        placeholder="Confirm new email address" 
-                                        value={confirmEmail} 
-                                        onChange={(e) => setConfirmEmail(e.target.value)} 
+                                    <input
+                                        type="text"
+                                        placeholder="Confirm new email address"
+                                        value={confirmEmail}
+                                        onChange={(e) => setConfirmEmail(e.target.value)}
                                     />
                                     <button onClick={handleChangeEmail}>Save</button>
                                 </div>
                             )}
-                            {/* End of mail settings dropdown */}
 
                             {showVerificationCodeInput && (
                                 <div className="settingsOption">
-                                    <input 
-                                        type="text" 
-                                        placeholder="Enter Verification Code" 
-                                        value={verificationCode} 
-                                        onChange={(e) => setVerificationCode(e.target.value)} 
+                                    <input
+                                        type="text"
+                                        placeholder="Enter Verification Code"
+                                        value={verificationCode}
+                                        onChange={(e) => setVerificationCode(e.target.value)}
                                     />
                                     <button onClick={handleConfirmEmailChange}>Confirm</button>
                                 </div>
                             )}
 
-                            <div className="settingsOption" onClick={() => { /* Implement logic */ }}>
+                            <div className="settingsOption" onClick={() => { }}>
                                 <img src={settingsIcon} alt="Globe Icon" className="icon" />
                                 Change region and language
                             </div>
                             <div className="horizontalOptions">
-                                <div className="settingsOption" onClick={() => { /* Implement logic */ }}>
+                                <div className="settingsOption" onClick={() => { }}>
                                     <img src={settingsIcon} alt="Currency Icon" className="icon" />
                                     Change global currency
                                 </div>
-                                <div className="settingsOption" onClick={() => { /* Implement logic */ }}>
+                                <div className="settingsOption" onClick={() => { }}>
                                     <img src={settingsIcon} alt="Profile Private Icon" className="icon" />
                                     Set profile to private
                                 </div>
                             </div>
-                            <div className="settingsOption" onClick={() => { /* Implement logic */ }}>
-                                <img src={settingsIcon} alt="Helpdesk Icon" className="icon" />
-                                Q&A Helpdesk
-                            </div>
+                            <Link to={`/helpdesk`}>
+                                <div className="settingsOption">
+                                    <img src={settingsIcon} alt="Helpdesk Icon" className="icon" />
+                                    Q&A Helpdesk
+                                </div>
+                            </Link>
                         </div>
                         <div className="dataRemovalSection">
                             <h2>Remove your data</h2>
-                            <div className="dataRemovalOption" onClick={() => { /* Implement logic */ }}>
+                            <div className="dataRemovalOption" onClick={() => { }}>
                                 <img src={settingsIcon} alt="Remove Reviews Icon" className="icon" />
                                 Remove your reviews
                             </div>
-                            <div className="dataRemovalOption" onClick={() => { /* Implement logic */ }}>
+                            <div className="dataRemovalOption" onClick={() => { }}>
                                 <img src={settingsIcon} alt="Remove Search Icon" className="icon" />
                                 Remove your search history
                             </div>
-                            <div className="dataRemovalOption" onClick={() => { /* Implement logic */ }}>
+                            <div className="dataRemovalOption" onClick={() => { }}>
                                 <img src={settingsIcon} alt="Delete Account Icon" className="icon" />
                                 Request account deletion
                             </div>
