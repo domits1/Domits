d10b28c0e35b090925c3cc177c245f4b1d939a6a
Danivdmijl <Danivandermijl@yahoo.com>
2024-04-18 11:41:54 +0200

    release.js updated elements

--- web/src/components/about/release.js
+++ web/src/components/about/release.js
@@ -3,25 +3,25 @@ import './release.css'
 
 function Release() {
     return (
-        <div className="release">
-            <div className="releasecontainer">
-                <div className="releaseheader">
+        <main className="release">
+            <article className="releasecontainer">
+                <section className="releaseheader">
                    <h1>Release</h1>
-              </div>
-               <div className="releaseversion">
+              </section>
+               <article className="releaseversion">
                  <h2>Release 1.0.0 - V 22.01.2024</h2>
                  <ul>
                       <li>Delete border strokes and replace with shadows (web and app)</li>
                       <li>Add "Travel Innovation Lab" into guest header (web)</li>
                       <li>Create a declining page after payment failed (web and app)</li>
                  </ul>
-             </div>
-             <div className="releaseversion">
+             </article>
+             <article className="releaseversion">
                   <h2>Release 1.1.0 - V 26.01.2024</h2>
-                </div>
+                </article>
                 <div style={{ height: "500px" }}></div>
-            </div>
-        </div>
+            </article>
+        </main>
 
 
 );
