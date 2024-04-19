22357c23135e9b655c87e89b44c2671abc0efa54
kacperfl <kacperfl29@gmail.com>
2024-04-19 16:14:28 +0200

    remove commands

--- web/cypress/e2e/TestDates.cy.js
+++ web/cypress/e2e/TestDates.cy.js
@@ -1,17 +1,17 @@
 describe('Date Picker Tests', () => {
     beforeEach(() => {
-      cy.visit('https://acceptance.domits.com'); // Gebruikt de verstrekte URL
-      cy.wait(5000); // Geeft de pagina tijd om volledig te laden
+      cy.visit('https://acceptance.domits.com'); 
+      cy.wait(5000); 
     });
   
     it('Allows the user to select check-in and check-out dates', () => {
       cy.get('#checkInPicker').click();
   
-      cy.get('div.react-datepicker__day--015').first().click(); // Adjust class name if necessary
-      cy.get('div.react-datepicker__day--020').first().click(); // Adjust class name if necessary
+      cy.get('div.react-datepicker__day--015').first().click(); 
+      cy.get('div.react-datepicker__day--020').first().click(); 
   
-      cy.get('#checkInPicker').invoke('val').should('contain', '15'); // Adjust format if necessary
-      cy.get('#checkInPicker').invoke('val').should('contain', '20'); // Adjust format if necessary
+      cy.get('#checkInPicker').invoke('val').should('contain', '15'); 
+      cy.get('#checkInPicker').invoke('val').should('contain', '20');
     });
   
   });
