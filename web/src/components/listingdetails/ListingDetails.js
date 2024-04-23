1381fa8aabbf731717e175218968d2fc9a531d78
ChhD <90622630+ChahidLaaouar@users.noreply.github.com>
2024-04-23 13:22:02 +0200

    Listing details page een beetje veranderd qua images

--- web/src/components/listingdetails/ListingDetails.js
+++ web/src/components/listingdetails/ListingDetails.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
 import { Link, useLocation } from "react-router-dom";
 import "./listing.css";
-import ImageGallery from 'react-image-gallery';
-import "react-image-gallery/styles/css/image-gallery.css";
+import ImageGallery from './ImageGallery';
 
 const ListingDetails = () => {
     const { search } = useLocation();
     const searchParams = new URLSearchParams(search);
     const id = searchParams.get('ID');
     const [accommodation, setAccommodation] = useState(null);
-    const [images, setImages] = useState([]);
 
     useEffect(() => {
         const fetchAccommodation = async () => {
@@ -21,7 +18,7 @@ const ListingDetails = () => {
                     headers: {
                         'Content-Type': 'application/json',
                     },
-                    body: JSON.stringify({ ID: id }) // Corrected this line
+                    body: JSON.stringify({ ID: id })
                 });
                 if (!response.ok) {
                     throw new Error('Failed to fetch accommodation data');
@@ -30,19 +27,13 @@ const ListingDetails = () => {
                 const data = JSON.parse(responseData.body)
                 console.log(data)
                 setAccommodation(data);
-
-                const galleryImages = Object.values(data.Images).map(url => ({
-                    original: url,
-                    thumbnail: url
-                }));
-                setImages(galleryImages);
             } catch (error) {
                 console.error('Error fetching accommodation data:', error);
             }
         };
         fetchAccommodation();
     }, [id]);
-    
+
     return (
         <div className="listing-details-container">
             {accommodation && (
@@ -58,11 +49,11 @@ const ListingDetails = () => {
                         </h1>
                     </div>
                     <div className="listing-details-image-window">
-                        <ImageGallery items={images} thumbnailPosition="right" />
+                        <ImageGallery images={Object.values(accommodation.Images)} />
                     </div>
                     <div className="price-and-rooms-row">
                         <p className="price-per-night-text">{`€${accommodation.Rent} per night`}</p>
-                        <p className="guest-and-rooms-text">{`${accommodation.Guests} guests`}</p>
+                        <p className="guest-and-rooms-text">{`${accommodation.GuestAmount} guests`}</p>
                         <p className="guest-and-rooms-text">{`${accommodation.Beds} beds`}</p>
                         <p className="guest-and-rooms-text">{`${accommodation.Bedrooms} bedrooms`}</p>
                         <p className="guest-and-rooms-text">{`${accommodation.Bathrooms} bathrooms`}</p>
