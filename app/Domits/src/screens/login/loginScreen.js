93a756dd765fe0793e91647e314416b62e9d3886
MuhammedC38 <muhammedc38@hotmail.com>
2024-05-01 14:48:39 +0200

    App: backend linked correctly.

--- app/Domits/src/screens/login/loginScreen.js
+++ app/Domits/src/screens/login/loginScreen.js
@@ -9,6 +9,7 @@ import {
 } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import { Auth } from "aws-amplify";
 
 const LoginScreen = () => {
   const navigation = useNavigation();
