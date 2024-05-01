820bd110577cca39ab59e046ff1b9c639aea86f1
SHvanderWilt <sebastiaanvanderwilt@gmail.com>
2024-05-01 16:32:47 +0200

    Finished profilePictureTest.js code (Testing is still needed).

--- web/src/components/guestdashboard/profilePictureTest.js
+++ web/src/components/guestdashboard/profilePictureTest.js
@@ -5,10 +5,40 @@ const S3_BUCKET_NAME = 'cognitodata';
 
 const UserProfile = () => {
     const [profilePicture, setProfilePicture] = useState(null);
-    
+
+    const handleProfilePictureUpload = async (event) => {
+        const file = event.target.files[0];
+        const filename = `${Date.now()}-${file.name}`;
+
+        try {
+            const result = await Storage.put(filename, file, {
+                bucket: S3_BUCKET_NAME,
+                contentType: file.type
+            });
+            const imageUrls = await Storage.get(result.key, { bucket: S3_BUCKET_NAME })
+
+            setProfilePicture(imageUrls);
+        } catch (error) {
+            console.error('Error uploading profile picture:', error)
+        }
+    };
+
     return (
-        <h1>Hello World</h1>
-    )
-}
+        <div>
+            <h1>User profile pictures</h1>
+            {profilePicture &&
+                <img
+                    src={profilePicture}
+                    alt="Profile Picture"
+                    style={{ width: '200px', height: '200px', borderRadius: '50%' }}
+                />}
+            <input
+                type="file"
+                accept="image/*"
+                onChange={handleProfilePictureUpload}
+            />
+        </div>
+    );
+};
 
 export default UserProfile;
