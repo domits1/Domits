3d1d6d0897b2935f39a0a94a448a5a8d341d8d58
SHvanderWilt <sebastiaanvanderwilt@gmail.com>
2024-05-02 17:06:29 +0200

    Profile photos now get stored in S3

--- web/src/components/guestdashboard/profilePictureTest.js
+++ web/src/components/guestdashboard/profilePictureTest.js
@@ -1,44 +1,48 @@
-import React, { useState } from "react";
-import { Storage } from "aws-amplify";
+import React, { useState } from 'react';
+import { Storage } from 'aws-amplify';
 
 const S3_BUCKET_NAME = 'cognitodata';
+const region = 'eu-north-1';
 
 const UserProfile = () => {
-    const [profilePicture, setProfilePicture] = useState(null);
+  const [profilePicture, setProfilePicture] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
 
-    const handleProfilePictureUpload = async (event) => {
-        const file = event.target.files[0];
-        const filename = `${Date.now()}-${file.name}`;
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
 
-        try {
-            const result = await Storage.put(filename, file, {
-                bucket: S3_BUCKET_NAME,
-                contentType: file.type
-            });
-            const imageUrls = await Storage.get(result.key, { bucket: S3_BUCKET_NAME })
+  const uploadFileToS3 = async () => {
+    if (!selectedFile) {
+      alert('Please select a file first.');
+      return;
+    }
 
-            setProfilePicture(imageUrls);
-        } catch (error) {
-            console.error('Error uploading profile picture:', error)
-        }
-    };
+    const filename = `${Date.now()}-${selectedFile.name}`;
 
-    return (
-        <div>
-            <h1>User profile pictures</h1>
-            {profilePicture &&
-                <img
-                    src={profilePicture}
-                    alt="Profile Picture"
-                    style={{ width: '200px', height: '200px', borderRadius: '50%' }}
-                />}
-            <input
-                type="file"
-                accept="image/*"
-                onChange={handleProfilePictureUpload}
-            />
-        </div>
-    );
+    try {
+      await Storage.put(filename, selectedFile, {
+        bucket: S3_BUCKET_NAME,
+        region: region,
+        contentType: selectedFile.type
+      });
+
+      const imageUrl = await Storage.get(filename, { bucket: S3_BUCKET_NAME });
+
+      setProfilePicture(imageUrl);
+    } catch (error) {
+      console.error('Error uploading profile picture:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>User Profile</h1>
+      {profilePicture && <img src={profilePicture} alt="Profile" style={{ width: '200px', height: '200px', borderRadius: '50%' }} />}
+      <input type="file" accept="image/*" onChange={handleFileSelect} />
+      <button onClick={uploadFileToS3}>Upload Profile Picture</button>
+    </div>
+  );
 };
 
 export default UserProfile;
