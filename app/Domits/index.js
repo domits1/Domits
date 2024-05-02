9b3a6a564d536ca1b01f60531d25d811c2d32417
MuhammedC38 <muhammedc38@hotmail.com>
2024-05-02 12:48:06 +0200

    jiaming

--- app/Domits/index.js
+++ app/Domits/index.js
@@ -5,9 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {Amplify, Auth } from 'aws-amplify';
-import awsExports from './src/aws-exports';
  
-Amplify.configure(awsExports);
-
 AppRegistry.registerComponent(appName, () => App);
--- app/Domits/src/screens/login/loginScreen.js
+++ app/Domits/src/screens/login/loginScreen.js
@@ -9,7 +9,6 @@ import {
 } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import { Auth } from "aws-amplify";
 
 const LoginScreen = () => {
   const navigation = useNavigation();
