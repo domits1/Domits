1aa6b7b3fa301b7321eaea47dfd5877492289314
Jiaming <chaosgamingnl33@gmail.com>
2024-05-03 13:59:43 +0200

    Fixed location of the configuration

--- app/Domits/index.js
+++ app/Domits/index.js
@@ -5,5 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
- 
+import {Amplify} from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+
+Amplify.configure(awsconfig);
+
 AppRegistry.registerComponent(appName, () => App);
