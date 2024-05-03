884de7d25a56ea809e280048c6dd43b224961f5c
Jiaming <chaosgamingnl33@gmail.com>
2024-05-03 12:02:14 +0200

    Configuration for amblify

--- app/Domits/App.js
+++ app/Domits/App.js
@@ -1,9 +1,12 @@
 // App.js
 import React from 'react';
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './src/amplifyconfiguration.json';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigation from './src/navigation/appNavigation.js';
 // import Header from './src/header/header.js';
 
+Amplify.configure(amplifyconfig);
 function App() {
   return (
     <NavigationContainer>
