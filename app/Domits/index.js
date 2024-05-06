d27a0adf71bd431330cdb162629da00f4ead46ef
MuhammedC38 <muhammedc38@hotmail.com>
2024-05-06 12:55:18 +0200

    amblify pulled to hopefully correct directory

--- app/Domits/index.js
+++ app/Domits/index.js
@@ -6,7 +6,10 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {Amplify} from 'aws-amplify';
-import awsconfig from './src/aws-exports';
+import awsconfig from './aws-exports';
+import aws from 'aws-sdk';
+
+export const cognitoClient = new aws.CognitoIdentityServiceProvider();
 
 Amplify.configure(awsconfig);
 
