770c17f589d0f19b7ac57cf3d69147e632430a2e
SHvanderWilt <sebastiaanvanderwilt@gmail.com>
2024-05-07 08:46:45 +0200

    Added user id to the profile picture test

--- web/src/components/guestdashboard/profilePictureTest.js
+++ web/src/components/guestdashboard/profilePictureTest.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Storage } from 'aws-amplify';
+import React, { useState, useEffect } from 'react';
+import { Storage, Auth } from 'aws-amplify';
 
 const S3_BUCKET_NAME = 'cognitodata';
 const region = 'eu-north-1';
@@ -7,6 +7,21 @@ const region = 'eu-north-1';
 const UserProfile = () => {
   const [profilePicture, setProfilePicture] = useState(null);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const fetchUserId = async () => {
+      try {
+        const user = await Auth.currentAuthenticatedUser();
+        setUserId(user.attributes.sub);
+        console.log(user.attributes.sub);
+      } catch (error) {
+        console.error('Error fetching user ID:', error);
+      }
+    };
+    
+    fetchUserId();
+  }, []);
 
   const handleFileSelect = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -18,7 +33,7 @@ const UserProfile = () => {
       return;
     }
 
-    const filename = `${Date.now()}-${selectedFile.name}`;
+    const filename = `${userId}/${Date.now()}-${selectedFile.name}`;
 
     try {
       await Storage.put(filename, selectedFile, {
