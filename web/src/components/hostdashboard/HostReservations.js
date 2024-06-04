be7ca2b9994b6833194c17f0ad5a00ff7aec66bd
Jiaming <chaosgamingnl33@gmail.com>
2024-06-04 15:12:25 +0200

    Added Pagination to Reservations

--- web/src/components/hostdashboard/HostReservations.js
+++ web/src/components/hostdashboard/HostReservations.js
@@ -9,12 +9,52 @@ import spinner from "../../images/spinnner.gif";
 const HostReservations = () => {
     const [userId, setUserId] = useState({});
     const [options] = useState(["Booking requests", "Accepted", "Reserved", "Cancelled", "All"]);
-    const [selectedOption, setSelectedOption] = useState(null);
+    const [selectedOption, setSelectedOption] = useState("Booking requests");
     const [reservations, setReservations] = useState([]);
+    const [pendingReservations, setPendingReservations] = useState([]);
+    const [acceptedReservations, setAcceptedReservations] = useState([]);
+    const [reservedReservations, setReservedReservations] = useState([]);
+    const [cancelledReservations, setCancelledReservations] = useState([]);
+    const [reservationDisplay, setReservationDisplay] = useState([]);
     const [selectedReservations, setSelectedReservations] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
+    const pageCount = Math.ceil(reservationDisplay.length / 5);
+    const indexOfLastItem = currentPage * 5;
+    const indexOfFirstItem = indexOfLastItem - 5;
+    const currentItems = reservationDisplay.slice(indexOfFirstItem, indexOfLastItem);
 
-
+    useEffect(() => {
+        if (currentItems.length === 0 && currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    }, [currentItems.length, currentPage]);
+    useEffect(() => {
+        if (reservations) {
+            setPendingReservations(reservations.filter(reservation => reservation.Status === "PENDING"));
+            setAcceptedReservations(reservations.filter(reservation => reservation.Status === "ACCEPTED"));
+            setReservedReservations(reservations.filter(reservation => reservation.Status === "RESERVED"));
+            setCancelledReservations(reservations.filter(reservation => reservation.Status === "CANCELLED"));
+        }
+    }, [reservations]);
+    useEffect(() => {
+        if (selectedOption === "Booking requests") {
+            setReservationDisplay(pendingReservations);
+        }
+        if (selectedOption === "Accepted") {
+            setReservationDisplay(acceptedReservations);
+        }
+        if (selectedOption === "Reserved") {
+            setReservationDisplay(reservedReservations);
+        }
+        if (selectedOption === "Cancelled") {
+            setReservationDisplay(cancelledReservations);
+        }
+        if (selectedOption === "All") {
+            setReservationDisplay(reservations);
+        }
+    }, [selectedOption]);
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
     const handleCheckboxChange = (event, reservation) => {
         const checked = event.target.checked;
         if (checked) {
@@ -110,7 +150,6 @@ const HostReservations = () => {
     useEffect(() => {
         if (userId) {
             fetchReservations();
-            setSelectedOption("Booking requests");
         }
     }, [userId]);
     return (
@@ -144,12 +183,8 @@ const HostReservations = () => {
                                 <img src={spinner} alt='spinner'/>
                             </div>
                             :
-                            reservations.length > 0 ? (
-                                reservations
-                                    .filter(reservation => selectedOption === "Booking requests" ? reservation.Status === "PENDING" :
-                                        selectedOption === "Accepted" ? reservation.Status === "ACCEPTED" :
-                                            selectedOption === "Reserved" ? reservation.Status === "RESERVED" :
-                                                selectedOption === "Cancelled" ? reservation.Status === "CANCELLED" : true)
+                            reservationDisplay.length > 0 ? (
+                                currentItems
                                     .map(reservation => (
                                         <div className="reservation-item" key={reservation.ID}>
                                             {selectedOption === "Booking requests" && (
@@ -163,19 +198,47 @@ const HostReservations = () => {
                                             <p>{reservation.ID}</p>
                                             <p>{reservation.Accommodation.Title}</p>
                                             <p>{DateFormatterDD_MM_YYYY(reservation.StartDate)} - {DateFormatterDD_MM_YYYY(reservation.EndDate)}</p>
+                                            {selectedOption === "All" && (
+                                                <div className="status-display">
+                                                    <p>Status: </p>
+                                                    <p style={{
+                                                        color: reservation.Status === 'ACCEPTED' ? 'green' :
+                                                            reservation.Status === 'CANCELLED' ? 'red' :
+                                                                reservation.Status === 'RESERVED' ? '#003366': 'inherit'
+                                                    }}>{reservation.Status}</p>
+                                                </div>
+                                            )}
                                         </div>
                                     ))
                             ) : (
                                 <p>You do not have any booking requests at the moment...</p>
                             )}
                     </section>
+                    <section className="pagination">
+                        <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>
+                            {'<'}
+                        </button>
+                        {Array.from({length: pageCount}, (_, index) => (
+                            <button key={index + 1} onClick={() => paginate(index + 1)}
+                                    disabled={currentPage === index + 1}>
+                                {index + 1}
+                            </button>
+                        ))}
+                        <button onClick={() => paginate(currentPage + 1)}
+                                disabled={currentPage === pageCount || pageCount === 0}>
+                            {'>'}
+                        </button>
+                    </section>
                     {selectedOption === "Booking requests" && selectedReservations.length > 0 && (
                         <div className="btn-box">
                             <button className="btn-undo" onClick={() => handleUndoSelect()}>Undo select</button>
                             <p className="selected-text">{selectedReservations.length} items selected</p>
                             <div className="btn-group">
-                                <button className="btn-deny" onClick={() => asyncUpdateReservation("CANCELLED")}>Deny</button>
-                                <button className="btn-approve" onClick={() => asyncUpdateReservation("ACCEPTED")}>Approve</button>
+                                <button className="btn-deny" onClick={() => asyncUpdateReservation("CANCELLED")}>Deny
+                                </button>
+                                <button className="btn-approve"
+                                        onClick={() => asyncUpdateReservation("ACCEPTED")}>Approve
+                                </button>
                             </div>
                         </div>
                     )}
