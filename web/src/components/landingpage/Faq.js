0afb19d5750c7602bee6bfe3e71bbd5c8605f23b
Denisa Minu <720645@student.inholland.nl>
2024-06-05 10:05:32 +0200

    faq for guests and for hosts

--- web/src/components/landingpage/Faq.js
+++ web/src/components/landingpage/Faq.js
@@ -1,16 +1,14 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './Faq.css';
-
 
 const FaqItem = ({ question, answer, toggleOpen, isOpen }) => {
   const answerRef = useRef(null);
-
   return (
     <div className="faq-box" onClick={toggleOpen}>
       <div className="question">{question}</div>
-      <div 
-        className="answer" 
-        style={{ maxHeight: isOpen ? `${answerRef.current.scrollHeight}px` : '0', overflow: 'hidden' }} 
+      <div
+        className="answer"
+        style={{ maxHeight: isOpen ? `${answerRef.current.scrollHeight}px` : '0', overflow: 'hidden' }}
         ref={answerRef}
       >
         {answer}
@@ -19,50 +17,99 @@ const FaqItem = ({ question, answer, toggleOpen, isOpen }) => {
   );
 };
 
-const Faq = () => {
-  const [faqs, setFaqs] = useState({
-    aboutDomits: [
-    {
-      question: "Is my information stored securely?",
-      answer: "Absolutely, we prioritize your privacy and security above all else. We implement advanced encryption and industry-standard data protection protocols to secure your personal information. Our systems are regularly updated and tested against security threats. Moreover, access to your personal information is strictly limited to authorized personnel who are required to keep it confidential. If you have any specific concerns or questions about your data security, please do not hesitate to contact us.",
-      isOpen: false
-    },
-    {
-      question: "How much does Domits charge?",
-      answer: "Domits charges a 15% fee for each booking. This fee is automatically calculated and applied to your total reservation cost, ensuring a transparent and straightforward booking process. If you have further questions about fees or any other aspect of our service, please don't hesitate to reach out.",
-      isOpen: false
-    }
-  ],
-  payment: [
-    {
-      question: "When can I expect payment after my guest's check-in, and how does the timing vary based on my chosen payment method?",
-      answer: "You will receive payment for your booking shortly after your guest's check-in date. The exact timing can vary depending on the payment method you've selected in your account preferences, but typically payments are processed within 24 hours of the guest's arrival. It's our priority to make sure you receive your funds promptly. If you require more detailed information or have specific payment inquiries, our customer service team is always available to assist you.",
-      isOpen: false
-    }
-  ],
-  hosting: [
-    {
-      question: "How long will my listing stay active on your platform, and what criteria would cause it to be removed?",
-      answer: "Your listing remains active on our platform until you decide to remove it or if it does not comply with our hosting standards and policies. We encourage you to keep your calendar and listing details up-to-date to maximize your booking potential.",
-      isOpen: false
-    },
-    {
-      question: "What steps are required to become a verified Domits host, and what criteria does my property need to meet for verification?",
-      answer: "To become a verified Domits host, you need to complete a verification process, which includes providing a valid government-issued ID and additional property information. Our team will review your submission and may conduct a property inspection to ensure it meets our standards for safety, comfort, and hospitality.",
-      isOpen: false
-    },
-    {
-      question: "What kind of support and resources do you provide to enhance the visibility and appeal of my property if it's not getting booked?",
-      answer: "If your property is not getting booked, consider optimizing your listing. This can include updating your photos, adjusting your pricing, enhancing your property description, and ensuring your availability is current. We also offer resources and support to help increase your property's visibility and appeal to potential guests.",
-      isOpen: false
-    }
-  ],
-});
+const Faq = ({ category }) => {
+  const faqData = {
+    guest: {
+      aboutDomits: [
+        {
+          question: "Is my information stored securely?",
+          answer: "Absolutely, we prioritize your privacy and security above all else...",
+          isOpen: false
+        },
+        {
+          question: "How much does Domits charge?",
+          answer: "Domits charges a 15% fee for each booking...",
+          isOpen: false
+        }
+      ],
 
-  const toggleOpen = (category, index) => {
+      searchAndBook: [
+        {
+          question: "How to search and book?",
+          answer: "To search and book, enter your destination and dates in the search bar on the homepage, browse the listings, and follow the booking process for your chosen accommodation.",
+          isOpen: false
+        }
+      ],
+
+      manageAccount: [
+        {
+          question: "How do I create and manage my account?",
+          answer: "To create and manage your account, click 'Sign Up' or 'Log In' on the website, follow the prompts to enter your details, and use the account settings to manage your information.",
+          isOpen: false
+        }
+      ]
+    },
+
+    host: {
+      aboutDomits: [
+        {
+          question: "Is my information stored securely?",
+          answer: "Absolutely, we prioritize your privacy and security above all else...",
+          isOpen: false
+        },
+        {
+          question: "How much does Domits charge?",
+          answer: "Domits charges a 15% fee for each booking...",
+          isOpen: false
+        }
+      ],
+
+      hosting: [
+        {
+          question: "How long will my listing stay active on your platform...",
+          answer: "Your listing remains active on our platform until you decide to remove it...",
+          isOpen: false
+        },
+        {
+          question: "What steps are required to become a verified Domits host...",
+          answer: "To become a verified Domits host, you need to complete a verification process...",
+          isOpen: false
+        },
+        {
+          question: "What kind of support and resources do you provide...",
+          answer: "If your property is not getting booked, consider optimizing your listing...",
+          isOpen: false
+        }
+      ],
+
+      payment: [
+        {
+          question: "When can I expect payment after my guest's check-in...",
+          answer: "You will receive payment for your booking shortly after your guest's check-in date...",
+          isOpen: false
+        }
+      ],
+
+      manageReservations: [
+        {
+          question: "How to see, change or cancel your reservations?",
+          answer: "Log in to your account, navigate to 'Booking' and select the reservation you want to view, change, or cancel.",
+          isOpen: false
+        }
+      ]
+    }
+  };
+
+  const [faqs, setFaqs] = useState(faqData[category] || {});
+
+  useEffect(() => {
+    setFaqs(faqData[category] || {});
+  }, [category]);
+
+  const toggleOpen = (subcategory, index) => {
     setFaqs(currentFaqs => ({
       ...currentFaqs,
-      [category]: currentFaqs[category].map((faq, i) => (
+      [subcategory]: currentFaqs[subcategory].map((faq, i) => (
         i === index ? { ...faq, isOpen: !faq.isOpen } : faq
       ))
     }));
@@ -71,45 +118,92 @@ const Faq = () => {
   return (
     <div className="faq-container">
       <h3 className="faqHeader3">FAQ - Frequently Asked Questions</h3>
-      
-      <div className='faq-category'>
-        <h4 className="faqHeader4">About Domits</h4>
-        {faqs.aboutDomits.map((faq, index) => (
-          <FaqItem
-           key={index}
-           question={faq.question}
-           answer={faq.answer}
-           isOpen={faq.isOpen}
-           toggleOpen={() => toggleOpen('aboutDomits', index)} 
-          />
-        ))}
-      </div>
 
-      <div className='faq-cateogry'>
-        <h4 className="faqHeader4">Payment</h4>
-        {faqs.payment.map((faq, index) => (
-          <FaqItem
-           key={index}
-           question={faq.question}
-           answer={faq.answer}
-           isOpen={faq.isOpen}
-           toggleOpen={() => toggleOpen('payment', index)}
-          />
-        ))}
-      </div>
+      {category === 'guest' && (
+        <>
+          <div className="faq-category">
+            <h4 className="faqHeader4">About Domits</h4>
+            {faqs.aboutDomits?.map((faq, index) => (
+              <FaqItem
+                key={index}
+                question={faq.question}
+                answer={faq.answer}
+                isOpen={faq.isOpen}
+                toggleOpen={() => toggleOpen('aboutDomits', index)}
+              />
+            ))}
+          </div>
 
-      <div className='faq-category'>
-        <h4 className="faqHeader4">Hosting</h4>
-        {faqs.hosting.map((faq, index) => (
-          <FaqItem
-           key={index}
-           question={faq.question}
-           answer={faq.answer}
-           isOpen={faq.isOpen}
-           toggleOpen={() => toggleOpen('hosting', index)}
-          />
-      ))}
-      </div>
+          <div className="faq-category">
+            <h4 className="faqHeader4">Search And Book</h4>
+            {faqs.searchAndBook?.map((faq, index) => (
+              <FaqItem
+                key={index}
+                question={faq.question}
+                answer={faq.answer}
+                isOpen={faq.isOpen}
+                toggleOpen={() => toggleOpen('searchAndBook', index)}
+              />
+            ))}
+          </div>
+
+          <div className="faq-category">
+            <h4 className="faqHeader4">Manage your account</h4>
+            {faqs.manageAccount?.map((faq, index) => (
+              <FaqItem
+                key={index}
+                question={faq.question}
+                answer={faq.answer}
+                isOpen={faq.isOpen}
+                toggleOpen={() => toggleOpen('manageAccount', index)}
+              />
+            ))}
+          </div>
+        </>
+      )}
+
+      {category === 'host' && (
+        <>
+          <div className="faq-category">
+            <h4 className="faqHeader4">Hosting</h4>
+              {faqs.hosting?.map((faq, index) => (
+                <FaqItem
+                  key={index}
+                  question={faq.question}
+                  answer={faq.answer}
+                  isOpen={faq.isOpen}
+                  toggleOpen={() => toggleOpen('hosting', index)} 
+                />
+              ))}
+         </div>
+        
+        <div className="faq-category">
+            <h4 className="faqHeader4">Payment</h4>
+              {faqs.payment?.map((faq, index) => (
+                <FaqItem
+                  key={index}
+                  question={faq.question}
+                  answer={faq.answer}
+                  isOpen={faq.isOpen}
+                  toggleOpen={() => toggleOpen('payment', index)}
+                />
+              ))}
+        </div>
+
+        <div className="faq-category">
+            <h4 className="faqHeader4">Manage Reservations</h4>
+              {faqs.manageReservations?.map((faq, index) => (
+                <FaqItem
+                  key={index}
+                  question={faq.question}
+                  answer={faq.answer}
+                  isOpen={faq.isOpen}
+                  toggleOpen={() => toggleOpen('manageReservations', index)}
+                />
+              ))}
+        </div>
+        </>
+      )}
     </div>
   );
 };
