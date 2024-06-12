d83ee616f71048cd21f05197539943e787afc4cb
ChhD <90622630+ChahidLaaouar@users.noreply.github.com>
2024-06-12 17:31:31 +0200

    Stripe live

--- web/src/components/booking/BookingOverview.js
+++ web/src/components/booking/BookingOverview.js
@@ -6,7 +6,7 @@ import { loadStripe } from '@stripe/stripe-js';
 import "./bookingoverview.css";
 import Register from "../base/Register";
 
-const stripePromise = loadStripe('pk_test_51OAG6OGiInrsWMEcRkwvuQw92Pnmjz9XIGeJf97hnA3Jk551czhUgQPoNwiCJKLnf05K6N2ZYKlXyr4p4qL8dXvk00sxduWZd3');
+const stripePromise = loadStripe('pk_live_51OAG6OGiInrsWMEcQy4ohaAZyT7tEMSEs23llcw2kr2XHdAWVcB6Tm8F71wsG8rB0AHgh4SJDkyBymhi82WABR6j00zJtMkpZ1');
 
 
 const BookingOverview = () => {
