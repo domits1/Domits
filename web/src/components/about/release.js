8c4fdad51b04df6cef71f75ebff315966c9b02fc
Denisa Minu <minudenisa29@gmail.com>
2024-06-21 09:24:42 +0200

    improve contact page

--- web/src/components/about/release.js
+++ web/src/components/about/release.js
@@ -1,10 +1,16 @@
 import React from "react";
 import './release.css'
+import release from '../../images/release.png';
 
 function Release() {
     return (
         <div className="release">
-         <h1>Coming soon..</h1>
+            <div className="release-header">
+              <h1>What’s New</h1> 
+              <h4>Innovative Vacation Rentals.</h4>
+              <h4>Redesigned for You.</h4>
+              <img src={release} alt="Release" />
+            </div>
         </div>
 
 
