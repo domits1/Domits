f42631057a955c8ace321d935a790a69c5a87584
2174951 <131653171+2174951@users.noreply.github.com>
2024-09-06 16:26:49 +0200

    TestHeader.cy.js
    nog niet af

---
+++ web/cypress/e2e/TestHeader.cy.js
@@ -0,0 +1,25 @@
+// /// <reference types="Cypress" />
+// import '../support/commands';
+
+// describe('Header logo button test', () => {
+//     it('should go to home page if clicked on logo', () => {
+//         cy.loginAsGuest(); 
+//         cy.get('.header-nav > .logo').click()
+//     });
+//   });
+
+// describe('Header search bar test' , () => {
+//     it('should be able to type and search', () => {
+//         cy.loginAsGuest();
+//         cy.get('.searchBar_inputfield').click().type('Stockholm');
+//         cy.get('.searchbar-button').click('');
+//         cy.get('#card-visibility > .accocard')
+//         .should('have.length', '1')
+//     });
+// });
+
+// // describe('Header accommodation button test' , () => {
+// //     it('Accommodation button works')
+// // })
+
+
