7dac97f5135b5f0a23cc3cf040b5f0b9df73e501
Timmhart <tim.hart@hva.nl>
2024-09-21 17:24:02 +0200

    Job detail page revamp. Not finished yet.

--- web/src/components/careers/jobDetails.js
+++ web/src/components/careers/jobDetails.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Jobs from "./jobs.json";
 import { useNavigate, useParams } from 'react-router-dom';
 import "./jobDetails.css";
+import office from '../../images/office.jpg';
+import coWork from '../../images/co-work.jpg';
 
 function JobDetails() {
     const navigate = useNavigate();
@@ -16,59 +18,74 @@ function JobDetails() {
 
     return(
         <div className="job-detail-container">
-            <h2><strong>{job.title}</strong></h2><br></br>
-            <p>{job.details.jobDescription}</p><br></br>
-            <h3>Experience</h3> 
-            <p>{job.experience}</p><br></br> 
-            <h3>Category</h3>
-            <p>{job.category}</p><br></br> 
-            <h3>Experience</h3> 
-            <p>{job.experience}</p><br></br> 
-            <h3>Location</h3>
-            <p>{job.location}</p><br></br> 
-           {/* <p>Description</p><br></br>
-            <p>{job.description}</p><br></br>*/}
-            <h3>What you'll be doing</h3>
-            <p>{job.details.responsibilities}</p><br></br>
+            <section class="hero">
+                <div class="left-container">
+                    <h1>{job.title}</h1>
+                    <span class="company-name">At Domits</span>
+                    <p>{job.details.jobDescription}</p>
+                    <div class="job-tags">
+                        <div class="tag">{job.experience}</div>
+                        <div class="tag">{job.category}</div>
+                        <div class="tag">{job.model}</div>
+                        <div class="tag">{job.location}</div>
+                    </div>
+                </div>
+                <div class="right-container">
+                    <img src={office} alt="Office Image"/>
+                </div>
+            </section>
 
-            <h3>Technologies we work with</h3>
-            <p>{job.details.technologies}</p>
-              {/* <ul>
-                {job.details.technologies.map((tech, index) => (
-                    <li key={index}>- {tech}</li>
-                ))}
-              </ul> */}
+            <section className="responsibilities">
+                <div className="resp-container">
+                    <img src={coWork} alt="" />
+                    <div className="resp-right">
+                        <h2 className="resp-heading">What you'll be doing</h2>
+                        <p>{job.details.responsibilities}</p>
+                    </div>
+                </div>
+            </section>
 
-            <h3>Bonus if you have experience with</h3>
-            <ul>
-                {job.details.preferredExperience.map((exp, index) => (
-                    <li key={index}>- {exp}</li>
-                ))}
-            </ul><br></br>
+            <section className="technologies">
+                <h2 className="tech-heading">Technologies we work with</h2>
+                <p>{job.details.responsibilities}</p>
+            </section>
 
-            <h3>What we offer</h3>
-            <p>{job.details.offerings}</p>
-            {/* <ul>
-              {job.details.offerings.map((offering, index) => (
-                <li key={index}>- {offering}</li>
-              ))}
-            </ul> */}
+            <section className="experience">
+                <div className="exp-container-top">
+                    <h2 className="exp-heading">Bonus if you have experience with</h2>
+                    <ul>
+                        {job.details.preferredExperience.map((exp, index) => (
+                            <li key={index}>- {exp}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="exp-container-bottom">
+                    <h2 className="exp-heading">What we ask</h2>
+                    <ul>
+                        {job.details.requirements.map((requirement, index) => (
+                        <li key={index}>- {requirement}</li>
+                        ))}
+                    </ul>
+                </div>
+            </section>
 
-            <h3>What we ask</h3>
-            <ul>
-                {job.details.requirements.map((requirement, index) => (
-                    <li key={index}>- {requirement}</li>
-                ))}
-            </ul>
-            
-            <h3>Apply now</h3>
-            <p>{job.details.apply}</p><br></br>
-            <h3>Extra information</h3>
+            <section className="technologies">
+                <h2 className="tech-heading">What we offer</h2>
+                <p>{job.details.offerings}</p>
+            </section>
+
+           <section className="apply">
+
+           </section>
+           <h3>Extra information</h3>
             <ul>
                 {job.details.extraInformation.map((extra, index) => (
                     <li key={index}>- {extra}</li>
                 ))}
             </ul>
+            <h3>Apply now</h3>
+            <p>{job.details.apply}</p><br></br>
+            
 
             <button className="apply-button" onClick={navigateToContact}>Apply</button>
         </div>
