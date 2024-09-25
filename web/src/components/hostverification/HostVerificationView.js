d5858534eb1b181841a485b1df2ac28ea7e51a90
Mohamed Dinle <113024715+mdinle@users.noreply.github.com>
2024-09-25 14:59:40 +0200

    Check if user is required to submit registration key

--- web/src/components/hostverification/HostVerificationView.js
+++ web/src/components/hostverification/HostVerificationView.js
@@ -1,26 +1,33 @@
-import styles from './hostverification.module.css';
-import Option from './components/Option';
-import Listing from './components/Listing';
+import styles from "./hostverification.module.css";
+import Option from "./components/Option";
+import Listing from "./components/Listing";
+import useOptionVisibility from "./hooks/useOptionVisibility";
 
 const HostVerificationView = () => {
+  const { isRegistrationNumberRequired, city, loading, error } = useOptionVisibility("e0707e21-a0e4-4a4f-a2b8-0b67135dd17f");
+
   return (
-    <main className={styles['main-container']}>
-        <div className={styles['left-container']}>
-          <h1>What is the next step?</h1>
-          <Option option="Meet local requirements" />
-          <hr></hr>
-          <Option option="Verify your identity"/>
-          <hr></hr>
-          <Option option="Connect with stripe to receive payments"/>
-          <hr></hr>
-          <Option option="Confirm your phone number"/>
-        </div>
-        <div className={styles['right-container']}>
-          <Listing />
-        </div>
-      <div className={styles['bottom-container']}>
-      <hr></hr>
-        <button className={styles['publish-btn']}>Publish Listing</button>
+    <main className={styles["main-container"]}>
+      <div className={styles["left-container"]}>
+        <h1>What is the next step?</h1>
+        {isRegistrationNumberRequired && (
+          <>
+            <Option option="Meet local requirements" />
+            <hr />
+          </>
+        )}
+        <Option option="Verify your identity" />
+        <hr></hr>
+        <Option option="Connect with stripe to receive payments" />
+        <hr></hr>
+        <Option option="Confirm your phone number" />
+      </div>
+      <div className={styles["right-container"]}>
+        <Listing />
+      </div>
+      <div className={styles["bottom-container"]}>
+        <hr></hr>
+        <button className={styles["publish-btn"]}>Publish Listing</button>
       </div>
     </main>
   );
