0cae5adf93e685bec282823f7ee59a23a4883d38
Chanlyj <justin.chan2@hva.nl>
2024-10-14 16:48:15 +0200

    Fixed a small error that checks the wrong url

--- web/cypress/e2e/TestHostDistribution.cy.js
+++ web/cypress/e2e/TestHostDistribution.cy.js
@@ -3,13 +3,9 @@ import '../support/commands';
 beforeEach(() => {
     cy.viewport(1920, 1080);
     cy.loginAsHost();
-
-    cy.get('.HostDashboard_dashboardHost__Y2joW').should('be.visible');
     cy.get('.hostchatbot-toggle-button').click();
-
-    cy.visit('https://acceptance.Domits.com/hostdashboard/distribution');
-    cy.url().should('eq', 'https://acceptance.Domits.com/hostdashboard/distribution');
-
+    cy.visit('https://acceptance.domits.com/hostdashboard/distribution');
+    cy.url().should('eq', 'https://acceptance.domits.com/hostdashboard/distribution');
 });
 
 describe.skip('Visit Web Host Distribution', () => {
