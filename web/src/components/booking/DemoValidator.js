9cfd9e05b116250b57167dc2a4c08dcf9dda3b78
Mohamed Dinle <113024715+mdinle@users.noreply.github.com>
2024-10-15 12:03:28 +0200

    console.log remove

--- web/src/components/booking/DemoValidator.js
+++ web/src/components/booking/DemoValidator.js
@@ -3,10 +3,7 @@ import React, { useEffect, useState } from 'react';
 function DemoValidator(hostId) {
     const [isDemo, setIsDemo] = useState(false);
     const demoOwnerId = process.env.REACT_APP_DEMO_OWNER_ID;
-
-    console.log('hostId', hostId);
-    console.log('demoOwnerId', demoOwnerId);
-
+    
     useEffect(() => {
         if (hostId === demoOwnerId) {
             setIsDemo(true);
