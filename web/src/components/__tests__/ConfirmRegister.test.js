97edd0fc5de044e6a174317f9574dc865c306377
chantbalci <chantbalci@live.com>
2024-10-18 10:13:34 +0200

    jest test wrong refrence import

--- web/src/components/__tests__/ConfirmRegister.test.js
+++ web/src/components/__tests__/ConfirmRegister.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render, screen, fireEvent, waitFor, act } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { Auth } from 'aws-amplify';
-import ConfirmEmail from '../base/ConfirmRegister.js';
 import FlowContext from '../../FlowContext';
 
 // Mock the Auth module from AWS Amplify
