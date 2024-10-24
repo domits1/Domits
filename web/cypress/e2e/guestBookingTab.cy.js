9b2d5c18fdcde772ab2c93adcd7bd95359afcea7
Muubii <siu7279@gmail.com>
2024-10-24 11:07:37 +0200

    Mocking api booking
    Mocking api with json data
    and showing there
    
    Test done ^^

--- web/cypress/e2e/guestBookingTab.cy.js
+++ web/cypress/e2e/guestBookingTab.cy.js
@@ -1,30 +1,51 @@
-import '../support/commands'
-
 describe('Landing Page Tests', () => {
     beforeEach(() => {
+        cy.clearLocalStorage();
+        cy.clearCookies();
+
+        cy.fixture('booking.json').then((bookings) => {
+            cy.intercept('POST', 'https://j1ids2iygi.execute-api.eu-north-1.amazonaws.com/default/FetchGuestPayments', {
+                statusCode: 200,
+                body: bookings,
+                headers: {
+                    'cache-control': 'no-store'
+                }
+            }).as('fetchBookings');
+        });
+
+        cy.window().then((win) => {
+            win.Auth = {
+                currentUserInfo: cy.stub().resolves({
+                    attributes: {
+                        sub: '12345',
+                    },
+                }),
+            };
+        });
+
         cy.viewport(1920, 1080);
-    });
-  
-    it('Should display the landing page and load key sections', () => {
         cy.loginAsGuest();
         cy.get('.header-links > .headerHostButton').click();
         cy.get('.dashboardSections > :nth-child(2)').click();
-        cy.wait(500); 
+        cy.wait(500);
+    });
 
-            cy.intercept('POST', 'https://j1ids2iygi.execute-api.eu-north-1.amazonaws.com/default/FetchGuestPayments', {
-                fixture: 'bookings.json' 
-            }).as('fetchBookings');
+    it('Should display the landing page and load key sections', () => {
+        cy.contains('Booking').should('be.visible');
+    });
 
-            cy.window().then((win) => {
-                win.Auth = {
-                currentUserInfo: cy.stub().resolves({
-                    attributes: {
-                    sub: '12345'
-                    }
-                })
-                };
-            });
+    it('Should display bookings after data is fetched', () => {
+        cy.wait('@fetchBookings').then((interception) => {
+            cy.log(JSON.stringify(interception.response.body, null, 2));
+        });
+
+        cy.get('.bookings-table').should('be.visible');
+        cy.get('.bookings-table tbody tr').should('have.length.at.least', 2);
+    });
+
+    it('Should redirect to listing details on booking click', () => {
+        cy.wait('@fetchBookings');
+        cy.get('.bookings-table tbody tr').first().click();
+        cy.url().should('include', '/listingdetails?ID=');
     });
 });
-
-
