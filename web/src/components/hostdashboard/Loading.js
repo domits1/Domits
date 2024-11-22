1fad1265241ff17353a86aa1e5d5d0a0a9450027
RoozbehKouchaki <126156782+RoozbehKouchaki@users.noreply.github.com>
2024-11-22 10:24:19 +0100

    New Features ( Ocuppancy rate and ADR )

---
+++ web/src/components/hostdashboard/Loading.js
@@ -0,0 +1,14 @@
+// Loading.jsx
+import React from 'react';
+import './Loading.css';
+
+const Loading = () => {
+    return (
+        <div className="loading-container">
+            <div className="loader"></div>
+            <p>Fetching data, please wait...</p>
+        </div>
+    );
+};
+
+export default Loading;
