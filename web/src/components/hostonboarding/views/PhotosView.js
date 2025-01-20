b6a2a3f6ff2f47b788a9c38fc1a85a4240743171
Muubii <siu7279@gmail.com>
2025-01-20 16:08:24 +0100

    FIX BETTER VER IMAGES HOSTONBOARDING

--- web/src/components/hostonboarding/views/PhotosView.js
+++ web/src/components/hostonboarding/views/PhotosView.js
@@ -1,31 +1,46 @@
 import { useParams } from "react-router-dom";
 import ImagePreview from "../components/ImagePreview";
-import { usePhotos } from "../hooks/usePhotos";
+import usePhotos from "./usePhotos";
 import Button from "../components/button";
+import { useState } from "react";
+import './PhotoVieuw.css';
 
 function PhotosView() {
   const { type: accommodationType } = useParams();
-  const { images, handleFileChange, deleteImage } = usePhotos();
+  const { images, handleFileChange, deleteImage, reorderImages } = usePhotos();
+  const [draggedIndex, setDraggedIndex] = useState(null);
+
+  const handleDragStart = (index) => {
+    setDraggedIndex(index);
+  };
+
+  const handleDrop = (index) => {
+    if (draggedIndex !== null && draggedIndex !== index) {
+      reorderImages(draggedIndex, index);
+    }
+    setDraggedIndex(null);
+  };
 
   return (
-    <main className="container">
-      <h2 className="onboardingSectionTitle">
-        Add photos of your accommodation
-      </h2>
-      <section className="accommodation-photos">
-        <section className="accommodation-images">
+    <main className="photo-gallery-container">
+      <h2 className="photo-gallery-title">Choose at least 5 photos</h2>
+      <section className="photo-gallery-section">
+        <section className="photo-gallery-images">
           {[...Array(5)].map((_, index) => (
             <ImagePreview
               key={index}
-              image={images[`image${index + 1}`]}
+              image={images[`image${index + 1}`] || null}
               index={index}
               onFileChange={handleFileChange}
               onDelete={deleteImage}
+              onDragStart={handleDragStart}
+              onDrop={handleDrop}
+              onDragOver={(e) => e.preventDefault()}
             />
           ))}
         </section>
       </section>
-      <nav className="onboarding-button-box">
+      <nav className="photo-gallery-navigation">
         <Button
           routePath={`/hostonboarding/${accommodationType}/rules`}
           btnText="Go back"
