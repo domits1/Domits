1a82deb757669dd67323cc5cb90a92f96ae5b4f3
Mohamed Dinle <113024715+mdinle@users.noreply.github.com>
2025-01-28 12:59:48 +0100

    fix

---
+++ web/src/features/hostdashboard/useSetLiveEligibility.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { getVerificationStatusFromDB } from "../verification/hostverification/services/verificationServices";
+
+export const useSetLiveEligibility = ({ userId }) => {
+  const [liveEligibility, setLiveEligibility] = useState(false);
+  const [liveEligibilityError, setLiveEligibilityError] = useState("");
+  const [liveEligibilityLoading, setLiveEligibilityLoading] = useState(false);
+
+  const fetchVerificationStatus = async () => {
+    setLiveEligibilityLoading(true);
+    try {
+      const status = await getVerificationStatusFromDB(userId);
+      const isVerified = status.verificationStatus === "verified";
+      setLiveEligibility(isVerified);
+    } catch (error) {
+        if (error.statusCode === 404) {
+            setLiveEligibility(false);
+          } else {
+            setLiveEligibilityError(error.message);
+            console.error("Error fetching verification status:", error);
+          }
+    } finally {
+      setLiveEligibilityLoading(false);
+    }
+  };
+  return {
+    liveEligibility,
+    liveEligibilityError,
+    liveEligibilityLoading,
+    fetchVerificationStatus,
+  };
+};
