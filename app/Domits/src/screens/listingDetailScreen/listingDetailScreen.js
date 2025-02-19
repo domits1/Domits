80b76b181824c1e88da341fc93475c8dd8c094de
Stephanie Lahmann <stephanie.lahmann@hva.nl>
2025-02-19 16:59:47 +0100

    App detail screen folder refactor

--- app/Domits/src/screens/listingDetailScreen/listingDetailScreen.js
+++ app/Domits/src/screens/listingDetailScreen/listingDetailScreen.js
@@ -3,21 +3,21 @@ import {
     ActivityIndicator,
     Dimensions,
     Image,
-    Modal,
     ScrollView,
-    StyleSheet,
     Text,
     ToastAndroid,
     TouchableOpacity,
     View,
 } from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import featureIcons from '../ui-components/FeatureIcons';
-import FetchOwnerData from '../features/search/FetchOwnerData';
-import FetchAccommodation from '../features/search/FetchAccommodation';
+import featureIcons from '../../ui-components/FeatureIcons';
+import FetchOwnerData from '../../features/search/FetchOwnerData';
+import FetchAccommodation from '../../features/search/FetchAccommodation';
 import Ionicons from "react-native-vector-icons/Ionicons";
+import {styles} from "./styles/listingDetailStyles";
+import AmenitiesPopup from "./views/AmenitiesPopup"
 
-const Detailpage = ({route, navigation}) => {
+const ListingDetailScreen = ({route, navigation}) => {
     const accommodationId = route.params.accommodation.id;
     const [parsedAccommodation, setParsedAccommodation] = useState({});
     const [owner, setOwner] = useState();
@@ -116,52 +116,6 @@ const Detailpage = ({route, navigation}) => {
         });
     };
 
-    const FeaturePopup = ({features, onClose}) => {
-        return (
-            <Modal transparent={true} visible={true} animationType="slide">
-                <View style={styles.modalOverlay}>
-                    <View style={styles.modalContent}>
-                        <TouchableOpacity onPress={onClose} style={styles.closeButton}>
-                            <Text style={styles.closeButtonText}>✖</Text>
-                        </TouchableOpacity>
-                        <Text style={styles.modalTitle}>
-                            What does this place have to offer?
-                        </Text>
-                        <ScrollView>
-                            {Object.keys(features).map(category => {
-                                const categoryItems = features[category];
-                                if (categoryItems.length > 0) {
-                                    return (
-                                        <View key={category}>
-                                            <Text style={styles.categorySubTitle}>{category}</Text>
-                                            <View style={styles.subCategoryDivider}/>
-                                            {categoryItems.map((item, index) => (
-                                                <View key={index} style={styles.categoryItem}>
-                                                    {typeof featureIcons[item] === 'string' ? (
-                                                        <Image
-                                                            source={{uri: featureIcons[item]}}
-                                                            style={styles.featureIcon}
-                                                        />
-                                                    ) : featureIcons[item] ? (
-                                                        <View style={styles.featureIcon}>
-                                                            {featureIcons[item]}
-                                                        </View>
-                                                    ) : null}
-                                                    <Text style={styles.featureText}>{item}</Text>
-                                                </View>
-                                            ))}
-                                        </View>
-                                    );
-                                }
-                                return null;
-                            })}
-                        </ScrollView>
-                    </View>
-                </View>
-            </Modal>
-        );
-    };
-
     const renderDateRange = () => {
         const dateRanges = parsedAccommodation.DateRanges || [];
 
@@ -258,7 +212,7 @@ const Detailpage = ({route, navigation}) => {
                             <Text style={styles.subtitleText}>
                                 {parsedAccommodation.Subtitle.trim()}
                             </Text>
-                            <Text style={styles.costPerNightText}>€{parsedAccommodation.Rent.toFixed(2)} per
+                            <Text style={styles.costPerNightText}>€{Number(parsedAccommodation.Rent).toFixed(2)} per
                                 night</Text>
                             <Text>{renderDateRange()}</Text>
                         </View>
@@ -325,7 +279,7 @@ const Detailpage = ({route, navigation}) => {
                                 <Text style={styles.ShowAllAmenitiesButtonText}>Show all amenities</Text>
                             </TouchableOpacity>
                             {showModal && (
-                                <FeaturePopup
+                                <AmenitiesPopup
                                     features={parsedAccommodation.Features}
                                     onClose={toggleModal}
                                 />
@@ -349,250 +303,5 @@ const Detailpage = ({route, navigation}) => {
         </SafeAreaView>
     );
 };
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-    },
-    // General
-    categoryTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: 'black',
-        marginBottom: 5,
-    },
-    categorySubTitle: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        color: 'black',
-        marginTop: 5,
-    },
-    categoryDivider: {
-        height: 1,
-        backgroundColor: 'black',
-        marginVertical: 20,
-        marginHorizontal: 30,
-        alignSelf: 'stretch',
-    },
-    subCategoryDivider: {
-        height: 1,
-        backgroundColor: 'lightgray',
-        marginVertical: 5,
-    },
-    // Header
-    header: {
-        flexDirection: 'row',
-        backgroundColor: 'white',
-        padding: 20,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-    },
-    headerIcon: {
-        color: 'black',
-        marginHorizontal: 10,
-    },
-    titleText: {
-        color: 'black',
-        fontSize: 16,
-        textAlign: 'center',
-        marginBottom: 5,
-        fontFamily: 'MotivaSansBold.woff',
-    },
-    // Loading screen
-    loaderContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    // Main content
-    mainContentContainer: {
-        marginTop: 10,
-        marginHorizontal: 15,
-    },
-    // Accommodation image
-    imageContainer: {
-        flexDirection: 'row',
-    },
-    image: {
-        height: 250,
-    },
-    // Main information
-    mainInfoContainer: {
-        marginBottom: 10,
-    },
-    subtitleText: {
-        color: 'black',
-        fontSize: 16,
-        fontWeight: 'bold',
-        fontFamily: 'MotivaSansRegular.woff',
-    },
-    costPerNightText: {
-        fontWeight: "bold",
-    },
-    // Main amenities
-    borderContainer: {
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        gap: 10,
-    },
-    mainAmenityContainer: {
-        padding: 5,
-        borderWidth: 1,
-        borderColor: '#f0f0f0',
-        borderRadius: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    mainAmenitiesText: {
-        color: 'black',
-        fontSize: 12,
-        textAlign: 'center',
-        fontFamily: 'MotivaSansRegular.woff',
-    },
-    // Booking button
-    bookingButtonContainer: {
-        marginHorizontal: 20,
-    },
-    bookingButton: {
-        borderRadius: 15,
-        padding: 8,
-        backgroundColor: 'green',
-        alignSelf: 'flex-end',
-    },
-    bookingButtonContent: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    bookingButtonText: {
-        color: 'white',
-        fontSize: 20,
-        textAlign: 'center',
-        marginHorizontal: 5,
-        fontFamily: 'MotivaSansRegular.woff',
-    },
-    bookingButtonIcon: {
-        color: 'white',
-    },
-    // Accommodation description
-    descriptionContainer: {
-        marginHorizontal: 10,
-        marginVertical: 10,
-    },
-    descriptionText: {
-        color: 'black',
-        fontSize: 14,
-        fontFamily: 'MotivaSansRegular.woff',
-    },
-    // Amenities
-    amenities: {
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-        marginBottom: 10,
-    },
-    featureIconItem: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 10,
-    },
-    categoryItem: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 5,
-    },
-    featureIcon: {
-        width: 24,
-        height: 24,
-        marginRight: 8,
-    },
-    featureText: {
-        fontSize: 14,
-        color: 'black',
-    },
-    // Host
-    nameButton: {
-        width: 110,
-        borderWidth: 1,
-        borderColor: 'green',
-        borderRadius: 12,
-        padding: 10,
-    },
-    nameText: {
-        color: 'black',
-        fontSize: 12,
-        fontFamily: 'MotivaSansBold.woff',
-        textAlign: 'center',
-    },
-    // All amenities modal
-    modalOverlay: {
-        flex: 1,
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    modalContent: {
-        width: '90%',
-        maxHeight: '80%',
-        backgroundColor: 'white',
-        padding: 20,
-        borderRadius: 10,
-    },
-    closeButton: {
-        alignSelf: 'flex-end',
-    },
-    closeButtonText: {
-        fontSize: 20,
-    },
-    modalTitle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
-    ShowAllAmenitiesButton: {
-        borderWidth: 2,
-        borderColor: 'green',
-        borderRadius: 15,
-        padding: 8,
-        alignSelf: 'center',
-    },
-    ShowAllAmenitiesButtonText: {
-        fontSize: 16,
-        color: 'black',
-        textAlign: 'center',
-    },
-    // Rendering dot
-    counterContainer: {
-        borderWidth: 2,
-        borderColor: 'rgba(0, 0, 0, 0.0)',
-        borderTopLeftRadius: 8,
-        borderBottomRightRadius: 8,
-        backgroundColor: 'rgba(0, 0, 0, 0.4)',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    counterText: {
-        fontSize: 16,
-        color: 'white',
-        fontFamily: 'MotivaSansRegular.woff',
-        marginLeft: 2,
-    },
-    dotContainer: {
-        flexDirection: 'row',
-        position: 'absolute',
-        bottom: 10,
-        alignSelf: 'center',
-    },
-    dot: {
-        width: 10,
-        height: 10,
-        borderRadius: 5,
-        marginHorizontal: 4,
-    },
-    activeDot: {
-        backgroundColor: 'green',
-    },
-    inactiveDot: {
-        backgroundColor: 'gray',
-    },
-});
 
-export default Detailpage;
+export default ListingDetailScreen;
