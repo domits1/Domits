7ca3e3db2ff7365d958510666991d87f7691e312
Ryan Reddy <90551370+Ryan-Reddy@users.noreply.github.com>
2025-02-20 11:26:16 +0100

    updates eslint config to use ignorePatterns instead of .eslintignore file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,24 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,tsx,jsx,css}"] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx,jsx,css}"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node }
+    },
+    plugins: {
+      react: pluginReact,
+      '@typescript-eslint': tseslint
+    },
+    extends: [
+      pluginJs.configs.recommended,
+      ...tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended
+    ],
+    ignorePatterns: [
+      "node_modules/",
+      "dist/",
+      "build/"
+    ],
+  },
 ];
