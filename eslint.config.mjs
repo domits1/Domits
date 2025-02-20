b3e0af43d897582c73ff606021d5aca0010e7fba
Ryan Reddy <90551370+Ryan-Reddy@users.noreply.github.com>
2025-02-20 11:08:28 +0100

    adds eslint eslint.config.mjs .eslintignore to root
    and to package.json & package-lock.json

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,tsx,jsx,css}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
