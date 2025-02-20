04a346537efed34e02f1afe52f24a577a762cc8e
Ryan Reddy <90551370+Ryan-Reddy@users.noreply.github.com>
2025-02-20 11:53:43 +0100

    prettier up and running now, needs finetuning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,36 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,tsx,jsx,css}"],
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+  },
+  {
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node }
+      globals: {
+        ...globals.browser, // for browser-specific globals
+        jest: true,  // adds Jest globals like describe, test, and expect
+      },
     },
-    plugins: {
-      react: pluginReact,
-      '@typescript-eslint': tseslint
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    ignores: [
+      "node_modules/*",
+      "dist/*",
+      "build/*",
+      "__tests__/*",  // Ignore test files
+      "amplify/*",     // Ignore compiled code in Amplify
+    ],
+  },
+  {
+    settings: {
+      react: {
+        version: "detect",  // Automatically detects the React version
+      },
     },
-    extends: [
-      pluginJs.configs.recommended,
-      ...tseslint.configs.recommended,
-      pluginReact.configs.flat.recommended
-    ],
-    ignorePatterns: [
-      "node_modules/",
-      "dist/",
-      "build/"
-    ],
   },
 ];
