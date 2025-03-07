fc653d929d1332f73ed8767dada6b57eaa0f3897
Mary-Anyanwu <maryanyanwu1@yahoo.com>
2025-03-07 11:59:35 +0100

    🐛 Fixed test bugs and made the test more stable in GuestBookingEngine.cy.js

--- web/src/tests/cypress/e2e/Guest/GuestBookingEngine.cy.js
+++ web/src/tests/cypress/e2e/Guest/GuestBookingEngine.cy.js
@@ -1,89 +1,94 @@
-describe('Landing Page Tests', () => {
+import '../../support/commands';
 
+describe('Booking Engine Test - Aventura Yachts A45', () => {
     beforeEach(() => {
         cy.viewport(1920, 1080);
-    });
 
-    it('Should login', () => {
-        cy.visit('https://acceptance.domits.com/landing/', { failOnStatusCode: false });
+        cy.session('loginSession', () => {
+            cy.loginAsGuest();
+            cy.get('.header-links').should('contain', 'Switch to Guest'); 
+        });
+
+        cy.visit('https://acceptance.domits.com/home', { failOnStatusCode: false });
         cy.wait(3000); 
-        
-        cy.get('[name="firstName"]').should('be.visible').type('Test');
-        cy.get('[name="lastName"]').should('be.visible').type('Persoon');
-        cy.get('[type="email"]').should('be.visible').type('testpersoon@scarden.com');
-        cy.get('#password').should('be.visible').type('Test24680!');
-        
-        cy.get('.hostCheckbox > input').click();
-        cy.get('.registerButton').click();
-        cy.get('.errorText').should('exist');
     });
 
-    it('should click on the Domits logo.', () => {
-        cy.visit('https://acceptance.domits.com/home/', { failOnStatusCode: false });
-        cy.wait(4000);
+    it('Should log in as a guest', () => {
+        // Step 1: Verify that the user is logged in correctly
+        cy.get('.header-links > .headerHostButton').click();
+        cy.get('.edit-icon-background').click();
         
-        cy.get('.logo > a > img').should('have.attr', 'alt', 'Logo');
-        cy.get('.logo > a').click();
-        cy.url().should('eq', 'https://acceptance.domits.com/home/');
+        cy.get(':nth-child(2) > .guest-edit-input')
+            .should('be.visible')
+            .clear()
+            .type('testpersoondomits@gmail.com', { force: true });
+
+        cy.get(':nth-child(3) > .guest-edit-input')
+            .clear()
+            .type('Test', { force: true });
+
+        cy.wait(2000);
+        cy.get('.edit-icon-background').click();
+        cy.wait(2000);
+
+        // Simulate API response for logged-in user
+        cy.intercept('GET', '**/currentUserInfo', {
+            statusCode: 200,
+            body: {
+                attributes: {
+                    email: 'testpersoondomits@gmail.com',
+                    given_name: 'Test',
+                },
+            },
+        });
     });
 
-    it('should be able to choose a property and complete interactions', () => {
-        cy.visit('https://acceptance.domits.com/home/', { failOnStatusCode: false });
-        cy.wait(3000);
+    it('should select Aventura Yachts A45 and complete the booking process', () => {
+       
+       cy.wait(3000); 
 
-        cy.get(':nth-child(9) > .swiper > .swiper-wrapper > .swiper-slide-visible > img').click();
-        cy.wait(2000); 
-        
-        cy.visit('https://acceptance.domits.com/listingdetails/?ID=ed1c0148-4dd7-44d7-80fd-88afa1183ba3', { failOnStatusCode: false });
-        cy.wait(3000);
+       // Click on the correct accommodation "Aventura Yachts A45"
+       cy.get(':nth-child(3) > .swiper > .swiper-wrapper > .swiper-slide-visible > img')
+           .should('be.visible')
+           .click({ force: true });
 
-        cy.get('.dropdown-button').click();
-        cy.get(':nth-child(1) > .button__box > :nth-child(1)').click();
-        cy.get('.button__box > :nth-child(3)').click();
-        cy.wait(1000);
+       // Verify that we are on the accommodation page
+       cy.url().should('include', '/listingdetails?ID=16426fb5-378d-420a-87a7-a1a509e91677');
+       cy.wait(2000);
 
-        cy.get(':nth-child(2) > .button__box > :nth-child(1)').click();
-        cy.get(':nth-child(2) > .button__box > :nth-child(2)').click();
-        cy.wait(1000);
+       // Open the guest selection dropdown and select guests
+       cy.get('.dropdown-button').click();
+       cy.wait(1000);
+       
+       
+       cy.get('.button__box > :nth-child(3)').click();
+       cy.wait(500);
+      
+       cy.get(':nth-child(2) > .button__box > :nth-child(2)').click();
+       cy.wait(500);
+      
+       cy.get(':nth-child(3) > .button__box > :nth-child(2)').click();
+       cy.wait(500);
+       
+       cy.get('.dropdown-button').click();
+       cy.wait(1000);
 
-        cy.get(':nth-child(3) > .button__box > :nth-child(1)').click();
-        cy.get(':nth-child(3) > .button__box > :nth-child(2)').click();
-        cy.wait(1000);
+       // Click the reserve button
+       cy.get('.reserve-button').should('be.visible').click();
+       cy.wait(3000);
 
-        cy.get('.dropdown-button').click();
-        cy.get('.reserve-button').click();
-    });
+       // Verify that we are on the correct booking overview page
+       cy.url().should('include', '/bookingoverview');
+       cy.wait(2000); // Extra wait to ensure the page is fully loaded
 
-    it('should be able to choose a property and complete interactions', () => {
-        cy.visit('https://acceptance.domits.com/bookingoverview/?id=ed1c0148-4dd7-44d7-80fd-88afa1183ba3&checkIn=null&checkOut=null&adults=1&kids=1&pets=1&cleaningFee=0&amountOfGuest=3&taxes=0&serviceFee=0', { failOnStatusCode: false });
-        cy.wait(3000);
+       cy.visit('https://acceptance.domits.com/bookingoverview/?id=16426fb5-378d-420a-87a7-a1a509e91677&checkIn=null&checkOut=null&adults=1&kids=0&pets=0&cleaningFee=0&amountOfGuest=1&taxes=0&ServiceFee=0', { failOnStatusCode: false });
+       cy.wait(3000);
 
-        cy.get('[name="firstName"]').should('be.visible').type('Test');
-        cy.get('[name="lastName"]').should('be.visible').type('Persoon');
-        cy.get('[type="email"]').should('be.visible').type('testpersoon@scarden.com');
+       // Verify that the "Confirm & Pay" button is visible
+       cy.get('.confirm-pay-button').should('be.visible');
 
-        cy.get('.countryCodeDropdown').should('be.visible').select('+31');
-        cy.get('input[name="phone"]').should('be.visible').type('0612345678');
+       // Click the "Confirm & Pay" button
+        cy.get('.confirm-pay-button').should('be.visible').click();
 
-        cy.get('#password').should('be.visible').type('Test24680!');
-        
-        cy.get('.hostCheckbox > input').click();
-        cy.get('.registerButton').click();
-        cy.get('.errorText').should('exist');
-    });
-
-    it('should confirm email by entering the code', () => {
-        cy.visit('https://acceptance.domits.com/confirm-email', { failOnStatusCode: false });
-        cy.wait(3000);
-        
-        cy.get('[name="digit0"]').should('be.visible').type('1');
-        cy.get('[name="digit1"]').should('be.visible').type('2');
-        cy.get('[name="digit2"]').should('be.visible').type('3');
-        cy.get('[name="digit3"]').should('be.visible').type('4');
-        cy.get('[name="digit4"]').should('be.visible').type('5');
-        cy.get('[name="digit5"]').should('be.visible').type('6');
-
-        cy.get('.hostverification_publish-btn__TiDGZ').click();
-    });
-
+   });
 });
