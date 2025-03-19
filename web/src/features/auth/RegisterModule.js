f1eaab22263c427dd0a1a731da93eeea7d201fcb
Sebastiaan van der Wilt <sebastiaanvanderwilt@gmail.com>
2025-03-19 10:18:34 +0100

    name changes to register modules

--- web/src/features/auth/RegisterModule.js
+++ web/src/features/auth/RegisterModule.js
@@ -1,354 +1,99 @@
-import React, { useState, useContext, useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Auth } from 'aws-amplify';
-import FlowContext from '../../services/FlowContext';
-import './Register.css';
-import PhoneInput from 'react-phone-input-2';
-import 'react-phone-input-2/lib/style.css';
+import React, { useState } from 'react';
+import './imagegallery.css';
+import IosShareIcon from '@mui/icons-material/IosShare';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
-const RegisterModule = () => {
-    const navigate = useNavigate();
-    const { flowState, setFlowState } = useContext(FlowContext);
+const ImageGallery = ({ images }) => {
+    const [selectedImg, setSelectedImg] = useState(images[0]);
+    const [liked, setLiked] = useState(false);
+    const [showOverlay, setShowOverlay] = useState(false);
 
-    const generateRandomUsername = () => {
-        const chars = String.fromCharCode(...Array(127).keys()).slice(33);
-        let result = '';
-        for (let i = 0; i < 15; i++) {
-            result += chars.charAt(Math.floor(Math.random() * chars.length));
-        }
-        return result;
-    };
-
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-        repeatPassword: '',
-        username: generateRandomUsername(),
-        firstName: '',
-        lastName: '',
-        phone: '',
-    });
-
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
-    const [signUpClicked, setSignUpClicked] = useState(false);
-    const [shouldShake, setShouldShake] = useState(false);
-    const [passwordShake, setPasswordShake] = useState(false);
-    const [requirements, setRequirements] = useState({
-        length: false,
-        uppercase: false,
-        number: false,
-        specialChar: false,
-    });
-    const [isPasswordStrong, setIsPasswordStrong] = useState(false);
-    const passwordRef = useRef(null);
-    const strengthBarRef = useRef(null);
-    const strengthTextRef = useRef(null);
-    const strengthContainerRef = useRef(null);
-
-    const handleHostChange = (e) => {
-        setFlowState(prevState => ({
-            ...prevState,
-            isHost: e.target.checked
-        }));
-    };
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value,
-        }));
-        if (name === 'password') {
-            checkPasswordStrength(value);
-        }
-    };
-
-    const handleCountryCodeChange = (e) => {
-        setFormData(prevState => ({
-            ...prevState,
-            countryCode: e.target.value,
-        }));
-    };
-
-    const checkPasswordStrength = (password) => {
-        let strength = 0;
-
-        const newRequirements = {
-            length: password.length >= 8,
-            uppercase: /[A-Z]/.test(password),
-            number: /[0-9]/.test(password),
-            specialChar: /[^A-Za-z0-9]/.test(password),
-        };
-
-        setRequirements(newRequirements);
-
-        for (const key in newRequirements) {
-            if (newRequirements[key]) strength++;
-        }
-
-        const strengthBar = strengthBarRef.current;
-        const strengthText = strengthTextRef.current;
-
-        if (strengthBar) {
-            const strengthPercentage = (strength / 4) * 100;
-            strengthBar.style.width = strengthPercentage + '%';
-
-            if (strength < 2) {
-                setColorAndText('red', 'Bad');
-                setIsPasswordStrong(false);
-            } else if (strength === 2) {
-                setColorAndText('orange', 'Weak');
-                setIsPasswordStrong(false);
-            } else if (strength === 3) {
-                setColorAndText('#088f08', 'Strong');
-                setIsPasswordStrong(true);
-            } else if (strength === 4) {
-                setColorAndText('green', 'Very Strong');
-                setIsPasswordStrong(true);
-            }
-        }
-
-        if (strengthContainerRef.current) {
-            strengthContainerRef.current.style.display = 'block';
-        }
-    };
-
-    const setColorAndText = (color, text) => {
-        const strengthBar = strengthBarRef.current;
-        const strengthText = strengthTextRef.current;
-
-        if (strengthBar) {
-            strengthBar.style.backgroundColor = color;
-        }
-        if (strengthText) {
-            strengthText.textContent = text;
-            strengthText.style.color = color;
-        }
-    };
-
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        const { username, email, password, firstName, lastName, phone, countryCode } = formData;
-
-        if (!isPasswordStrong) {
-            setErrorMessage("Password must be strong to submit.");
-            setPasswordShake(true);
-        }
-        if (firstName.length < 1) {
-            setErrorMessage('First name cannot be empty.');
-            return;
-        }
-        if (lastName.length < 1) {
-            setErrorMessage('Last name cannot be empty.');
-            return;
-        }
-        if (phone.length < 1) {
-            setErrorMessage('Phone number cannot be empty.');
-            return;
-        }
-        if (password.length < 8) {
-            setErrorMessage('Password must be at least 8 characters long.');
-            setPasswordShake(true);
-            return;
-        }
-
-        if (password.length > 64) {
-            setErrorMessage('Password must be less than 64 characters.');
-            setPasswordShake(true);
-            return;
-        }
-
-        const hasUppercase = /[A-Z]/.test(password);
-        const hasNumber = /[0-9]/.test(password);
-
-        if (!hasUppercase && !hasNumber) {
-            setErrorMessage('Password must contain at least one uppercase letter or one number.');
-            setPasswordShake(true);
-            return;
-        }
-
-        if (!email) {
-            setErrorMessage('Email can\'t be empty!');
-            return;
-        }
-
-        if (!password) {
-            setErrorMessage('Password can\'t be empty!');
-            return;
-        }
-        try {
-            const emailName = email.split('@')[0];
-            const groupName = flowState.isHost ? "Host" : "Traveler";
-            await Auth.signUp({
-                username: email,
-                password,
-                attributes: {
-                    'custom:group': groupName,
-                    'custom:username': emailName + username,
-                    'given_name': firstName,
-                    'family_name': lastName,
-                    'phone_number': `${countryCode}${phone}`,
-                },
+    const handleShare = () => {
+        navigator.clipboard
+            .writeText(window.location.href)
+            .then(() => {
+                alert('URL is copied to clipboard!');
+            })
+            .catch((error) => {
+                console.error('Could not copy the URL:', error);
             });
-            navigate('/confirm-email', {
-                state: { email, password }
-            });
-        } catch (error) {
-            if (error.code === 'UsernameExistsException') {
-                setErrorMessage('Email already exists!');
-            } else {
-                console.error("Error:", error);
-                setErrorMessage('An unexpected error occurred');
-            }
-        }
     };
 
-    const handleSignOut = async () => {
-        try {
-            await Auth.signOut();
-            setIsAuthenticated(false);
-            window.location.reload();
-        } catch (error) {
-            console.error('Error logging out:', error);
-        }
+    const handleOverlayToggle = () => {
+        setShowOverlay(!showOverlay);
     };
-
-    useEffect(() => {
-        const checkAuth = async () => {
-            try {
-                await Auth.currentAuthenticatedUser();
-                setIsAuthenticated(true);
-            } catch (error) {
-                setIsAuthenticated(false);
-            }
-        };
-
-        checkAuth();
-    }, []);
-
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            if (!errorMessage.includes('Username')) {
-                setShouldShake(false);
-            }
-            if (!errorMessage.includes('Password')) {
-                setPasswordShake(false);
-            }
-        }, 300);
-        return () => clearTimeout(timeout);
-    }, [errorMessage]);
-
-    useEffect(() => {
-        if (signUpClicked) {
-            setSignUpClicked(false);
-        }
-    }, [signUpClicked]);
 
     return (
-        <>
-            {isAuthenticated ? (
-                <div className='signOutDiv'>
-                    <button className='signOutButton' onClick={handleSignOut}>Sign out</button>
-                </div>
-            ) : (
-                <div className="registerContainer">
-                    <div className="registerTitle">Create an account on Domits</div>
-                    <form onSubmit={onSubmit} className="registerForm">
-                        <label>First Name*</label>
-                        <input
-                            className="registerInput"
-                            type="text"
-                            name="firstName"
-                            value={formData.firstName}
-                            onChange={handleChange}
-                        />
+        <div className="image-gallery-container">
+            {/* Selected image */}
+            <div className="selected-image">
+                <img
+                    className="selected-thumbnail"
+                    src={selectedImg}
+                    srcSet={`${selectedImg}?w=800 800w, ${selectedImg}?w=1200 1200w, ${selectedImg}?w=1600 1600w`}
+                    sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    alt="Selected"
+                    loading="lazy"
+                />
+                <button className="share-button" onClick={handleShare}>
+                    <IosShareIcon />
+                </button>
+                <button className="like-button" onClick={() => setLiked(!liked)}>
+                    {liked ? (
+                        <FavoriteIcon sx={{ color: '#ec5050' }} />
+                    ) : (
+                        <FavoriteBorderOutlinedIcon />
+                    )}
+                </button>
+            </div>
 
-                        <label>Last Name*</label>
-                        <input
-                            className="registerInput"
-                            type="text"
-                            name="lastName"
-                            value={formData.lastName}
-                            onChange={handleChange}
-                        />
+            {/* Image thumbnails */}
+            <div className="image-thumbnails">
+                {images.slice(0, 4).map((img, index) => (
+                    <img
+                        key={index}
+                        src={img}
+                        srcSet={`${img}?w=100 100w, ${img}?w=200 200w`}
+                        sizes="100px"
+                        alt={`Thumbnail ${index}`}
+                        onClick={() => setSelectedImg(img)}
+                        className={img === selectedImg ? 'active' : ''}
+                        loading="lazy"
+                    />
+                ))}
+            </div>
 
-                        <label>Email*</label>
-                        <input
-                            className="registerInput"
-                            type="email"
-                            name="email"
-                            value={formData.email}
-                            onChange={handleChange}
-                        />
-                        <label>Phone Number*</label>
+            {/* Show all button */}
+            {images.length > 4 && (
+                <button className="show-all-button" onClick={handleOverlayToggle}>
+                    Show All Pictures
+                </button>
+            )}
 
-                        <PhoneInput
-                            country={'nl'}
-                            value={formData.phone}
-                            onChange={phone => setFormData(prevState => ({ ...prevState, phone }))}
-                            inputClass="registerInput"
-                            containerClass="phoneInputContainer"
-                            inputStyle={{ width: '110%', border: '1px solid #999' }}
-                        />
-
-                        <label>Password*</label>
-                        <div className="passwordContainer">
-                            <input
-                                id="password"
-                                ref={passwordRef}
-                                className={`registerInput ${errorMessage.includes('Password') ? 'inputError' : ''} ${passwordShake ? 'inputShake' : ''}`}
-                                type="password"
-                                name="password"
-                                value={formData.password}
-                                onChange={handleChange}
-                                onFocus={() => {
-                                    if (strengthContainerRef.current) {
-                                        strengthContainerRef.current.style.display = 'block';
-                                    }
-                                }}
-                                style={{ borderColor: errorMessage.includes('Password') ? 'red' : 'var(--secondary-color)' }}
-                            />
+            {/* Overlay */}
+            {showOverlay && (
+                <div className="overlay">
+                    <div className="overlay-content">
+                        <button className="close-overlay-button" onClick={handleOverlayToggle}>
+                            Close
+                        </button>
+                        <div className="overlay-images">
+                            {images.map((img, index) => (
+                                <img
+                                    key={index}
+                                    src={img}
+                                    alt={`Overlay Thumbnail ${index}`}
+                                    onClick={() => setSelectedImg(img)}
+                                    className="overlay-thumbnail"
+                                />
+                            ))}
                         </div>
-                        <div ref={strengthContainerRef} className="strength-container" style={{ display: 'none' }}>
-                            <div id="strength-bar" ref={strengthBarRef}></div>
-                            <div className="strength-text" ref={strengthTextRef}></div>
-                            <div className="requirements">
-                                <label>
-                                    <input type="checkbox" checked={requirements.length} readOnly />
-                                    At least 8 characters
-                                </label>
-                                <label>
-                                    <input type="checkbox" checked={requirements.uppercase} readOnly />
-                                    At least 1 uppercase letter
-                                </label>
-                                <label>
-                                    <input type="checkbox" checked={requirements.number} readOnly />
-                                    At least 1 number
-                                </label>
-                                <label>
-                                    <input type="checkbox" checked={requirements.specialChar} readOnly />
-                                    At least 1 special character
-                                </label>
-                            </div>
-                        </div>
-                        <label className="hostCheckbox">
-                            <input
-                                type="checkbox"
-                                checked={flowState.isHost}
-                                onChange={handleHostChange}
-                            /> Become a Host
-                        </label>
-                        <div className="alreadyAccountText">
-                            Already have an account? <a href="/web/src/features/auth/Login">Log in here</a>
-                        </div>
-                        {errorMessage && <div className="errorText">{errorMessage}</div>}
-                        <button type="submit" className="registerButton" onClick={() => setShouldShake(true)}>Sign Up</button>
-                    </form>
+                    </div>
                 </div>
             )}
-        </>
+        </div>
     );
 };
 
-export default RegisterModule;
+export default ImageGallery;
