5f1db35824a0aae4f2ae236f31f970bf9ea474ad
Damien Eiking <152298130+D-eiking@users.noreply.github.com>
2025-03-20 17:04:06 +0100

    summery

--- web/src/pages/home/AccommodationCard.js
+++ web/src/pages/home/AccommodationCard.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import './styles/Accommodations.css';
+import '../../pages/home/styles/Accommodations.css';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
