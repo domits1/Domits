2a6f1a6b32f331b2bb6673c6248fa64dc0e3cdab
2174951 <131653171+2174951@users.noreply.github.com>
2025-03-26 14:45:36 +0100

    Guest chat new responsive styling

--- web/src/features/guestdashboard/guestmessages/components/guestChatScreen.jsx
+++ web/src/features/guestdashboard/guestmessages/components/guestChatScreen.jsx
@@ -6,11 +6,10 @@ import ChatMessage from './chatMessage';
 import { WebSocketContext } from '../context/webSocketContext';
 import { v4 as uuidv4 } from 'uuid';
 import '../styles/guestChatScreen.css'
-import { FaHome, FaUsers, FaCalendar, } from 'react-icons/fa';
-
+import { FaHome, FaUsers, FaCalendar, FaTimes } from 'react-icons/fa';
 import profileImage from '../domits-logo.jpg';
 
-const GuestChatScreen = ({ userId, contactId, contactName, connectionId, handleContactListMessage }) => {
+const GuestChatScreen = ({ userId, contactId, contactName, connectionId, handleContactListMessage, onClose }) => {
     const { messages, loading, error, fetchMessages, addNewMessage } = useFetchMessages(userId);
     const { bookingDetails } = useFetchBookingDetails(contactId, userId);
     const { sendMessage, sending, error: sendError } = useSendMessage(userId);
@@ -66,71 +65,82 @@ const GuestChatScreen = ({ userId, contactId, contactName, connectionId, handleC
     };
 
     return contactId ? (
-        <div className="guest-chat-screen">
-            <div className="host-chat-header">
-                {/* <img src={contactImage} alt={contactName} className="profile-img" /> */}
-                <img src={profileImage} alt={contactName} className="profile-img" />
-                <div className="host-chat-header-info">
+        <div className='guest-chat'>
+            <div className="guest-chat-screen">
+                <div className="guest-chat-header">
+                    
+                    {/* <img src={contactImage} alt={contactName} className="profile-img" /> */}
+                    <img src={profileImage} alt={contactName} className="profile-img" />
+                    <div className="guest-chat-header-info">
 
-                    <div className="host-chat-header-grid">
-                        <h3>{contactName}</h3>
-                        <p style={{ visibility: bookingDetails?.Title ? 'visible' : 'hidden' }}>
-                            <FaHome style={{ marginRight: '8px' }} /> {bookingDetails?.Title}
-                        </p>
+                        <div className="guest-chat-header-grid">
+                            <h3>{contactName}</h3>
+                            <p style={{ visibility: bookingDetails?.Title ? 'visible' : 'hidden' }}>
+                                <FaHome style={{ marginRight: '8px' }} /> {bookingDetails?.Title}
+                            </p>
 
-                        <p style={{ visibility: bookingDetails?.AmountOfGuest ? 'visible' : 'hidden' }}>
-                            <FaUsers style={{ marginRight: '8px' }} /> {bookingDetails?.AmountOfGuest} Travelers
-                        </p>
+                            <p style={{ visibility: bookingDetails?.AmountOfGuest ? 'visible' : 'hidden' }}>
+                                <FaUsers style={{ marginRight: '8px' }} /> {bookingDetails?.AmountOfGuest} Travelers
+                            </p>
 
-                        <p style={{ visibility: bookingDetails?.StartDate && bookingDetails?.EndDate ? 'visible' : 'hidden' }}>
-                            <FaCalendar style={{ marginRight: '8px' }} /> {bookingDetails?.StartDate} / {bookingDetails?.EndDate}
-                        </p>
+                            <p style={{ visibility: bookingDetails?.StartDate && bookingDetails?.EndDate ? 'visible' : 'hidden' }}>
+                                <FaCalendar style={{ marginRight: '8px' }} /> {bookingDetails?.StartDate} / {bookingDetails?.EndDate}
+                            </p>
 
+                        </div>
                     </div>
+                    {onClose && (
+                        <button className="close-chat-button" onClick={onClose} title="Close Chat">
+                            <FaTimes />
+                        </button>
+                    )}
                 </div>
+
+                <div className="chat-screen">
+                    {loading ? (
+                        <p>Loading messages...</p>
+                    ) : error ? (
+                        <p>{error}</p>
+                    ) : (
+                        messages.map((message) => (
+                            <ChatMessage
+                                key={message.id}
+                                message={message}
+                                userId={userId}
+                                contactName={contactName}
+                            />
+                        ))
+                    )}
+                </div>
+
+                <div className="guest-chat-input">
+                    <textarea
+                        value={newMessage}
+                        onChange={(e) => setNewMessage(e.target.value)}
+                        placeholder=""
+                        className="guest-message-input-textarea"
+                        onKeyUp={(e) => {
+                            if (e.key === "Enter") {
+                                handleSendMessage();
+                            }
+                        }}
+                    />
+                    <button
+                        onClick={handleSendMessage}
+                        className="guest-message-input-send-button"
+                        disabled={sending}
+                    >
+                        {sending ? 'Sending...' : 'Send'}
+                    </button>
+                </div>
+
+                {sendError && <p className="error-message">{sendError.message}</p>}
             </div>
 
-            <div className="chat-screen">
-                {loading ? (
-                    <p>Loading messages...</p>
-                ) : error ? (
-                    <p>{error}</p>
-                ) : (
-                    messages.map((message) => (
-                        <ChatMessage
-                            key={message.id}
-                            message={message}
-                            userId={userId}
-                            contactName={contactName}
-                        />
-                    ))
-                )}
-            </div>
 
-            <div className="guest-chat-input">
-                <textarea
-                    value={newMessage}
-                    onChange={(e) => setNewMessage(e.target.value)}
-                    placeholder=""
-                    className="guest-message-input-textarea"
-                    onKeyUp={(e) => {
-                        if (e.key === "Enter") {
-                            handleSendMessage();
-                        }
-                    }}
-                />
-                <button
-                    onClick={handleSendMessage}
-                    className="guest-message-input-send-button"
-                    disabled={sending}
-                >
-                    {sending ? 'Sending...' : 'Send'}
-                </button>
-            </div>
-
-            {sendError && <p className="error-message">{sendError.message}</p>}
         </div>
-    ): null;
+
+    ) : null;
 
 };
 
