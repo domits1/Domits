72db173321cdeecf3fc2ce5fff617e215fb7d6a2
Greedy <sem@example.com>
2025-04-01 16:26:44 +0200

    Had wrong js code now its fixed

--- web/src/pages/Performance/performance.js
+++ web/src/pages/Performance/performance.js
@@ -1,14 +1,14 @@
 import React from "react";
-import "./performance.css";
+import "./App.css";
 
 function Performance() {
   return (
-    <div className="performance">
-      <div className="performance__title-container">
-        <h2 className="performance__title">
+    <div className="security">
+      <div className="security__title-container">
+        <h2 className="security__title">
           <span className="highlightH2">Performance Bundles</span>
         </h2>
-        <p className="performance__description">
+        <p className="security__text">
           Our performance team loves to help you start, grow and scale in your
           short-term rental journey. It doesn’t matter if you use Domits or
           other tools. We're ready to increase revenue, reduce workload, and
@@ -16,16 +16,16 @@ function Performance() {
         </p>
       </div>
 
-      <div className="performance__categories">
-        <p className="performance__category">
+      <div className="security__factsbox">
+        <p className="security__fact-title">
           Starting host 1-9 properties | Growing host 10-99 properties | Scaling
           host 100+ properties
         </p>
       </div>
 
-      <div className="performance__section">
-        <h3 className="performance__subtitle">1. Distribution & Marketing</h3>
-        <ul className="performance__list">
+      <div className="security__text-container">
+        <h3 className="security__subtitle">1. Distribution & Marketing</h3>
+        <ul className="security__list">
           <li>
             Channel management for distribution with Booking.com, Airbnb, VRBO,
             and 100+ channels.
@@ -46,9 +46,9 @@ function Performance() {
         </ul>
       </div>
 
-      <div className="performance__section">
-        <h3 className="performance__subtitle">2. Streamline Operations</h3>
-        <ul className="performance__list">
+      <div className="security__text-container">
+        <h3 className="security__subtitle">2. Streamline Operations</h3>
+        <ul className="security__list">
           <li>Automate property management</li>
           <li>Reservation management</li>
           <li>Rate management</li>
@@ -58,9 +58,9 @@ function Performance() {
         </ul>
       </div>
 
-      <div className="performance__section">
-        <h3 className="performance__subtitle">3. Grow Revenue</h3>
-        <ul className="performance__list">
+      <div className="security__text-container">
+        <h3 className="security__subtitle">3. Grow Revenue</h3>
+        <ul className="security__list">
           <li>Dynamic pricing</li>
           <li>Digital upselling</li>
           <li>Occupancy rates</li>
@@ -69,9 +69,9 @@ function Performance() {
         </ul>
       </div>
 
-      <div className="performance__section">
-        <h3 className="performance__subtitle">4. Guest Experience</h3>
-        <ul className="performance__list">
+      <div className="security__text-container">
+        <h3 className="security__subtitle">4. Guest Experience</h3>
+        <ul className="security__list">
           <li>Warmth and friendliness</li>
           <li>Booking engine</li>
           <li>Messaging</li>
