4b983c1cb3c24dc6b0417a59e09d7e3fc8c9d427
Ryan Reddy <90551370+Ryan-Reddy@users.noreply.github.com>
2025-04-02 16:52:48 +0200

    fixes a bunch of random errors

--- web/src/features/sustainability/Sustainability.js
+++ web/src/features/sustainability/Sustainability.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Sustainability.css";
-import {submitAccommodation} from "../../services/hostOnBoardingService/hostonboardingApi";
+import {submitAccommodation} from "../hostonboarding/services/hostonboardingApi";
 
 const Sustainability = () => {
     return (
