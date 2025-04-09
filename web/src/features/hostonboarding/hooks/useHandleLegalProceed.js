2af8b19a8a06a156aa0c12bd6e910e82c9f17656
Ryan Reddy <90551370+Ryan-Reddy@users.noreply.github.com>
2025-04-09 17:05:41 +0200

    removes console.error from useHandleLegalProceed

--- web/src/features/hostonboarding/hooks/useHandleLegalProceed.js
+++ web/src/features/hostonboarding/hooks/useHandleLegalProceed.js
@@ -10,8 +10,6 @@ export const useHandleLegalProceed = () => {
       return console.error("no property type selected");
     }
 
-    console.error(validateAccommodationDetails());
-
     console.log("navigating", selectedType);
 
     if (["Villa", "House", "Apartment", "Cottage"].includes(selectedType)) {
@@ -21,17 +19,5 @@ export const useHandleLegalProceed = () => {
     }
   };
 
-  const validateAccommodationDetails = (details) => {
-    const errors = [];
-
-    if (!details.type) errors.push("Type is required");
-    if (!details.title) errors.push("Title is required");
-    if (!details.description) errors.push("Description is required");
-
-    // if (Object.keys(details.images).length === 0)
-    //   errors.push("At least one image required");
-
-    return errors;
-  };
   return { handleProceed };
 };
