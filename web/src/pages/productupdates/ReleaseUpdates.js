5e2e970595d0ea369e8f7076dbfdaa42e872ee66
smallproject <j.a.villas@live.com.ph>
2025-04-10 08:20:28 +0200

    clean code

--- web/src/pages/productupdates/ReleaseUpdates.js
+++ web/src/pages/productupdates/ReleaseUpdates.js
@@ -7,39 +7,6 @@ function ReleaseUpdates() {
   const [embedError, setEmbedError] = useState(false);
   const [tweets, setTweets] = useState([]);
   const [error, setError] = useState(false);
-
-  // Array of Twitter URLs
-  const twitterUrls = [
-    // Add more Tweet IDs here
-    // "1897981256601645559",
-    // "1879062001433112582",
-    // "1871785101635592671",
-    // "1862071434496950433",
-    // "1848735303080497277",
-    // "1836033944933863521",
-    // "1805597517528068345",
-    // "1798979282028712125",  
-    // "1796461135090315768",
-    // "1795443789844136263",
-
-    // "1793952270226264519",
-    // "1793187248512331973",
-    // "1786342625571643755",
-    // "1785634255713804567",
-    // "1785315941720408469",
-  ];
-
-  // useEffect(() => {
-  //   axios.get("https://api.twitter.com/2/users/1720359275644547072/tweets?max_results=5", {       
-  //     headers: {
-  //        Authorization: `Bearer ${BEARER_TOKEN}`,
-  //     },
-  //   })
-  //   .then((response) => response.json())
-  //   .then((data) => setTweets(data))
-  //   .catch(() => setError(true));
-  // }, []);
-
  
   const fetchTweets = async () => {
     try {
