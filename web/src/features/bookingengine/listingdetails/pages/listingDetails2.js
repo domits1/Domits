9d6447fccab22513e597cb1f61fd3270f82a613e
Randy Luo <117186456+Randy20020603@users.noreply.github.com>
2025-04-16 11:04:15 +0200

    fetching hostInfo succesfully

--- web/src/features/bookingengine/listingdetails/pages/listingDetails2.js
+++ web/src/features/bookingengine/listingdetails/pages/listingDetails2.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import {useLocation} from "react-router-dom";
 import Loading from "../../../hostdashboard/Loading";
 import FetchPropertyById from "../services/fetchPropertyById";
+import fetchHostInfo from "../services/fetchHostInfo";
 import "../../../../styles/sass/booking-engine/listingDetails.scss";
 import Header from "../components/header";
 import PropertyContainer from "../views/propertyContainer";
@@ -14,7 +15,9 @@ const ListingDetails2 = () => {
     const [property, setProperty] = useState({});
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [hostId, setHostId] = useState();
+    const [hostId, setHostId] = useState(null);
+    const [host, setHost] = useState(null);
+
 
     useEffect(() => {
         setError(null);
@@ -25,7 +28,8 @@ const ListingDetails2 = () => {
                 const fetchedProperty = await FetchPropertyById(id);
                 setProperty(fetchedProperty);
                 setHostId(fetchedProperty.property.hostId);
-                console.log(fetchedProperty.property.hostId);
+                const hostData = await fetchHostInfo(fetchedProperty.property.hostId);
+                setHost(hostData);
                 setLoading(false);
             } catch (error) {
                 setError("Something went wrong while fetching the requested data, please try again later.")
