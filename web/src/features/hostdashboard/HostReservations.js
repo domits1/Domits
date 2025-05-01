50c3e013f824893dae8cc82198c4bd57a15de224
bambaisbanned <Iphonegamer400@gmail.com>
2025-05-01 13:54:53 +0200

    State of calender now having everything in table
    force commit to merge acceptance with branch

--- web/src/features/hostdashboard/HostReservations.js
+++ web/src/features/hostdashboard/HostReservations.js
@@ -1,71 +1,102 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import Pages from "./Pages.js";
-import styles from "../../styles/sass/hostdashboard/hostreservations.module.scss"
-import EventIcon from '@mui/icons-material/Event';
-import SwapVertIcon from '@mui/icons-material/SwapVert';
-import FilterListIcon from '@mui/icons-material/FilterList';
+import styles from "../../styles/sass/hostdashboard/hostreservations.module.scss";
+import EventIcon from "@mui/icons-material/Event";
+import SwapVertIcon from "@mui/icons-material/SwapVert";
+import FilterListIcon from "@mui/icons-material/FilterList";
 import pageSwitcherStyling from "../../utils/PageSwitcher.module.css";
 import info from "../../images/icons/info.png";
-import {Auth} from "aws-amplify";
+import { Auth } from "aws-amplify";
 import spinner from "../../images/spinnner.gif";
 import ReservationItem from "../../utils/ReservationItem";
 import chevron from "../../images/icons/sort-solid.svg";
 
 const HostReservations = () => {
-    return (
-            <main className="page-body">
-                <h2>Reservations</h2>
-                <section className={styles.reservationContainer}>
-                <Pages />
-                <section className={styles.reservationContent}>
-                    <div className={styles.reservationInfo}>
-                        <h2>Manage Reservations</h2>
-                        <p><EventIcon />You can manage your reservations on your properties here.</p>
-                    </div>
-                    <div className={styles.reservationButtons}>
-                        <button>All</button>
-                        <button>Incoming</button>
-                        <button>Accepted</button>
-                        <button>Cancelled</button>
-                    </div>
-                    <section className={styles.reservationSortBox}>
-                        <section className={styles.reservationBarTop}> {/* sort from high to low/low to high */}
-                            <button>Reservation Id<span className={styles.icon}><span className={styles.icon}><SwapVertIcon /></span></span></button>
-                            <button>Accommodation name<span className={styles.icon}><SwapVertIcon /></span></button>
-                            <button>Reserved dates<span className={styles.icon}><FilterListIcon /></span></button>
-                            <button>Requested on<span className={styles.icon}><FilterListIcon /></span></button>
-                            <button>Guest name<span className={styles.icon}><SwapVertIcon /></span></button>
-                            <button>Rate<span className={styles.icon}><SwapVertIcon /></span></button>
-                            <button>Payed<span className={styles.icon}><SwapVertIcon /></span></button>
-                            <button>Status<span className={styles.icon}><SwapVertIcon /></span></button>
-                        </section>
-                    </section>
-                    <section className={styles.reservationData}>
-                    <div className={styles.reservationRow}>
-                            <div className={styles.singleReservationRow}>1241254211`231231242142145125</div>
-                            <div className={styles.singleReservationRow}>Discover unique views in t-</div>
-                            <div className={styles.singleReservationRow}>April 29 - May 0-</div>
-                            <div className={styles.singleReservationRow}>April 26</div>
-                            <div className={styles.singleReservationRow}>Mike</div>
-                            <div className={styles.singleReservationRow}>€821</div>
-                            <div className={styles.singleReservationRow}>Yes</div>
-                            <div className={styles.singleReservationRow}>Accepted</div>
-                       </div>
-                       <div className={styles.reservationRow}>
-                            <div className={styles.singleReservationRow}>1241254215125</div>
-                            <div className={styles.singleReservationRow}>Discover unique views in t-</div>
-                            <div className={styles.singleReservationRow}>April 29 - May 0-</div>
-                            <div className={styles.singleReservationRow}>April 26</div>
-                            <div className={styles.singleReservationRow}>Mike</div>
-                            <div className={styles.singleReservationRow}>€821</div>
-                            <div className={styles.singleReservationRow}>Yes</div>
-                            <div className={styles.singleReservationRow}>Accepted</div>
-                        </div>
-                    </section>
-                </section>
-            </section>
-        </main>
-    );
-}
+  return (
+    <main className="page-body">
+      <h2>Reservations</h2>
+      <section className={styles.reservationContainer}>
+        <Pages />
+        <section className={styles.reservationContent}>
+          <div className={styles.reservationInfo}>
+            <h2>Manage Reservations</h2>
+            <p>
+              <EventIcon />
+              You can manage your reservations on your properties here.
+            </p>
+          </div>
+          <div className={styles.reservationButtons}>
+            <button>All</button>
+            <button>Incoming</button>
+            <button>Accepted</button>
+            <button>Cancelled</button>
+          </div>
+          <section className={styles.reservationData}>
+            <table className={styles.reservationTable}>
+                <thead>
+                    <tr>
+                        <th>
+                            Reservation Id
+                            <span className={styles.reservationIcons}><SwapVertIcon /></span>
+                        </th>
+                        <th>
+                            Accommodation name
+                            <span className={styles.reservationIcons}><SwapVertIcon /></span>
+                        </th>
+                        <th>
+                            Reserved dates
+                            <span className={styles.reservationIcons}><FilterListIcon /></span>
+                        </th>
+                        <th>
+                            Requested on
+                            <span className={styles.reservationIcons}><FilterListIcon /></span>
+                        </th>
+                        <th>
+                            Guest name
+                            <span className={styles.reservationIcons}><SwapVertIcon /></span>
+                        </th>
+                        <th>
+                            Rate
+                            <span className={styles.reservationIcons}><SwapVertIcon /></span>
+                        </th>
+                        <th>
+                            Payed
+                            <span className={styles.reservationIcons}><SwapVertIcon /></span>
+                        </th>
+                        <th>
+                            Status
+                            <span className={styles.reservationIcons}><SwapVertIcon /></span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className={styles.singleReservationRow}>1241254211</td>
+                        <td className={styles.singleReservationRow}>Discover unique views in t-</td>
+                        <td className={styles.singleReservationRow}>April 29 - May 0-</td>
+                        <td className={styles.singleReservationRow}>April 26</td>
+                        <td className={styles.singleReservationRow}>Mike</td>
+                        <td className={styles.singleReservationRow}>€821</td>
+                        <td className={styles.singleReservationRow}>Yes</td>
+                        <td className={styles.singleReservationRow}>Accepted</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.singleReservationRow}>1241254215125</td>
+                        <td className={styles.singleReservationRow}>Discover unique views in t-</td>
+                        <td className={styles.singleReservationRow}>April 29 - May 0-</td>
+                        <td className={styles.singleReservationRow}>April 26</td>
+                        <td className={styles.singleReservationRow}>Mike</td>
+                        <td className={styles.singleReservationRow}>€821</td>
+                        <td className={styles.singleReservationRow}>Yes</td>
+                        <td className={styles.singleReservationRow}>Accepted</td>
+                    </tr>
+              </tbody>
+            </table>
+          </section>
+        </section>
+      </section>
+    </main>
+  );
+};
 
 export default HostReservations;
