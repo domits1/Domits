b046064b8c6d2118385fca57e2b9dd5d7a469163
smallproject <j.a.villas@live.com.ph>
2025-05-06 13:01:55 +0200

    added multilanguage feature

--- web/src/pages/travelinnovationlab/travelinnovation.js
+++ web/src/pages/travelinnovationlab/travelinnovation.js
@@ -1,38 +1,51 @@
-import React from "react";
+import React, { useContext } from "react";
 import huis from "../../images/til-hero.png";
 import travel from "../../images/icons/Travel.png";
 import './ninedots.css';
+import { useNavigate, useLocation } from 'react-router-dom';
+import {LanguageContext} from "../../context/LanguageContext.js";
+import en from "../../content/en.json";
+import nl from "../../content/nl.json";
+import de from "../../content/de.json";
+import es from "../../content/es.json";
+
+const contentByLanguage = {
+  en,
+  nl,
+  de,
+  es,
+};
 
 function TravelInnovation() {
+    const {language} = useContext(LanguageContext);
+    const travelInnovationContent = contentByLanguage[language]?.travelInnovationLabs;
     return (
         <div>
             <div className="image-container" style={{ backgroundImage: `url(${huis})` }}>
-                <h1 className="title">Travel Innovation Labs</h1>
-                <h3 className="subtitle">Sustainable Value Creation</h3>
+                <h1 className="title">{travelInnovationContent.title}</h1>
+                <h3 className="subtitle">{travelInnovationContent.description}</h3>
             </div>
             <div className="header-two">
                 <div className="header-text">
-                    <h2 className="text-h2">Travel Innovation Flywheel</h2>
+                    <h2 className="text-h2">{travelInnovationContent.groups.flyWheel.title}</h2>
                     <p className="text-description">
-                        The mission of Domits is to simplify travel. For sustainable value creation,
-                        we have three innovation labs which come together in our flywheel. The labs
-                        are travel finance, travel business, and travel & daily life.
+                        {travelInnovationContent.groups.flyWheel.description}
                     </p>
                 </div>
                 <img className="travel-image" src={travel} alt="Scenic travel destination" />
             </div>
             <div className="travel-container">
                 <div className="travel-item">
-                    <h2 className="travel-subtitle">Travel Finance</h2>
-                    <p>The core starts with simplifying travel finance. We focus on travel payments, travel capital, travel insurance, and travel investing.</p>
+                    <h2 className="travel-subtitle">{travelInnovationContent.groups.finance.title}</h2>
+                    <p>{travelInnovationContent.groups.finance.description}</p>
                 </div>
                 <div className="travel-item">
-                    <h2 className="travel-subtitle">Travel Business</h2>
-                    <p>To serve all travel stakeholders, we have four business divisions: #1 growth, #2 people, #3 cloud ICT, and #4 AI data.</p>
+                    <h2 className="travel-subtitle">{travelInnovationContent.groups.business.title}</h2>
+                    <p>{travelInnovationContent.groups.business.description}</p>
                 </div>
                 <div className="travel-item">
-                    <h2 className="travel-subtitle">Travel & Daily Life</h2>
-                    <p>The financial business flow provides opportunities to experience limitless freedom for travel & daily life, like accommodation services, mobility services, municipal services, local services, and many more...</p>
+                    <h2 className="travel-subtitle">{travelInnovationContent.groups.dailyLife.title}</h2>
+                    <p>{travelInnovationContent.groups.dailyLife.description}</p>
                 </div>
             </div>
         </div>
