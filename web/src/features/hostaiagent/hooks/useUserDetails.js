7b1225c3d5eaa8d09fa73029cdf4f033d84ec521
Marijn Klappe <mf.klappe@gmail.com>
2025-05-07 09:32:09 +0200

    useUserDetails functie herschreven

--- web/src/features/hostaiagent/hooks/useUserDetails.js
+++ web/src/features/hostaiagent/hooks/useUserDetails.js
@@ -1,30 +1,27 @@
-import { useState, useEffect } from 'react';
 import { Auth } from 'aws-amplify';
 
 const useUserDetails = (setMessages, fetchPollySpeech) => {
-    const [userId, setUserId] = useState(null);
-    const [username, setUserName] = useState('Host');
 
-    useEffect(() => {
-        const setUserDetails = async () => {
-            try {
-                const userInfo = await Auth.currentUserInfo();
-                setUserId(userInfo.attributes.sub);
-                const name = userInfo.attributes['custom:username'] || 'Host';
-                setUserName(name);
-                const greeting = `Hello, ${name}! Please choose an option:`;
-                const messageId = Date.now();
-                setMessages([{ id: messageId, text: greeting, sender: 'bot', contentType: 'text' }]);
-                fetchPollySpeech(greeting, messageId);
-            } catch (error) {
-                console.error('Error fetching user details:', error);
+    const getUserDetails = async () => {
+        try {
+            const userInfo = await Auth.currentUserInfo();
+
+            if(!userInfo){
+                return {userId: null,username: null}
             }
-        };
 
-        setUserDetails();
-    }, []); // Only runs on mount
+            const userId = userInfo.attributes.sub
+            const username  = userInfo.attributes['custom:username'] || 'Host';
 
-    return { userId, username };
+            return {userId, username}
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    getUserDetails();
+
+    return getUserDetails();
 };
 
 export default useUserDetails;
