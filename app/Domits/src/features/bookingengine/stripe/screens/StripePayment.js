686dd2d689779a9701ef97d93c8bf5b5d6b15eb7
Sander van Goch <sander.van.goch2003@gmail.com>
2025-05-08 08:39:22 +0200

    Start of stripepayment UI (BookingEngineScreen is only formatting)

--- app/Domits/src/features/bookingengine/stripe/screens/StripePayment.js
+++ app/Domits/src/features/bookingengine/stripe/screens/StripePayment.js
@@ -1,22 +1,34 @@
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import {StripeProvider} from "@stripe/stripe-react-native";
-import {useEffect} from "react";
+import React, {useEffect} from "react";
+import Header from "../components/Header";
+import {S3URL} from "../../../../store/constants";
+import {styles} from "../styles/styles";
 
-const StripePayment = () => {
+const StripePayment = ({navigation, route}) => {
+    const property = route.params.property;
 
     useEffect(() => {
+        console.log(`${S3URL}${property.images[0].key}`)
         // Create booking (Should return a paymentIntent, ephemeralKey and customerId (Stripe id)
         // See https://docs.stripe.com/payments/accept-a-payment?platform=react-native#setup-server-side
     })
 
     return (
-        <StripeProvider
-            publishableKey={"pk_test_51OAG6OGiInrsWMEcRkwvuQw92Pnmjz9XIGeJf97hnA3Jk551czhUgQPoNwiCJKLnf05K6N2ZYKlXyr4p4qL8dXvk00sxduWZd3"}
-            urlScheme={"com.domits.domits"}>
-            <View>
-                <Text>Something</Text>
+        <>
+            <View style={styles.container}>
+                <Header navigation={navigation}/>
+                <Image
+                    source={{uri: `${S3URL}${property.images[0].key}`}}
+                    style={styles.image}
+                />
+                <Text>{property.property.title}</Text>
             </View>
-        </StripeProvider>
+            <StripeProvider
+                publishableKey={"pk_test_51OAG6OGiInrsWMEcRkwvuQw92Pnmjz9XIGeJf97hnA3Jk551czhUgQPoNwiCJKLnf05K6N2ZYKlXyr4p4qL8dXvk00sxduWZd3"}
+                urlScheme={"com.domits.domits"}>
+            </StripeProvider>
+        </>
     );
 };
 
---
+++ app/Domits/src/features/bookingengine/stripe/styles/styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+    },
+    image: {
+        marginTop: 70,
+        alignSelf: "center",
+        width: "90%",
+        height: "100%",
+        maxHeight: 100,
+        borderRadius: 20
+    }
+})
