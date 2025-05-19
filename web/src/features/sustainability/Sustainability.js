86746b24acd23b84fa440b852771d1aa1bcd0c2d
smallproject <j.a.villas@live.com.ph>
2025-05-19 10:23:23 +0200

    implemented multilanguage feature

--- web/src/features/sustainability/Sustainability.js
+++ web/src/features/sustainability/Sustainability.js
@@ -1,55 +1,58 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Sustainability.css";
 import {submitAccommodation} from "../hostonboarding/services/SubmitAccommodation";
+import {LanguageContext} from "../../context/LanguageContext.js";
+import en from "../../content/en.json";
+import nl from "../../content/nl.json";
+import de from "../../content/de.json";
+import es from "../../content/es.json";
+import ReactMarkDown from "react-markdown";
+
+const contentByLanguage = {
+  en,
+  nl,
+  de,
+  es,
+};
 
 const Sustainability = () => {
+  const {language} =  useContext(LanguageContext);
+  const sustainabilityContent = contentByLanguage[language]?.sustainability;
+
     return (
         <div className="sustainability">
-            <h1>Sustainability</h1>
+            <h1>{sustainabilityContent.title}</h1>
             <hr/>
-            <h2>Why choose Domits for sustainability?</h2>
+            <h2>{sustainabilityContent.subtitle}</h2>
             <hr/>
 
             <section id="sustainable-business">
                 <h3>
-                    1. Sustainable <strong>Business</strong>
+                <ReactMarkDown>{sustainabilityContent.business.title}</ReactMarkDown>
                 </h3>
-                <strong>Recycling plan with bins</strong>
+                <strong>{sustainabilityContent.business.subtitle}</strong>
                 <p>
-                    Domits is starting out simple with making conscious decisions like
-                    choosing more energy-saving appliances and will continue to improve
-                    this 1% every day. With our food waste policy we don’t want to be to
-                    rigid and starting with a flexible recycling plan for glass, paper,
-                    plastic and organic. For biodiversity and ecosystems we’re aiming to
-                    eat more organic food, use Eco-friendly cleaning products and have
-                    paperless procedures.
+                    {sustainabilityContent.business.description}
                 </p>
             </section>
 
             <hr/>
             <section id="sustainable-accommodations">
                 <h3>
-                    2. Sustainable <strong>Accommodations</strong>
+                    <ReactMarkDown>{sustainabilityContent.accommodations.title}</ReactMarkDown>
                 </h3>
-                <strong>Certification</strong>
+                <strong>{sustainabilityContent.accommodations.subtitle}</strong>
                 <p>
-                    Domits is actively focusing on onboarding hosts that are already
-                    sustainable. If they’re not yet, we’re supporting them step-by-step to
-                    increase awareness. At a certain point we want to give hosts
-                    certifications as a proof of sustainability with energy, waste,
-                    biodiversity, ecosystems, destinations and community.
+                   {sustainabilityContent.accommodations.description}
                 </p>
             </section>
             <hr/>
             <section id="sustainable-guest-experience">
                 <h3>
-                    3. Sustainable <strong>Guest Experience</strong>
+                    <ReactMarkDown>{sustainabilityContent.experience.title}</ReactMarkDown>
                 </h3>
                 <p>
-                    From the first touch point with Domits for guest we want to let them
-                    feel that they are traveling as sustainable as possible. Domits begins
-                    with having sustainability filters and a large overview of all
-                    eco-friendly amenities.
+                    {sustainabilityContent.experience.description}
                 </p>
 
             </section>
