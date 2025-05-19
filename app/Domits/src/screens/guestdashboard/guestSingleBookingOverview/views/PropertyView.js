df9e1038de8429772695547304ac015839667630
Sandervg03 <sander.van.goch2003@gmail.com>
2025-05-19 11:18:52 +0200

    Cleans up bookingOverview

--- app/Domits/src/screens/guestdashboard/guestSingleBookingOverview/views/PropertyView.js
+++ app/Domits/src/screens/guestdashboard/guestSingleBookingOverview/views/PropertyView.js
@@ -1,73 +1,25 @@
-import {Image, Text, TouchableOpacity, View} from 'react-native';
-import {S3URL} from '../../../../store/constants';
+import {Text, View} from 'react-native';
 import {styles} from '../styles/singleBooking';
-import {useState} from 'react';
-import AmenitiesPopup from '../../../propertyDetailsScreen/components/amenitiesPopup';
 import Spacer from '../../../../components/Spacer';
+import PropertyImage from "../components/PropertyImage";
+import PropertyLocation from "../components/PropertyLocation";
+import PropertyAmenities from "../components/PropertyAmenities";
+import BookingDetailsView from "./BookingView";
 
 const PropertyView = ({property, booking}) => {
-  const [showAmenitiesModal, setShowAmenitiesModal] = useState(false);
   return (
     <View style={styles.propertyContainer}>
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.image}
-          source={{uri: `${S3URL}${property.images[0].key}`}}
-        />
-      </View>
+      <PropertyImage imageKey={property.images[0].key} />
       <View style={styles.infoContainer}>
-        {property.location.street && (
-          <View>
-            <Text style={styles.locationTitle}>
-              {property.location.city}, {property.location.country}
-            </Text>
-            <Text style={styles.locationSubTitle}>
-              {property.location.street} {property.location.houseNumber}
-            </Text>
-          </View>
-        )}
-        {!property.location.street && (
-          <View>
-            <Text style={styles.locationTitle}>
-              {property.location.city}, {property.location.country}
-            </Text>
-          </View>
-        )}
+        <PropertyLocation location={property.location} />
         <Spacer />
         <Text style={styles.bookingSubTitle} numberOfLines={5}>
           {property.property.description}
         </Text>
         <Spacer />
-        <TouchableOpacity onPress={() => setShowAmenitiesModal(true)}>
-          <View style={styles.showAmenitiesContainer}>
-            <Text style={styles.showAmenitiesText}>Show amenities</Text>
-          </View>
-        </TouchableOpacity>
-        {showAmenitiesModal && (
-          <AmenitiesPopup
-            propertyAmenities={property.amenities}
-            onClose={() => setShowAmenitiesModal(false)}
-          />
-        )}
+        <PropertyAmenities amenities={property.amenities} />
         <Spacer />
-        <View style={styles.bookingContainer}>
-          <Text style={styles.bookingTitle}>Booking</Text>
-          <Text style={styles.bookingSubTitle}>Status: {booking.status.S}</Text>
-          <Text style={styles.bookingSubTitle}>
-            Period:{' '}
-            {
-              new Date(parseFloat(booking.arrivalDate.N))
-                .toISOString()
-                .split('T')[0]
-            }{' '}
-            -{' '}
-            {
-              new Date(parseFloat(booking.departureDate.N))
-                .toISOString()
-                .split('T')[0]
-            }
-          </Text>
-        </View>
+        <BookingDetailsView booking={booking} />
       </View>
     </View>
   );
