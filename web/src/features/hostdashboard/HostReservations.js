e7b99e2fceb4b228f9f7408d2ed84af3be48639d
Randy Luo <117186456+Randy20020603@users.noreply.github.com>
2025-05-21 13:28:07 +0200

    fetch to the right API

--- web/src/features/hostdashboard/HostReservations.js
+++ web/src/features/hostdashboard/HostReservations.js
@@ -107,19 +107,17 @@ const HostReservations = () => {
         } else {
             setIsLoading(true);
             try {
-                const response = await fetch('https://5ycj23b6db.execute-api.eu-north-1.amazonaws.com/default/FetchReservations', {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        HostID: userId,
-                        index: index ? index : null
-                    }),
-                    headers: {'Content-type': 'application/json; charset=UTF-8',
-                    }
+                const response = await fetch('https://92a7z9y2m5.execute-api.eu-north-1.amazonaws.com/development/bookings?readType=hostId', {
+                    method: "GET",
+                    headers: {
+                        Authorization: "eyJraWQiOiJYdkVKWjI2RlYxVngxSktIVFo4R1k1MlZlTzBnNGJCMFdiS1BRUGg3alRFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJjZTkzZjM3Ny05YWMwLTQ2YzItOWQ0NS1jZjIwZDMzZGNjMzMiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuZXUtbm9ydGgtMS5hbWF6b25hd3MuY29tXC9ldS1ub3J0aC0xX21QeE5odlNGWCIsImNsaWVudF9pZCI6Ijc4amZyZmhwZGVkNm1lZXZsbHBmbW83M21vIiwib3JpZ2luX2p0aSI6IjcxOTRkNzg0LWNlNzYtNDk3Zi1hYjQyLWZjYWM2NWE3MzhiOCIsImV2ZW50X2lkIjoiNDhiZmMzN2YtMDQ4ZC00ZmM4LWE3OTItMTNjNDkxY2Y1MzA1IiwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJhd3MuY29nbml0by5zaWduaW4udXNlci5hZG1pbiIsImF1dGhfdGltZSI6MTc0NzgxNTMwNSwiZXhwIjoxNzQ3ODIwNTY1LCJpYXQiOjE3NDc4MTY5NjUsImp0aSI6IjBlMTRkMWMwLWMzZDQtNDdkZi1iM2M4LTg5M2QxZjFjNjk5OCIsInVzZXJuYW1lIjoiY2U5M2YzNzctOWFjMC00NmMyLTlkNDUtY2YyMGQzM2RjYzMzIn0.CdX-Om5MSEFd94ZFOI9qAl83C7Ei_zdIB7U2voM6iaGJyhMHnmXtB7MbK3fxQPfeZCH9RCXHrwcUsRSbGHJv6824N6VVBT3W4sI0Cy_iwmfFS4u3NiiCEwryI4kcB4dTTqQRBrIYJFgiQhziqGt0HA9CyW2AzRlO-o1Y6YDdBxGIuAYViX2G3anHlBt3fT87pK_C7DbTPUP7myvbKKHGD5L4mzxbo35yOdBQPbhMLwVn6yKa1EzRPGGxBO1x4V15-hRpe9mZznWiDyosU_NXa4fFjrSKJ01POTV0IKokdA7UJ1ryvOVDjNeZ088dqHW_zLnP9dz1lymS8dvcAcHsjA"
+                    },
                 });
                 if (!response.ok) {
                     throw new Error('Failed to fetch');
                 }
                 const data = await response.json();
+                console.log("Dit is de data: ", data);
                 const parsedData = JSON.parse(data.body);
                 await handleData(parsedData);
             } catch (error) {
