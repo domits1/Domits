8a010b29c938f623130dd46a841364906613158b
bambaisbanned <67043806+Bambaclad1@users.noreply.github.com>
2025-06-04 09:55:08 +0200

    change test to template test because of failing test

--- backend/test/General-Bookings-CRUD-Bookings-develop/unit.test.js
+++ backend/test/General-Bookings-CRUD-Bookings-develop/unit.test.js
@@ -1,4 +1,6 @@
-// View https://jestjs.io/ docs for information on the testing framework we use.
+import {expect, it, jest, test} from '@jest/globals';
+import {handler} from "../../functions/General-Bookings-CRUD-Bookings-develop/index.js";
+
 describe("template test", () => {
     it("should say 1 + 1 = 2", () => {
         expect(1 + 1).toBe(2);
