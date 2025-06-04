33217fc93ce256658a0c6e866e16bd3119b6295d
Sander van Goch <sander.van.goch2003@gmail.com>
2025-06-04 16:54:20 +0200

    .\Quickfix backend

--- frontend/web/src/features/bookingengine/listingdetails/components/pricing.js
+++ frontend/web/src/features/bookingengine/listingdetails/components/pricing.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Pricing = ({ pricing, nights }) => {
   return (
     <div className="pricing-container">
@@ -7,12 +9,20 @@ const Pricing = ({ pricing, nights }) => {
         </div>
         <div className="pricing-price">${nights * pricing.roomRate}</div>
       </div>
+      <hr />
       <div className="pricing-description-and-price">
-        <div className="pricing-description">Cleaning fee</div>
-        <div className="pricing-price">${pricing.cleaning}</div>
+        <div className="pricing-description"><strong>Cleaning fee</strong></div>
       </div>
       <div className="pricing-description-and-price">
-        <div className="pricing-description">Domits service fee</div>
+        <div className="pricing-description">{nights} night{nights > 1 ? "s" : ""} x ${pricing.cleaning} a night</div>
+        <div className="pricing-price">${pricing.cleaning * nights}</div>
+      </div>
+      <hr />
+      <div className="pricing-description-and-price">
+        <div className="pricing-description"><strong>Domits service fee</strong></div>
+      </div>
+      <div className="pricing-description-and-price">
+        <div className="pricing-description">15% x ${pricing.roomRate} a night</div>
         <div className="pricing-price">${pricing.roomRate * 0.15 * nights}</div>
       </div>
       <div className="pricing-description-and-price">
@@ -21,7 +31,7 @@ const Pricing = ({ pricing, nights }) => {
         </div>
         <div className="pricing-price">
           <h2>
-            ${nights * pricing.roomRate * 1.15 + pricing.cleaning}
+            ${(pricing.roomRate * 1.15 * nights + pricing.cleaning * nights).toFixed(2)}
           </h2>
         </div>
       </div>
