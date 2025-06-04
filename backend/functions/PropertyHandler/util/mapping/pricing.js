acdfd07e69ed61e30f9d0ad15a4c6eb3beaa7d33
Sander van Goch <sander.van.goch2003@gmail.com>
2025-06-04 16:58:24 +0200

    .

--- backend/functions/PropertyHandler/util/mapping/pricing.js
+++ backend/functions/PropertyHandler/util/mapping/pricing.js
@@ -3,6 +3,7 @@ import { PropertyPricing } from "../../business/model/propertyPricing.js";
 export class PricingMapping {
 
     static mapDatabaseEntryToPricing(pricingEntry) {
+        console.log(pricingEntry);
         return new PropertyPricing(
             pricingEntry.property_id.S,
             parseFloat(pricingEntry.roomRate.N),
