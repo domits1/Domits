d09adc358ec0550049ead9d56acb31d3b9668e03
bambaisbanned <67043806+Bambaclad1@users.noreply.github.com>
2025-06-16 16:08:16 +0200

    Moved block code to a seperate file which handles propertyfetching

---
+++ frontend/web/src/features/bookingengine/services/FetchPropertyDetails.js
@@ -0,0 +1,30 @@
+import calculateDifferenceInNights from "../utils/CalculateDifferenceInNights";
+
+const FetchPropertyDetails = async (propertyId, checkInDate, checkOutDate) => {
+  try {
+    const response = await fetch(
+      `https://wkmwpwurbc.execute-api.eu-north-1.amazonaws.com/default/property/bookingEngine/listingDetails?property=${propertyId}`
+    );
+    const accommodationData = await response.json();
+
+    console.log("Accommodation Data:", accommodationData);
+    const differenceInDays = calculateDifferenceInNights(checkInDate, checkOutDate);
+    const totalRate = Math.round((accommodationData.pricing.roomRate * 1.15 + accommodationData.pricing.cleaning) * differenceInDays * 100) / 100;
+    return { 
+        roomRate: accommodationData.pricing.roomRate,
+        cleaning: accommodationData.pricing.cleaning,
+        images: accommodationData.images,
+        total: totalRate,
+        title: accommodationData.property.title,
+        city: accommodationData.location.city,
+        country: accommodationData.location.country,
+        differenceInDays: differenceInDays,
+        hostId: accommodationData.property.hostId,
+    }
+  } catch (error) {
+    console.error("Unable to fetch property data.", error);
+    
+  }
+};
+
+export default FetchPropertyDetails;
