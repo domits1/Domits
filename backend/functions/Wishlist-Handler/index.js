33ba22755e3007b0e60df079de488d4828155fca
Mary-Anyanwu <maryanyanwu1@yahoo.com>
2025-06-19 16:48:53 +0200

    🚦 Changed 405 error to 404 when method or path not found in Lambda handler

--- backend/functions/Wishlist-Handler/index.js
+++ backend/functions/Wishlist-Handler/index.js
@@ -8,28 +8,39 @@ export const handler = async (event) => {
   }
 
   try {
-    switch (event.httpMethod) {
-      case "POST":
-        return await controller.create(event);
-      case "GET":
-        return await controller.read(event);
-      case "DELETE":
-        return await controller.remove(event);
-      case "PUT":
-        return await controller.addList(event);
-      case "PATCH":
-        return await controller.update(event);
-      default:
-        return {
-          statusCode: 405,
-          body: JSON.stringify({ message: "Unsupported method" })
-        };
-    }
+    return await (async () => {
+      const { httpMethod, resource } = event;
+
+      if (resource === "/wishlist") {
+        switch (httpMethod) {
+          case "POST":
+            return await controller.create(event);
+          case "GET":
+            return await controller.read(event);
+          case "DELETE":
+            return await controller.remove(event);
+          case "PUT":
+            return await controller.addList(event);
+          case "PATCH":
+            return await controller.update(event);
+          default:
+            return {
+              statusCode: 404,
+              body: "Method not found." 
+            };
+        }
+      }
+
+      return {
+        statusCode: 404,
+        body: "Path not found."
+      };
+    })();
   } catch (error) {
-    console.error("Error in handler:", error);
+    console.error(error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: "Something went wrong", error: error.message })
+      body: "Something went wrong, please contact support."
     };
   }
 };
