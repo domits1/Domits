0d438fbfd4e2b5916ae7896fefb9df0cd3f4d0b9
Moncef Elmzouri <elmzourim@outlook.com>
2025-09-09 14:11:55 +0200

    Revert "Revert "Landing: move dummy reviews to store and import from it""
    This reverts commit b81b4acb20ca0aaadd72804818be814ae71dc5d5.

--- frontend/web/src/pages/landingpage/Landing.js
+++ frontend/web/src/pages/landingpage/Landing.js
@@ -21,6 +21,7 @@ import nl from "../../content/nl.json";
 import de from "../../content/de.json";
 import es from "../../content/es.json";
 import ReactMarkDown from "react-markdown";
+import { reviews } from "./store/reviews";
 import { handleContactFormSubmission } from "../../services/contactService";
 import ContactForm from "./ContactForm.jsx";
 import FaqItem from "./FaqItem.jsx";
@@ -96,56 +97,7 @@ function Landing() {
   const isAllowedKey = (key) => ALLOWED_KEYS.includes(key);
   const isValidPhonePaste = (text) => /^[0-9+\s]*$/.test(text);
 
-  const reviews = [
-    {
-      id: 1,
-      text:
-        "Renting out my home through this website has been a wonderful experience. The user-friendly interface and the reliable platform make it easy for me to list my property. The booking system works flawlessly, and I always receive timely notifications when a reservation is made. Communication with guests is smooth, allowing me to offer a personal and hassle-free service. Thanks to this website, I am confident that my home is in good hands, and the positive feedback from my guests reaffirms this every time!",
-      author: "Rick Terp",
-      location: "Host from the Netherlands",
-      img: "https://pbs.twimg.com/media/FNA5U8jXwAURgR-?format=jpg&name=4096x4096",
-    },
-    {
-      id: 2,
-      text:
-        "Renting out my boat through this platform was a fantastic experience. Everything went smoothly and professionally, from the booking to the communication with renters. Perfect for boat owners!",
-      author: "Melissa Steenberk",
-      location: "Host from the Netherlands",
-      img: "https://pbs.twimg.com/media/FNA5U8jXwAURgR-?format=jpg&name=4096x4096",
-    },
-    {
-      id: 3,
-      text:
-        "As the owner of a luxury yacht company, this platform offers us the perfect opportunity to rent out our fleet easily and efficiently. From the user-friendly system to the excellent customer support, everything is flawlessly organized. Our clients appreciate the quality and luxury of our yachts, and thanks to the platform, we can provide them with a hassle-free booking experience. The team behind the platform ensures that our yachts receive optimal visibility for potential renters, resulting in frequent and reliable bookings. A valuable asset for our business!",
-      author: "James Heck",
-      location: "Owner of a luxury yacht company",
-      img: "https://pbs.twimg.com/media/FNA5U8jXwAURgR-?format=jpg&name=4096x4096",
-    },
-    {
-      id: 4,
-      text:
-        "I recently hosted my website with Domits in the UK, and the experience has been exceptional. The platform is user-friendly, allowing for quick setup and seamless integration. Speed and reliability are top-notch, with minimal downtime, ensuring my site is always accessible. The customer support team is also extremely helpful, addressing any issues promptly and professionally. If you're looking for a solid hosting solution in the UK, this service offers great performance, security, and value for money. Highly recommended for anyone serious about a stable online presence!",
-      author: "Jaimee Becker",
-      location: "Host from UK",
-      img: "https://pbs.twimg.com/media/FNA5U8jXwAURgR-?format=jpg&name=4096x4096",
-    },
-    {
-      id: 5,
-      text:
-        "Reliable and Efficient Hosting from Germany I recently switched to a hosting provider based in Germany, and it’s been an excellent decision. The platform is fast and stable, offering great performance with little to no downtime. The setup process was straightforward, and the service provides excellent security features, which is especially important for websites dealing with sensitive data. The German-based servers have shown impressive speed for both local and international visitors, making it a fantastic choice for businesses or personal websites looking for reliability and performance. Highly recommended for those seeking a strong hosting solution from Germany!",
-      author: "Maurice von Dorn",
-      location: "Host from Germany",
-      img: "https://pbs.twimg.com/media/FNA5U8jXwAURgR-?format=jpg&name=4096x4096",
-    },
-    {
-      id: 6,
-      text:
-        "As the owner of Amode, I’ve had the opportunity to host several websites on various platforms, and the service we provide has consistently exceeded expectations. Our hosting solutions are designed to offer a seamless, high-performance experience with reliable uptime, top-notch security features, and a user-friendly interface. Whether you're running a small business or a larger operation, our infrastructure is built to scale efficiently and ensure your site is always performing at its best. At Amode, we prioritize customer satisfaction, and our support team is available around the clock to help with any issues that may arise. We take pride in offering hosting that’s as robust as it is reliable, making us a trusted choice for clients worldwide.",
-      author: "Laisa Feldt",
-      location: "Owner at Amode",
-      img: "https://pbs.twimg.com/media/FNA5U8jXwAURgR-?format=jpg&name=4096x4096",
-    },
-  ];
+  
 
   const [reviewStartIndex, setReviewStartIndex] = useState(0);
   const advanceReviews = () => {
