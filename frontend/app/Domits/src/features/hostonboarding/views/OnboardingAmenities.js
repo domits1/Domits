49d66c422d0d179c068358addba06e64e3d62905
Stephanie Lahmann <Stephanie_Lahmann@outlook.com>
2025-09-09 17:08:25 +0200

    🚧 amenities checkboxes
    • added checkboxes for selected few amenities
    • refactored some functions for amenities into an utils file
    • updated how selected amenities are tracked

--- frontend/app/Domits/src/features/hostonboarding/views/OnboardingAmenities.js
+++ frontend/app/Domits/src/features/hostonboarding/views/OnboardingAmenities.js
@@ -1,23 +1,17 @@
 import {styles} from "../styles/HostOnboardingStyles";
 import {ScrollView, Text, View} from "react-native";
 import TranslatedText from "../../translation/components/TranslatedText";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
+import amenityUtils from "../../../hooks/AmenityUtils";
+import CheckBox from "@react-native-community/checkbox";
 
 const OnboardingAmenities = ({formData, updateFormData, reportValidity, markVisited}) => {
-  const amenities = [
-    { name: 'Wi-fi', icon: '' },
-    { name: 'Tv with cable/satellite', icon: '' },
-    { name: 'Heating', icon: ''},
-    { name: 'Hot water', icon: ''},
-    { name: 'Bed linens', icon: ''},
-    { name: 'Toilet paper', icon: ''},
-    { name: 'Air conditioning', icon: ''},
-    { name: 'Towels', icon: ''},
-    { name: 'Extra pillows and blankets', icon: ''},
-    { name: 'Soap and shampoo', icon: ''},
-  ];
-
   const [selected, setSelected] = useState([]);
+  const amenityNames = ['Wi-Fi', 'TV with cable/satellite', 'Heating', 'Hot water', 'Bed linens', 'Toilet paper', 'Air conditioning', 'Towels', 'Extra pillows and blankets', 'Soap and shampoo']
+
+  const displayedAmenities = amenityNames
+      .map(amenityUtils.getAmenityByName)
+      .filter(Boolean);
 
   const toggleAmenity = (name) => {
     setSelected(prev =>
@@ -27,8 +21,29 @@ const OnboardingAmenities = ({formData, updateFormData, reportValidity, markVisi
     );
   };
 
+  const amenityCheckbox = (amenityName) => {
+    const amenityItem = amenityUtils.getAmenityByName(amenityName);
+    const isSelected = selected.includes(amenityName);
+
+    return (
+        <View style={styles.checkboxContainer}>
+          <CheckBox
+              disabled={false}
+              value={isSelected}
+              onValueChange={() => toggleAmenity(amenityName)}
+          />
+          {amenityItem.icon}
+          <Text>
+            <TranslatedText
+                textToTranslate={amenityItem.amenity}/>
+          </Text>
+        </View>
+    )
+  }
+
   useEffect(() => {
     markVisited(true);
+    reportValidity(true); // Checkboxes not mandatory
   }, [])
 
   return (
@@ -41,6 +56,10 @@ const OnboardingAmenities = ({formData, updateFormData, reportValidity, markVisi
             <TranslatedText textToTranslate={"You can add more facilities after publishing your property."}/>
           </Text>
           <ScrollView contentContainerStyle={styles.scrollContainer}>
+
+            {displayedAmenities.map(amenityName =>
+                amenityCheckbox(amenityName)
+            )}
 
           </ScrollView>
         </View>
