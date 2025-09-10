f457e72920d5513906dc285607921266e56c968e
Moncef Elmzouri <elmzourim@outlook.com>
2025-09-10 10:20:04 +0200

    fix(guest): correct getAccessToken import path in bookingContainer

--- frontend/web/src/features/bookingengine/listingdetails/views/bookingContainer.js
+++ frontend/web/src/features/bookingengine/listingdetails/views/bookingContainer.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getAccessToken } from "../../../services/getAccessToken";
+import { getAccessToken } from "../../../../services/getAccessToken";
 import DateSelectionContainer from "./dateSelectionContainer";
 import GuestSelectionContainer from "./guestSelectionContainer";
 import Pricing from "../components/pricing";
