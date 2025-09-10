eb938628ceb38d3b40a2f3075929160512ee3dad
Alessio12-tech <alessioitofo88@gmail.com>
2025-09-10 16:37:06 +0200

    feat: implement core functionality for Alessio-Revenue service, including user authentication, data retrieval, and response handling

--- backend/events/PropertyHandler/post.js
+++ backend/events/PropertyHandler/post.js
@@ -6,17 +6,17 @@ async function main() {
     console.log(await handler({
         httpMethod: "POST",
         headers: {
-            Authorization: await getHostAuthToken()
+            Authorization: "eyJraWQiOiJYdkVKWjI2RlYxVngxSktIVFo4R1k1MlZlTzBnNGJCMFdiS1BRUGg3alRFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJmMDg5OTlkMy02OTdjLTQ0YTAtYjM4OC1hNDE0MTc4ZDRjNWIiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuZXUtbm9ydGgtMS5hbWF6b25hd3MuY29tXC9ldS1ub3J0aC0xX21QeE5odlNGWCIsImNsaWVudF9pZCI6Ijc4amZyZmhwZGVkNm1lZXZsbHBmbW83M21vIiwib3JpZ2luX2p0aSI6IjJjNGJjZDQ4LWQzNzctNGI5MC05NWU3LTI4ZjQ2MDBiOTIxZCIsImV2ZW50X2lkIjoiMDQwYWJmMzgtOTQ2OS00MzU2LTkzNDgtOTY4YWY3NTczNzNjIiwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJhd3MuY29nbml0by5zaWduaW4udXNlci5hZG1pbiIsImF1dGhfdGltZSI6MTc1NzQ4OTM2MiwiZXhwIjoxNzU3NTAyMzAwLCJpYXQiOjE3NTc0OTg3MDAsImp0aSI6ImM1NTIxMWIxLTJhMTEtNDM2Yi1hZjA0LTQwMjIyMzViZTU3MiIsInVzZXJuYW1lIjoiZjA4OTk5ZDMtNjk3Yy00NGEwLWIzODgtYTQxNDE3OGQ0YzViIn0.ozrEr-29s5UCRPvpJBLy9yAYjgYiVKJn6vI8S5WYgg5tQjCl0j4hufLaRnaNb4VoUcyHRCj13kTLEwRNrQdPgMZ1lVDnGCZ88_xtxiFrIOKMNTavMk10lL7AMpgM7rTib9TrrlL_9sAwlh-1mq3kLfo2AEv--QG9sY0hd9cAKxdjvTK9UWrNGhKkm0lSXqFT-DTL98Eb4szUS67K5MyhU9iJHjoHSytWEwv-UXJRBy6FSkbji1cO17xC20WxeZUwJcxAmZJiJ7PUOdqThGVq5N6lYKi3zrDp9nH74AVBT5-ooEmj22Mtv6RSmvY1sTnOHFmi31wGjVCJn_gWjGrqeA"
         },
         body: JSON.stringify({
             "property": {
                 "id": "e65ceab8-f77e-4d6f-b1f3-198f41b47f3e",
-                "hostId": "ce93f377-9ac0-46c2-9d45-cf20d33dcc33",
-                "title": "Charming Windmill Stay on Historic Castle Grounds in Maurik",
-                "subtitle": "Relax in a Beautifully Renovated Windmill Surrounded by Nature – Perfect for Cycling, Hiking, and Exploring Nearby Cities",
-                "description": "Experience a unique stay in a historic windmill built on the remnants of a medieval castle, offering a perfect blend of old-world charm and modern comfort. Fully renovated in 2006, this stunning windmill features spacious living areas, two cozy bedrooms, and two modern bathrooms. Enjoy breathtaking garden views and unwind in the tranquil surroundings. Whether you're cycling, hiking, or just relaxing on your private patio, Maurik offers a peaceful retreat with easy access to Utrecht, Den Bosch, Arnhem, and Nijmegen. Ideal for families or small groups looking to explore the beauty of the Dutch countryside.",
+                "hostId": "f08999d3-697c-44a0-b388-a414178d4c5b",
+                "title": "KaasBaas",
+                "subtitle": "Relaxing way to eat",
+                "description": "a historic windmill built on the remnants of a medieval castle, offering a perfect blend of old-world charm and modern comfort. Fully renovated in 2006, this stunning windmill features spacious living areas, two cozy bedrooms, and two modern bathrooms. Enjoy breathtaking garden views and unwind in the tranquil surroundings. Whether you're cycling, hiking, or just relaxing on your private patio, Maurik offers a peaceful retreat with easy access to Utrecht, Den Bosch, Arnhem, and Nijmegen. Ideal for families or small groups looking to explore the beauty of the Dutch countryside.",
                 "guestCapacity": 6,
-                "registrationNumber": "11",
+                "registrationNumber": "232323233233232239",
                 "status": "ACTIVE",
                 "propertyType": "House",
                 "createdAt": Date.now() + 100000,
