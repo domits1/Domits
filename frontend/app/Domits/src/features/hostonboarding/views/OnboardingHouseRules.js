cfe926fcb03c04c8266548d51829ebfe64333a16
Stephanie Lahmann <Stephanie_Lahmann@outlook.com>
2025-09-11 17:03:16 +0200

    🚧 (WIP) Checkin times
    • Updated property form data (time will be stored as INT minutes after midnight)
    • NPM installed timepicker
    • Started of views and logic of handling time selection

--- frontend/app/Domits/src/features/hostonboarding/views/OnboardingHouseRules.js
+++ frontend/app/Domits/src/features/hostonboarding/views/OnboardingHouseRules.js
@@ -1,13 +1,59 @@
 import {styles} from "../styles/HostOnboardingStyles";
-import {ScrollView, Text, View} from "react-native";
+import {ScrollView, Text, TouchableOpacity, View} from "react-native";
 import TranslatedText from "../../translation/components/TranslatedText";
 import React, {useState} from "react";
 import CheckBox from "@react-native-community/checkbox";
+import RNDateTimePicker from "@react-native-community/datetimepicker";
 
 const OnboardingHouseRules = ({formData, updateFormData, reportValidity, markVisited}) => {
+  const [showTimeModal, setShowTimeModal] = useState(false);
+
   const [allowSmoking, setAllowSmoking] = useState(false);
   const [allowPets, setAllowPets] = useState(false);
   const [allowParties, setAllowParties] = useState(false);
+
+  const [checkinFromTime, setCheckinFromTime] = useState(new Date());
+  const [checkinTillTime, setCheckinTillTime] = useState(new Date());
+  const [checkoutTillTime, setCheckoutTillTime] = useState(new Date());
+  const [checkoutFromTime, setCheckoutFromTime] = useState(new Date());
+
+  const handleTimeChange = (field, value, subField = null) => {
+    if (subField) {
+      updateFormData(prevData => ({
+        ...prevData,
+        [field]: {
+          ...prevData[field],
+          [subField]: value,
+        },
+      }));
+    } else {
+      updateFormData(prevData => ({
+        ...prevData,
+        [field]: value,
+      }));
+    }
+  };
+
+  // const handleChange = (event, selectedTime) => {
+  //   setShowTimeModal(false);
+  //   if (selectedTime) {
+  //     onTimeChange(selectedTime);
+  //   }
+  // };
+
+  const showTimePicker = (label, time, onTimeChange) => {
+    return (
+        <View>
+          <TranslatedText textToTranslate={label}/>
+          <RNDateTimePicker
+          value={time}
+          mode={"time"}
+          is24Hour={true}
+          onChange={onTimeChange}
+          />
+        </View>
+    )
+  }
 
   const AllowCheckBox = (changeFunction, checkedValue, text) => {
     return (
@@ -36,6 +82,12 @@ const OnboardingHouseRules = ({formData, updateFormData, reportValidity, markVis
               <TranslatedText textToTranslate={"Check-in/-out"}/>
             </Text>
 
+            <TouchableOpacity onPress={}>
+              <TranslatedText textToTranslate={"from"}/>
+              <Text>
+                00:00
+              </Text>
+            </TouchableOpacity>
 
             <Text style={styles.onboardingPageDescription}>
               <TranslatedText textToTranslate={"General rules"}/>
