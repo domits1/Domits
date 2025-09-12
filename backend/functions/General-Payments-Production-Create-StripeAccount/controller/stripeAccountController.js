fb15a2f7eb4cb404f52132935bc624123e217dda
Gurpreet Singh <Singhgurpreet14082002@gmail.com>
2025-09-12 10:36:22 +0200

    feat: Add StripeAccountController for handling Stripe account creation

---
+++ backend/functions/General-Payments-Production-Create-StripeAccount/controller/stripeAccountController.js
@@ -0,0 +1,42 @@
+import responsejson from "../util/constant/responseheader.json" with { type: 'json' };
+import StripeAccountService from "../business/service/stripeAccountService.js";
+const responseHeaderJSON = responsejson
+
+class StripeAccountController {
+
+    constructor() {
+        this.stripeAccountService = new StripeAccountService();
+    }
+
+
+    // -----------
+    // POST
+    // -----------
+
+    async create(event){
+        try {
+
+            const body = JSON.parse(event.body);
+            const response = await this.stripeAccountService.createStripeAccount(body);
+
+            return {
+                statusCode: 200,
+                headers: responseHeaderJSON,
+                response: {
+                    message: response.message,
+                    url: response.url
+                },
+            };
+        } catch (error) {
+            console.error(error);
+            return {
+                statusCode: 500,
+                headers: responseHeaderJSON,
+                message: error.message || "Something went wrong, please contact support."
+            }
+
+        }
+    }
+
+}
+export default StripeAccountController;
