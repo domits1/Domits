c235c240c2bb072ff16bbf5d2b164262bd6fd6f3
Ramandeep Singh <bambadevstuff@gmail.com>
2025-09-16 11:05:39 +0200

    fix: add longer time-out to booking-engine-requests to avoid CI errors/JEST errors

--- backend/test/PropertyHandler/end-to-end.test.js
+++ backend/test/PropertyHandler/end-to-end.test.js
@@ -154,5 +154,5 @@ describe("End-to-end tests", () => {
         expect(response.statusCode).toBe(200);
         expect(data.property.id).toBe("42a335b3-e72e-49ee-bc8d-ed61e9bd35e5");
       });
-    });
+    }, 10000);
 });
