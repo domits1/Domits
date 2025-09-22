5a8145c0ecc90e8a15e5547f7918aecb16f8d956
Alessio12-tech <alessioitofo88@gmail.com>
2025-09-22 13:19:20 +0200

    Revenue calculated in the backend query and cognitouerid replacement for the host id

--- backend/functions/Alessio-Revenue/data/repository.js
+++ backend/functions/Alessio-Revenue/data/repository.js
@@ -2,17 +2,25 @@ import Database from "database";
 import { Booking } from "database/models/Booking";
 
 export class Repository {
-  async getRevenue() {
+  async getRevenue(cognitoUserId) {
     const client = await Database.getInstance();
-    const response = await client
 
+    const result = await client
       .getRepository(Booking)
       .createQueryBuilder("booking")
       .innerJoin("property", "pt", "pt.id = booking.property_id")
-      .innerJoin("payment", "py", "py.stripepaymentid = booking.paymentid")
-      .where("booking.hostId = :hostId", { hostId: "0f5cc159-c8b2-48f3-bf75-114a10a1d6b3" })
-      .getMany();
+      .innerJoin("property_pricing", "pp", "pp.property_id = pt.id")
+      .where("booking.hostId = :hostId", { hostId: cognitoUserId })
+      .select(
+        `
+        SUM(
+          (DATE_PART('day', to_timestamp(booking."departuredate" / 1000) - to_timestamp(booking."arrivaldate" / 1000)) * pp.roomRate)
+          + pp.cleaning
+        ) AS "totalRevenue"
+        `
+      )
+      .getRawOne();
 
-    return response;
+    return { totalRevenue: result.totalRevenue };
   }
 }
