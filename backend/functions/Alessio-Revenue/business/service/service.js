753120a351c68301c8429c20b42070cc646824dd
Alessio12-tech <alessioitofo88@gmail.com>
2025-09-24 10:03:33 +0200

    fix for the get request endpoint

--- backend/functions/Alessio-Revenue/business/service/service.js
+++ backend/functions/Alessio-Revenue/business/service/service.js
@@ -2,21 +2,19 @@ import { Repository } from "../../data/repository.js";
 import AuthManager from "../../auth/authManager.js";
 
 export class Service {
-    constructor() {
-        this.repository = new Repository();
-        this.authManager = new AuthManager();
+  constructor() {
+    this.repository = new Repository();
+    this.authManager = new AuthManager();
+  }
+
+  async getRevenue(event) {
+    const token = event.headers.Authorization;
+    if (!token) {
+      throw new Error("Authorization token is missing");
     }
 
-    async getRevenue(event) {
-        const token = event.headers.Authorization;
-        if (!token) {
-            throw new Error("Authorization token is missing");
-        }
+    const { sub: cognitoUserId } = await this.authManager.authenticateUser(token);
 
-        // Authenticate the token to get the Cognito user ID (sub)
-        const { sub: cognitoUserId } = await this.authManager.authenticateUser(event.headers.Authorization);
-
-        // Pass the Cognito user ID to the repository
-        return await this.repository.getRevenue(cognitoUserId);
-    }
+    return await this.repository.getRevenue(cognitoUserId);
+  }
 }
