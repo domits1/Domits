941f67ca80712e1f3733b7a1045ecb6938fe1f62
Gurpreet Singh <Singhgurpreet14082002@gmail.com>
2025-09-29 09:50:47 +0200

    refactor: update get function to retrieve user payouts with new path and authorization header

--- backend/events/General-Payments-Production-CRUD-fetchHostPayout/get.js
+++ backend/events/General-Payments-Production-CRUD-fetchHostPayout/get.js
@@ -1,9 +1,15 @@
 import { handler } from "../../functions/General-Payments-Production-CRUD-fetchHostPayout/index.js";
+import { getHostAuthToken } from "../../test/util/getHostAuthToken.js";
 
 async function get() {
   console.log(
     await handler({
       httpMethod: "GET",
+      path: "/retrieve-suser-payout",
+      resource: "/retrieve-suser-payout",
+      headers: {
+        Authorization: await getHostAuthToken(),
+      },
     })
   );
 }
