62324e1b8cee336e3d0e13b2a44d5deabcc2bd64
Gurpreet Singh <Singhgurpreet14082002@gmail.com>
2025-10-09 12:47:19 +0200

    fix: update CalculateTotalRate function to return detailed pricing breakdown

--- backend/functions/General-Bookings-CRUD-Bookings-develop/util/calcuateTotalRate.js
+++ backend/functions/General-Bookings-CRUD-Bookings-develop/util/calcuateTotalRate.js
@@ -2,10 +2,14 @@ import LambdaRepository from "../data/lambdaRepository.js";
 import CalculateDifferenceInNights from "./calculateDifferenceInNights.js";
 
 async function CalculateTotalRate (propertyId, dates) {
-      const pricing = await new LambdaRepository().getPropertyPricingById(propertyId);
-      const differenceInDays = CalculateDifferenceInNights(dates.arrivalDate, dates.departureDate);
-      const basepricing = pricing.pricing[0];
-      const total = (basepricing.roomrate * 1.10 + basepricing.cleaning) * differenceInDays; // workaround: encapsulate pricing
-      return Math.round(total * 100); 
+  const pricing = await new LambdaRepository().getPropertyPricingById(propertyId);
+  const differenceInDays = CalculateDifferenceInNights(dates.arrivalDate, dates.departureDate);
+  const basepricing = pricing.pricing[0];
+  const totalWithoutCleaningfee = (basepricing.roomrate * 1.1) * differenceInDays; // workaround: encapsulate pricing
+  const totalWithCleaningfee = (basepricing.roomrate * 1.1 + basepricing.cleaning) * differenceInDays; // workaround: encapsulate pricing
+  return {
+    totalWithoutCleaningfee: Math.round(totalWithoutCleaningfee * 100),
+    totalWithCleaningfee: Math.round(totalWithCleaningfee * 100),
+  };
 };
 export default CalculateTotalRate;
