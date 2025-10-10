f0ec43750aa0aa4e12ed26a4f1d13ee8d862d812
Gurpreet Singh <Singhgurpreet14082002@gmail.com>
2025-10-10 13:28:17 +0200

    fix: remove workaround comments from total rate calculation

--- backend/functions/General-Bookings-CRUD-Bookings-develop/util/calcuateTotalRate.js
+++ backend/functions/General-Bookings-CRUD-Bookings-develop/util/calcuateTotalRate.js
@@ -5,8 +5,8 @@ async function CalculateTotalRate (propertyId, dates) {
   const pricing = await new LambdaRepository().getPropertyPricingById(propertyId);
   const differenceInDays = CalculateDifferenceInNights(dates.arrivalDate, dates.departureDate);
   const basepricing = pricing.pricing[0];
-  const totalWithoutCleaningfee = (basepricing.roomrate * 1.1) * differenceInDays; // workaround: encapsulate pricing
-  const totalWithCleaningfee = (basepricing.roomrate * 1.1 + basepricing.cleaning) * differenceInDays; // workaround: encapsulate pricing
+  const totalWithoutCleaningfee = (basepricing.roomrate * 1.1) * differenceInDays;
+  const totalWithCleaningfee = (basepricing.roomrate + basepricing.cleaning) * differenceInDays;
   return {
     totalWithoutCleaningfee: Math.round(totalWithoutCleaningfee * 100),
     totalWithCleaningfee: Math.round(totalWithCleaningfee * 100),
