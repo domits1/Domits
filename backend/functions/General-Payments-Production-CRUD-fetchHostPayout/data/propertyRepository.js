0ae7e6618d50feb0321c203b4423b8c1651665d3
Gurpreet Singh <Singhgurpreet14082002@gmail.com>
2025-10-16 18:03:42 +0200

    refactor: enhance getProperty method to include property images in response

--- backend/functions/General-Payments-Production-CRUD-fetchHostPayout/data/propertyRepository.js
+++ backend/functions/General-Payments-Production-CRUD-fetchHostPayout/data/propertyRepository.js
@@ -1,5 +1,6 @@
 import Database from "database";
 import { Property } from "database/models/Property";
+import { Property_Image } from "database/models/Property_Image";
 import "dotenv/config";
   
 export default class PropertyRepository {
@@ -8,10 +9,23 @@ export default class PropertyRepository {
   async getProperty(property_id) {
     const client = await Database.getInstance();
     const record = await client
-      .getRepository(Property)
-      .createQueryBuilder("property")
-      .where("property.id = :id", { id: property_id })
-      .getOne();
+    .getRepository(Property)
+    .createQueryBuilder("property")
+    .leftJoin("property_image", "image", "image.property_id = property.id")
+    .where("property.id = :id", { id: property_id })
+    .select([
+        "property.id",
+        "property.updatedat",
+        "property.title",
+        "property.subtitle",
+        "property.description",
+        "property.registrationnumber",
+        "property.hostid",
+        "property.status",
+        "property.createdat",
+        "image.key",
+    ])
+    .getRawOne();
 
     return record;
   }
