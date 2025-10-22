65346457e495af14adf0e434bcfdd12cf24a9b1b
Ramandeep Singh <bambadevstuff@gmail.com>
2025-10-22 15:24:05 +0200

    clean: prettier formatting + adds semicolon back

--- frontend/web/src/components/base/FilterButton.jsx
+++ frontend/web/src/components/base/FilterButton.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import { FaSlidersH } from 'react-icons/fa';
-import FilterModal from './FilterModal'; 
-import './FilterButton.scss';
+import { useState } from "react";
+import { FaSlidersH } from "react-icons/fa";
+import FilterModal from "./FilterModal";
+import "./FilterButton.scss";
 
 const FilterButton = () => {
   const [isModalOpen, setModalOpen] = useState(false);
@@ -9,18 +9,15 @@ const FilterButton = () => {
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
 
- return (
-  <div className="filters-container">
-    <button
-      className="filter-button"
-      onClick={openModal}
-    >
-      <FaSlidersH size={15} />
-      <span className="filter-text">Filter</span>
-    </button>
-    <FilterModal isOpen={isModalOpen} onClose={closeModal} />
-  </div>
-);
-} 
+  return (
+    <div className="filters-container">
+      <button className="filter-button" onClick={openModal}>
+        <FaSlidersH size={15} />
+        <span className="filter-text">Filter</span>
+      </button>
+      <FilterModal isOpen={isModalOpen} onClose={closeModal} />
+    </div>
+  );
+};
 
 export default FilterButton;
