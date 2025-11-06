967d1bd09fc380ac6aa20d162d93e817d38356b1
Gurpreet Singh <Singhgurpreet14082002@gmail.com>
2025-11-06 11:00:18 +0100

    fix: correct import path for TablePager component

--- frontend/web/src/features/hostdashboard/HostFinanceTab.js
+++ frontend/web/src/features/hostdashboard/HostFinanceTab.js
@@ -13,7 +13,7 @@ import {
 import ClipLoader from "react-spinners/ClipLoader";
 import formatMoney from "./hostfinance/utils/formatMoney";
 import StatusBadge from "./hostfinance/components/StatusBadge/StatusBadge";
-import { TablePager } from "./hostfinance/components/TabelPager/TabelPager";
+import { TablePager } from "./hostfinance/components/TabelPager/TablePager";
 import { pageSlice, MAX_ITEMS_PER_PAGE, getTotalPages } from "./hostfinance/utils/pagination";
 
 const S3_URL = "https://accommodation.s3.eu-north-1.amazonaws.com/";
