3b57cdefb3578fc3b8d44d2b7f5eb45000ac1838
Alessio12-tech <alessioitofo88@gmail.com>
2025-11-18 12:11:30 +0100

    added payment service to seperate stripe function and removed globla styling

--- backend/functions/Alessio-Revenue/business/service/service.js
+++ backend/functions/Alessio-Revenue/business/service/service.js
@@ -2,6 +2,7 @@ import { Repository } from "../../data/repository.js";
 import StripeAccountRepository from "../../../General-Payments-Production-CRUD-fetchHostPayout/data/stripeAccountRepository.js";
 import AuthManager from "../../auth/authManager.js";
 import Stripe from "stripe";
+import { PaymentsService } from "./paymentService.js";
 
 export class Service {
   constructor() {
@@ -9,46 +10,7 @@ export class Service {
     this.repository = new Repository();
     this.stripeAccountRepository = new StripeAccountRepository();
     this.authManager = new AuthManager();
-  }
-
-  async getTotalHostRevenue(event) {
-    const token = event.headers.Authorization;
-    if (!token) throw new Error("Authorization token is missing");
-
-    const { sub: cognitoUserId } = await this.authManager.authenticateUser(token);
-    if (!cognitoUserId) throw new Error("User ID is missing");
-
-    const stripeAccount = await this.stripeAccountRepository.getExistingStripeAccount(cognitoUserId);
-    if (!stripeAccount?.account_id) {
-      throw new Error("No Stripe account found for this user.");
-    }
-
-    const transfers = await this.stripe.transfers.list({
-      destination: stripeAccount.account_id,
-      limit: 100,
-      expand: [
-        "data.source_transaction",
-        "data.source_transaction.balance_transaction",
-        "data.source_transaction.application_fee",
-        "data.source_transaction.payment_intent",
-      ],
-    });
-
-    let totalRevenue = 0;
-
-    for (const tr of transfers.data) {
-      const charge = tr.source_transaction;
-      const appFee = charge.application_fee;
-      const customerPaid = charge.amount;
-      const platformFeeGross = appFee.amount;
-      const hostReceives = customerPaid - platformFeeGross;
-      totalRevenue += hostReceives;
-    }
-return { totalRevenue: (totalRevenue / 100).toLocaleString("de-DE", {
-  minimumFractionDigits: 2,
-  maximumFractionDigits: 2,
-}) };
-
+    this.paymentsService = new PaymentsService();
   }
 
   async getKpiMetric(event, kpiMetric) {
@@ -63,7 +25,7 @@ return { totalRevenue: (totalRevenue / 100).toLocaleString("de-DE", {
     const { filterType, startDate, endDate } = event.queryStringParameters || event.body || {};
     const { startDate: start, endDate: end } = this.getDateRange(filterType, startDate, endDate);
 
-    const totalRevenue = await this.getTotalHostRevenue(event);
+    const totalRevenue = await this.paymentsService.getTotalHostRevenue(event);
 
     const bookedNights = await this.repository.getBookedNights(userId, start, end);
     const availableNights = await this.repository.getAvailableNights(userId, start, end);
