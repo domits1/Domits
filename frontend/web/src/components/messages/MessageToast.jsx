884f645955b5cf6f243bfc3ee91eede3705234ad
Moncef Elmzouri <elmzourim@outlook.com>
2025-11-25 12:35:55 +0100

    pr comments
    removed console.log
    fixed inline scss

--- frontend/web/src/components/messages/MessageToast.jsx
+++ frontend/web/src/components/messages/MessageToast.jsx
@@ -1,49 +1,31 @@
 import React from 'react';
 import profileImage from './domits-logo.jpg';
+import './MessageToast.scss';
 
 const MessageToast = ({ contactName, contactImage, message }) => {
-  const maxLength = 80;
-  const truncatedMessage = message && message.length > maxLength 
-    ? `${message.substring(0, maxLength)}...` 
-    : message;
+    const maxLength = 80;
+    const truncatedMessage =
+        message && message.length > maxLength
+            ? `${message.substring(0, maxLength)}...`
+            : message;
 
-  return (
-    <div style={{ display: 'flex', alignItems: 'flex-start', gap: '12px', width: '100%' }}>
-      <img 
-        src={contactImage || profileImage} 
-        alt={contactName || 'Contact'} 
-        style={{
-          width: '40px',
-          height: '40px',
-          borderRadius: '50%',
-          objectFit: 'cover',
-          flexShrink: 0,
-          marginTop: '10px'
-        }}
-      />
-      <div style={{ flex: 1, minWidth: 0 }}>
-        <div style={{ 
-          fontWeight: 700, 
-          fontSize: '16px', 
-          marginBottom: '4px',
-          color: '#E5E5E5',
-          lineHeight: '1.4',
-          marginLeft: '2px'
-        }}>
-          {contactName || 'Contact'}
+    return (
+        <div className="message-toast">
+            <img
+                className="message-toast__avatar"
+                src={contactImage || profileImage}
+                alt={contactName || 'Contact'}
+            />
+            <div className="message-toast__content">
+                <div className="message-toast__name">
+                    {contactName || 'Contact'}
+                </div>
+                <div className="message-toast__body">
+                    {truncatedMessage}
+                </div>
+            </div>
         </div>
-        <div style={{ 
-          fontSize: '13px', 
-          color: '#B3B3B3',
-          wordBreak: 'break-word',
-          lineHeight: '1.4',
-          marginLeft: '2px'
-        }}>
-          {truncatedMessage}
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default MessageToast;
