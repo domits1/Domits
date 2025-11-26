8468373d814a78e22994231a9856005b7a9c3b04
Burak Ozdere <ozdereb@hva.nl>
2025-11-26 12:24:59 +0100

    removed data that is unnecessary in the property handler

--- backend/functions/PropertyHandler/business/service/propertyService.js
+++ backend/functions/PropertyHandler/business/service/propertyService.js
@@ -159,13 +159,12 @@ export class PropertyService {
   }
 
   async getCardPropertyAttributes(propertyId) {
-    const [basePropertyInfo, generalDetails, pricing, images, location, checkIn] = await Promise.all([
+    const [basePropertyInfo, generalDetails, pricing, images, location] = await Promise.all([
       this.getBasePropertyInfo(propertyId),
       this.getGeneralDetails(propertyId),
       this.getPricing(propertyId),
       this.getImages(propertyId),
       this.getLocation(propertyId),
-      this.getCheckIn(propertyId),
     ]);
     if (!basePropertyInfo) {
       throw new NotFoundException(`Property ${propertyId} not found.`);
@@ -176,7 +175,6 @@ export class PropertyService {
       propertyPricing: pricing,
       propertyImages: images,
       propertyLocation: location,
-      propertyCheckIn: checkIn,
     };
   }
 
