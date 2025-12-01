74533beef16b4e53dc994f93f93c4aebd43a7ced
Ramandeep Singh <67043806+Bambaclad1@users.noreply.github.com>
2025-12-01 17:00:15 +0100

    oh shit

--- backend/test/util/getHostAuthToken.js
+++ backend/test/util/getHostAuthToken.js
@@ -12,8 +12,8 @@ export async function getHostAuthToken() {
       ClientId: "3mbk6j5phshnmnc8nljued41qt",
       AuthFlow: "ADMIN_NO_SRP_AUTH",
       AuthParameters: {
-          USERNAME: "bambadevstuff@gmail.com",
-          PASSWORD: "password123",
+          USERNAME: "xasici5246@cigidea.com",
+          PASSWORD: "Test.123",
       },
       });
 
