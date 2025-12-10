5510b1c6b2b3810b91bb047df0894a5408e91172
YusufTopal61 <yusuf.topal@hva.nl>
2025-12-10 17:24:49 +0100

    backend code import ical

--- backend/functions/Ical-retrieve/controller/controller.js
+++ backend/functions/Ical-retrieve/controller/controller.js
@@ -1,31 +1,39 @@
-import {Service} from "../business/service/service.js";
-import {AuthManager} from "../auth/authManager.js";
-
-import responseHeaders from "../util/constant/responseHeader.json" with { type: "json" };
+import { Service } from "../business/service/service.js";
+import { AuthManager } from "../auth/authManager.js";
+import { ok, err, parseJson } from "../util/http.js";
 
 export class Controller {
-    service;
-    authManager;
+  service;
+  authManager;
 
-    constructor() {
-        this.service = new Service();
-        this.authManager = new AuthManager();
+  constructor() {
+    this.service = new Service();
+    this.authManager = new AuthManager();
+  }
+
+  async retrieve(event) {
+    try {
+      const token =
+        event.headers?.Authorization ||
+        event.headers?.authorization ||
+        event?.multiValueHeaders?.Authorization?.[0] ||
+        event?.multiValueHeaders?.authorization?.[0] ||
+        event?.params?.header?.Authorization ||
+        event?.params?.header?.authorization ||
+        event.authorizationToken;
+
+      await this.authManager.userIsAuthorized(token);
+
+      const body = parseJson(event);
+      if (!body?.calendarUrl) {
+        return err(400, "calendarUrl is required");
+      }
+
+      const data = await this.service.retrieveFromExternalCalendar(body.calendarUrl);
+      return ok(data);
+    } catch (e) {
+      console.error(e);
+      return err(e.statusCode || 500, e.message || "Internal error");
     }
-
-    async getUser(event) {
-        try {
-            return {
-                statusCode: 200,
-                body: JSON.stringify(await this.service.getUser()),
-                headers: responseHeaders
-            };
-        } catch (error) {
-            console.error(error.message);
-            return {
-                statusCode: error.statusCode || 500,
-                body: JSON.stringify(error.message || "Something went wrong, please contact support.")
-            }
-        }
-    }
-
+  }
 }
