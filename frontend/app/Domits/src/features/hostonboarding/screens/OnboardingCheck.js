773f1f6e5a108bc116add6cee805f09439a0faa7
Stephanie Lahmann <Stephanie_Lahmann@outlook.com>
2025-12-11 16:06:31 +0100

    fix: Reformatting OnboardingCheck.js
    - formatting of lines

--- frontend/app/Domits/src/features/hostonboarding/screens/OnboardingCheck.js
+++ frontend/app/Domits/src/features/hostonboarding/screens/OnboardingCheck.js
@@ -23,14 +23,14 @@ const OnboardingCheck = ({route, navigation}) => {
 
   const propertyGeneralDetailsString = () => {
     return formData.propertyGeneralDetails
-      .map(item => `${item.detail}: ${item.value}`)
-      .join('\n');
+        .map(item => `${item.detail}: ${item.value}`)
+        .join('\n');
   };
 
   const propertyHouseRulesString = () => {
     return formData.propertyRules
-      .map(item => `${item.rule.replace('allow', '')}: ${item.value ? '✔' : '✖'}`)
-      .join('\n')
+        .map(item => `${item.rule.replace('allow', '')}: ${item.value ? '✔' : '✖'}`)
+        .join('\n')
   };
 
   const createProperty = async () => {
@@ -59,117 +59,117 @@ const OnboardingCheck = ({route, navigation}) => {
     const Modal = modalComponent;
 
     return (
-      <View style={styles.tableItem}>
-        <View style={styles.labelItem}>
-          <Text style={styles.labelText}>
-            <TranslatedText textToTranslate={label} />
-          </Text>
-        </View>
-        <TouchableOpacity disabled={modalComponent == null} style={styles.valueItem}
-                          onPress={() => setShowModal(!showModal)}>
-          <Text style={styles.valueText} numberOfLines={4}>
-            <TranslatedText textToTranslate={value} />
-          </Text>
-        </TouchableOpacity>
+        <View style={styles.tableItem}>
+          <View style={styles.labelItem}>
+            <Text style={styles.labelText}>
+              <TranslatedText textToTranslate={label}/>
+            </Text>
+          </View>
+          <TouchableOpacity disabled={modalComponent == null} style={styles.valueItem}
+                            onPress={() => setShowModal(!showModal)}>
+            <Text style={styles.valueText} numberOfLines={4}>
+              <TranslatedText textToTranslate={value}/>
+            </Text>
+          </TouchableOpacity>
 
-        {showModal && modalComponent && (
-          <Modal
-            label={label}
-            value={value}
-            formData={formData}
-            onClose={() => setShowModal(false)}
-          />
-        )}
-      </View>
+          {showModal && modalComponent && (
+              <Modal
+                  label={label}
+                  value={value}
+                  formData={formData}
+                  onClose={() => setShowModal(false)}
+              />
+          )}
+        </View>
     )
   }
 
   return (
-    <SafeAreaView style={styles.safeAreaNavMargin}>
-      <View style={styles.contentContainer}>
-        <ScrollView persistentScrollbar={true} contentContainerStyle={styles.scrollContainer}>
+      <SafeAreaView style={styles.safeAreaNavMargin}>
+        <View style={styles.contentContainer}>
+          <ScrollView persistentScrollbar={true} contentContainerStyle={styles.scrollContainer}>
 
-          <View>
-            <View style={styles.goBackToEditContainer}>
-              <TouchableOpacity style={styles.goBackToEditButton} onPress={() => navigation.goBack()}>
-                <Ionicons name="arrow-back-outline" size={24} style={styles.goBackToEditIcon} />
-                <Text style={styles.goBackToEditText}>
-                  <TranslatedText textToTranslate={'Edit'} />
+            <View>
+              <View style={styles.goBackToEditContainer}>
+                <TouchableOpacity style={styles.goBackToEditButton} onPress={() => navigation.goBack()}>
+                  <Ionicons name="arrow-back-outline" size={24} style={styles.goBackToEditIcon}/>
+                  <Text style={styles.goBackToEditText}>
+                    <TranslatedText textToTranslate={'Edit'}/>
+                  </Text>
+                </TouchableOpacity>
+              </View>
+
+              <Text style={styles.onboardingPageTitle}>
+                <TranslatedText textToTranslate={'Overview'}/>
+              </Text>
+              <Text style={styles.onboardingPageDescription}>
+                <TranslatedText textToTranslate={'Review your property details.'}/>
+              </Text>
+            </View>
+            <View style={styles.table}>
+              {tableItem('Space', formData.propertyType.property_type)}
+              {tableItem('Space type', formData.propertyType.spaceType)}
+              {tableItem('Name', formData.property.title)}
+              {tableItem('Location',
+                  formData.propertyLocation.street + ' ' + formData.propertyLocation.houseNumber + formData.propertyLocation.houseNumberExtension + ',\n' +
+                  formData.propertyLocation.postalCode + ',\n' +
+                  formData.propertyLocation.city + ',\n' +
+                  formData.propertyLocation.country
+              )}
+              {tableItem('Description', formData.property.description, CheckTextModal)}
+              {tableItem('General details', propertyGeneralDetailsString())}
+              {tableItem('Amenities', 'Tap to see amenities', CheckAmenitiesModal)}
+              {tableItem('Check-in', formData.propertyCheckIn.checkIn.from + " - " + formData.propertyCheckIn.checkIn.till)}
+              {tableItem('Check-out', formData.propertyCheckIn.checkOut.from + " - " + formData.propertyCheckIn.checkOut.till)}
+              {tableItem('House Rules', propertyHouseRulesString())}
+              {tableItem('Photos', 'Tap to see images', CheckPhotosModal)}
+            </View>
+
+            <View>
+              <View style={styles.checkboxContainer}>
+                <CheckBox
+                    disabled={false}
+                    value={toggleComplianceCheckBox}
+                    onValueChange={(newValue) => setToggleComplianceCheckBox(newValue)}
+                />
+                <Text>
+                  <TranslatedText
+                      textToTranslate={'I declare that this property is legitimate, complete with required licenses and permits, which can be displayed upon request. Domits B.V. reserves the right to verify and investigate your registration information.'}/>
                 </Text>
+              </View>
+              <View style={styles.checkboxContainer}>
+                <CheckBox
+                    disabled={false}
+                    value={toggleTermsConditionsCheckBox}
+                    onValueChange={(newValue) => setToggleTermsConditionsCheckBox(newValue)}
+                />
+                <Text>
+                  <TranslatedText textToTranslate={'I confirm that I have read and accept the '}/>
+                  <Text
+                      style={{color: 'blue', textDecorationLine: 'underline'}}
+                      onPress={() => Linking.openURL('https://www.domits.com/terms')}>
+                    <TranslatedText textToTranslate={'General Terms and Conditions'}/>
+                  </Text>.
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.completeButtonContainer}>
+              <TouchableOpacity
+                  disabled={!areCheckboxesChecked || loadingCreateProperty}
+                  style={[styles.completeButton, !areCheckboxesChecked && {backgroundColor: 'rgb(128,128,128)'}]}
+                  onPress={() => createProperty()}>
+                <Text style={styles.completeButtonText}>
+                  <TranslatedText textToTranslate={'Complete'}/>
+                </Text>
+                {loadingCreateProperty &&
+                    <ActivityIndicator size="large" color={COLORS.domitsGuestGreen}/>
+                }
               </TouchableOpacity>
             </View>
-
-            <Text style={styles.onboardingPageTitle}>
-              <TranslatedText textToTranslate={'Overview'} />
-            </Text>
-            <Text style={styles.onboardingPageDescription}>
-              <TranslatedText textToTranslate={'Review your property details.'} />
-            </Text>
-          </View>
-          <View style={styles.table}>
-            {tableItem('Space', formData.propertyType.property_type)}
-            {tableItem('Space type', formData.propertyType.spaceType)}
-            {tableItem('Name', formData.property.title)}
-            {tableItem('Location',
-              formData.propertyLocation.street + ' ' + formData.propertyLocation.houseNumber + formData.propertyLocation.houseNumberExtension + ',\n' +
-              formData.propertyLocation.postalCode + ',\n' +
-              formData.propertyLocation.city + ',\n' +
-              formData.propertyLocation.country
-            )}
-            {tableItem('Description', formData.property.description, CheckTextModal)}
-            {tableItem('General details', propertyGeneralDetailsString())}
-            {tableItem('Amenities', 'Tap to see amenities', CheckAmenitiesModal)}
-            {tableItem('Check-in', formData.propertyCheckIn.checkIn.from + " - " + formData.propertyCheckIn.checkIn.till)}
-            {tableItem('Check-out', formData.propertyCheckIn.checkOut.from + " - " + formData.propertyCheckIn.checkOut.till)}
-            {tableItem('House Rules', propertyHouseRulesString())}
-            {tableItem('Photos', 'Tap to see images', CheckPhotosModal)}
-          </View>
-
-          <View>
-            <View style={styles.checkboxContainer}>
-              <CheckBox
-                disabled={false}
-                value={toggleComplianceCheckBox}
-                onValueChange={(newValue) => setToggleComplianceCheckBox(newValue)}
-              />
-              <Text>
-                <TranslatedText
-                  textToTranslate={'I declare that this property is legitimate, complete with required licenses and permits, which can be displayed upon request. Domits B.V. reserves the right to verify and investigate your registration information.'} />
-              </Text>
-            </View>
-            <View style={styles.checkboxContainer}>
-              <CheckBox
-                disabled={false}
-                value={toggleTermsConditionsCheckBox}
-                onValueChange={(newValue) => setToggleTermsConditionsCheckBox(newValue)}
-              />
-              <Text>
-                <TranslatedText textToTranslate={'I confirm that I have read and accept the '} />
-                <Text
-                  style={{color: 'blue', textDecorationLine: 'underline'}}
-                  onPress={() => Linking.openURL('https://www.domits.com/terms')}>
-                  <TranslatedText textToTranslate={'General Terms and Conditions'} />
-                </Text>.
-              </Text>
-            </View>
-          </View>
-
-          <View style={styles.completeButtonContainer}>
-            <TouchableOpacity
-              disabled={!areCheckboxesChecked || loadingCreateProperty}
-              style={[styles.completeButton, !areCheckboxesChecked && {backgroundColor: 'rgb(128,128,128)'}]}
-              onPress={() => createProperty()}>
-              <Text style={styles.completeButtonText}>
-                <TranslatedText textToTranslate={'Complete'} />
-              </Text>
-              {loadingCreateProperty &&
-                <ActivityIndicator size="large" color={COLORS.domitsGuestGreen} />
-              }
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-      </View>
-    </SafeAreaView>
+          </ScrollView>
+        </View>
+      </SafeAreaView>
   )
 }
 
