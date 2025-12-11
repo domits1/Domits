16ff194f8a2ead1d1e387867d925240d9f7ee075
Gurpreet Singh <Singhgurpreet14082002@gmail.com>
2025-12-11 10:07:23 +0100

    add FaqService to handle finance FAQs retrieval

---
+++ backend/functions/General-Payments-Production-CRUD-fetchHostPayout/business/service/faqService.js
@@ -0,0 +1,28 @@
+import AuthManager from "../../auth/authManager.js";
+import FaqRepository from "../../data/FaqRepository.js";
+
+import { NotFoundException } from "../../util/exception/NotFoundException.js";
+
+
+
+export default class FaqService {
+  constructor() {
+    this.authManager = new AuthManager();
+    this.faqRepository = new FaqRepository();
+  }
+
+  async getFinanceFaqs() {
+    const faqs = await this.faqRepository.getFinanceFaqs();
+
+    if (!faqs) {
+      throw new NotFoundException("No finance FAQs found");
+    }
+
+    return {
+        statusCode: 200,
+        message: "Finance FAQs fetched successfully",
+        details: { faqs },
+    };
+  }
+
+}
