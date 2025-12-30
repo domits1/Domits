b7dd4e4a0ec4be1b5e201f1511258c16580513e1
Moncef Elmzouri <elmzourim@outlook.com>
2025-12-30 09:07:57 +0100

    switch case

--- backend/functions/UnifiedMessaging/index.js
+++ backend/functions/UnifiedMessaging/index.js
@@ -3,35 +3,40 @@ import MessageController from "./controller/messageController.js";
 const controller = new MessageController();
 
 export const handler = async (event) => {
-    let returnedResponse = {};
-    const { httpMethod, path } = event;
+  let returnedResponse = {};
+  const { httpMethod, path } = event;
 
-    try {
-        if (httpMethod === 'POST' && path.endsWith('/send')) {
-             returnedResponse = await controller.sendMessage(event);
-        } else if (httpMethod === 'GET' && path.endsWith('/threads')) {
-             returnedResponse = await controller.getThreads(event);
-        } else if (httpMethod === 'GET' && path.endsWith('/messages')) {
-             returnedResponse = await controller.getMessages(event);
-        } else {
-             returnedResponse = { statusCode: 404, response: "Not Found" };
-        }
-    } catch (error) {
-        console.error(error);
-        returnedResponse = {
-            statusCode: 500,
-            response: "Internal Server Error"
-        };
+  try {
+    const route = `${httpMethod}:${path}`;
+
+    switch (true) {
+      case httpMethod === "POST" && path.endsWith("/send"):
+        returnedResponse = await controller.sendMessage(event);
+        break;
+      case httpMethod === "GET" && path.endsWith("/threads"):
+        returnedResponse = await controller.getThreads(event);
+        break;
+      case httpMethod === "GET" && path.endsWith("/messages"):
+        returnedResponse = await controller.getMessages(event);
+        break;
+      default:
+        returnedResponse = { statusCode: 404, response: "Not Found" };
     }
-
-    return {
-        statusCode: returnedResponse?.statusCode || 200,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Content-Type,Authorization",
-            ...(returnedResponse?.headers || {})
-        },
-        body: JSON.stringify(returnedResponse?.response),
+  } catch (error) {
+    console.error(error);
+    returnedResponse = {
+      statusCode: 500,
+      response: "Internal Server Error",
     };
-};
+  }
 
+  return {
+    statusCode: returnedResponse?.statusCode || 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type,Authorization",
+      ...(returnedResponse?.headers || {}),
+    },
+    body: JSON.stringify(returnedResponse?.response),
+  };
+};
