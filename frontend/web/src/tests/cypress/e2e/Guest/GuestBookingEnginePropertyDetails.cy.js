708a3c6f49dfd038a86581b9bc71be768238cc97
DawidHru <158039367+DawidHru@users.noreply.github.com>
2026-03-02 12:13:52 +0100

    Remove unused chatbot close button click code
    Removed commented-out code for closing the chatbot.

--- frontend/web/src/tests/cypress/e2e/Guest/GuestBookingEnginePropertyDetails.cy.js
+++ frontend/web/src/tests/cypress/e2e/Guest/GuestBookingEnginePropertyDetails.cy.js
@@ -19,8 +19,6 @@ describe('Property Details', () => {
         
         cy.get('.loginButton').click();
         
-        // cy.get('.hostchatbot-close-button').click();
-        
         cy.get('.logo > a > img').should('be.visible').click();
         cy.get(':nth-child(3) > .domits-accommodationGroup > :nth-child(2) > .swiper > .swiper-wrapper > .swiper-slide-visible > img').click();
 
