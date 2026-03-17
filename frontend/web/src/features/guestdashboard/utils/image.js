8374ecbc379915069cbb896c8a08c0e7e0003917
Valentijn <vaentijn.jacobs@hva.nl>
2026-03-17 10:10:45 +0100

    fix(guestdashboard): wrap image helper exports for sonar and dev stability

--- frontend/web/src/features/guestdashboard/utils/image.js
+++ frontend/web/src/features/guestdashboard/utils/image.js
@@ -1,20 +1,22 @@
 import {
   S3_ACCOMMODATION_URL,
-  placeholderImage,
-  normalizeImageUrl,
-  resolveAccommodationImageKey,
-  resolveAccommodationImageUrl,
-  resolveAccommodationImageUrls,
-  resolvePrimaryAccommodationImageUrl,
+  placeholderImage as accommodationPlaceholderImage,
+  normalizeImageUrl as normalizeAccommodationImageUrl,
+  resolveAccommodationImageKey as resolveAccommodationImageKeyInternal,
+  resolveAccommodationImageUrl as resolveAccommodationImageUrlInternal,
+  resolveAccommodationImageUrls as resolveAccommodationImageUrlsInternal,
+  resolvePrimaryAccommodationImageUrl as resolvePrimaryAccommodationImageUrlInternal,
 } from "../../../utils/accommodationImage";
 
 export const S3_URL = S3_ACCOMMODATION_URL;
-
-export {
-  placeholderImage,
-  normalizeImageUrl,
-  resolveAccommodationImageKey,
-  resolveAccommodationImageUrl,
-  resolveAccommodationImageUrls,
-  resolvePrimaryAccommodationImageUrl,
-};
+export const placeholderImage = accommodationPlaceholderImage;
+export const normalizeImageUrl = (maybeKeyOrUrl) =>
+  normalizeAccommodationImageUrl(maybeKeyOrUrl);
+export const resolveAccommodationImageKey = (image, preferredVariant = "web") =>
+  resolveAccommodationImageKeyInternal(image, preferredVariant);
+export const resolveAccommodationImageUrl = (image, preferredVariant = "web") =>
+  resolveAccommodationImageUrlInternal(image, preferredVariant);
+export const resolveAccommodationImageUrls = (images, preferredVariant = "web") =>
+  resolveAccommodationImageUrlsInternal(images, preferredVariant);
+export const resolvePrimaryAccommodationImageUrl = (images, preferredVariant = "thumb") =>
+  resolvePrimaryAccommodationImageUrlInternal(images, preferredVariant);
