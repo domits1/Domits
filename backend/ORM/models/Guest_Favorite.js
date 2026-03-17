b6f3bade580aeffead133e38b26c83fa2717edff
sakhi <sakhi.anwari@hva.nl>
2026-03-17 15:37:25 +0100

    feat(wishlist): update GetWishlist function to support custom wishlist names and handle errors gracefully

--- backend/ORM/models/Guest_Favorite.js
+++ backend/ORM/models/Guest_Favorite.js
@@ -2,29 +2,35 @@ import {EntitySchema} from "typeorm";
 
 export const Guest_Favorite = new EntitySchema({
     name: "Guest_Favorite",
+    schema: "main",
     tableName: "guest_favorite",
     columns: {
         guestId: {
+            name: "guestid",
             primary: true,
             type: "varchar",
             generated: false,
             nullable: false
         },
         wishlistKey: {
+            name: "wishlistkey",
             primary: true,
             type: "varchar",
             generated: false,
             nullable: false
         },
         isPlaceholder: {
+            name: "isplaceholder",
             type: "boolean",
             nullable: false
         },
         propertyId: {
+            name: "propertyid",
             type: "varchar",
             nullable: true
         },
         wishlistName: {
+            name: "wishlistname",
             type: "varchar",
             nullable: false
         }
