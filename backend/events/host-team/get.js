3b7924922b5220ca0c77388e7442aeffe08828d9
DawidHru <158039367+DawidHru@users.noreply.github.com>
2026-05-13 16:45:58 +0200

    Clean up get.js by removing unused code
    Remove commented-out code for GET request handler.

--- backend/events/host-team/get.js
+++ backend/events/host-team/get.js
@@ -1,7 +1,1 @@
 import { handler } from "../function/index.js";
-
-console.log(await handler({
-    httpMethod: "GET",
-    path: "/team",
-    headers: { Authorization: "your-access-token-here" }
-}));
