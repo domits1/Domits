c6f19ba0b34fce7376fb1c38516df49704be9cb4
sakhi <sakhi.anwari@hva.nl>
2026-05-19 12:16:13 +0200

    refactor(PanoramaLandingTemplate): remove unused import for scroll reveal props

--- frontend/web/src/features/hostdashboard/website/rendering/templates/PanoramaLandingTemplate.jsx
+++ frontend/web/src/features/hostdashboard/website/rendering/templates/PanoramaLandingTemplate.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "../WebsiteTemplatePreview.module.scss";
-import { getScrollRevealProps } from "../animations/scrollRevealProps.js";
 import { getAmenityIconNode } from "../amenityIconRegistry";
 import {
   getInteractiveTargetProps,
