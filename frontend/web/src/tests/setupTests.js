8539d1169e558670e3f1dd0587eef4b6109dd6ca
Ameen <Ameen.Abdelrahman@hva.nl>
2026-06-23 14:03:32 +0200

    test: load TextDecoder polyfill in shared Jest setup

--- frontend/web/src/tests/setupTests.js
+++ frontend/web/src/tests/setupTests.js
@@ -1,5 +1,15 @@
+const { TextEncoder, TextDecoder } = require("util");
+
+if (typeof global.TextEncoder === "undefined") {
+  global.TextEncoder = TextEncoder;
+}
+
+if (typeof global.TextDecoder === "undefined") {
+  global.TextDecoder = TextDecoder;
+}
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+require("@testing-library/jest-dom");
