4d97feb317ecae37bc96dd51ee18541ad1c12eea
Ameen <Ameen.Abdelrahman@hva.nl>
2026-06-23 13:40:53 +0200

    git commit -m "test: add TextDecoder polyfill for Jest environment"

---
+++ frontend/web/src/setupTests.js
@@ -0,0 +1,11 @@
+const { TextEncoder, TextDecoder } = require("util");
+
+if (typeof global.TextEncoder === "undefined") {
+  global.TextEncoder = TextEncoder;
+}
+
+if (typeof global.TextDecoder === "undefined") {
+  global.TextDecoder = TextDecoder;
+}
+
+require("./tests/setupTests");
