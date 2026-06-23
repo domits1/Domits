072e0ce954f90dec832bf76225cbfd5af86f5fa9
sakhi <sakhi.anwari@hva.nl>
2026-06-23 13:16:22 +0200

    test: address sonar smells in setupTests polyfill
    Use node:util import, globalThis instead of global, and direct
    undefined comparison instead of typeof.

--- frontend/web/src/setupTests.js
+++ frontend/web/src/setupTests.js
@@ -1,11 +1,11 @@
 // jsdom does not provide TextEncoder/TextDecoder, which newer AWS SDK
 // (@smithy/@aws-sdk) modules require at import time when `aws-amplify`
 // is loaded. Polyfill them globally before any test runs.
-import { TextEncoder, TextDecoder } from "util";
+import { TextEncoder, TextDecoder } from "node:util";
 
-if (typeof global.TextEncoder === "undefined") {
-  global.TextEncoder = TextEncoder;
+if (globalThis.TextEncoder === undefined) {
+  globalThis.TextEncoder = TextEncoder;
 }
-if (typeof global.TextDecoder === "undefined") {
-  global.TextDecoder = TextDecoder;
+if (globalThis.TextDecoder === undefined) {
+  globalThis.TextDecoder = TextDecoder;
 }
