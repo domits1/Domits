e9a1366118d467281ecfec30f5ae8c09c52565f2
BeMounir <mounir.bekkaoui@student.hu.nl>
2026-08-31 14:39:46 +0200

    Added myself to the about page as a beginner issue

--- frontend/web/src/pages/about/Team.js
+++ frontend/web/src/pages/about/Team.js
@@ -22,6 +22,7 @@ import Sakhi from "../../images/about-img/Sakhi.png";
 import Denisa from "../../images/about-img/Denisa.jpeg";
 import Omer from "../../images/about-img/Omer.jpg";
 import Ameen from "../../images/about-img/Ameen.jpg";
+import Mounir from "../../images/about-img/Mounir.png";
 
 import { LanguageContext } from "../../context/LanguageContext.js";
 import en from "../../content/en.json";
@@ -73,6 +74,7 @@ function Team() {
     { name: "Omer", src: Omer, role: aboutContent.crew.developer },
     { name: "Ameen", src: Ameen, role: aboutContent.crew.developer },
     { name: "Hadeel", src: standard, role: aboutContent.crew.developer },
+    { name: "Mounir", src: Mounir, role: aboutContent.crew.developer },
   ].sort((a, b) => a.name.localeCompare(b.name));
 
   const marketingMembers = [
